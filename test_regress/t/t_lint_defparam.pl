1e6808635571455fa3761e6cff80fc59b4ec5e93
Wilson Snyder <wsnyder@wsnyder.org>
2011-01-01 18:12:29 -0500

    Fix defparam test failing after make clean

--- test_regress/t/t_lint_defparam.pl
+++ test_regress/t/t_lint_defparam.pl
@@ -8,8 +8,11 @@ if (!$::Driver) { use FindBin; exec("$FindBin::Bin/bootstrap.pl", @ARGV, $0); di
 # Version 2.0.
 
 compile (
-	 v_flags2 => ["--lint-only"],
-	 ) if $Self->{v3};
+    v_flags2 => ["--lint-only"],
+    verilator_make_gcc => 0,
+    make_top_shell => 0,
+    make_main => 0,
+    ) if $Self->{v3};
 
 ok(1);
 1;
