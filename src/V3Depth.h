c2c7c7bd9ae527b8c208eb25307da0d7061f58ce
Wilson Snyder <wsnyder@wsnyder.org>
2012-01-15 10:26:28 -0500

    Copyright year update

--- src/V3Depth.h
+++ src/V3Depth.h
@@ -8,7 +8,7 @@
 //
 //*************************************************************************
 //
-// Copyright 2003-2011 by Wilson Snyder.  This program is free software; you can
+// Copyright 2003-2012 by Wilson Snyder.  This program is free software; you can
 // redistribute it and/or modify it under the terms of either the GNU
 // Lesser General Public License Version 3 or the Perl Artistic License
 // Version 2.0.
