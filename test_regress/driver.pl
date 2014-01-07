4422de0c6c6f48b6a65e18b3c161db2e060f3ff9
Wilson Snyder <wsnyder@wsnyder.org>
2014-01-06 19:28:57 -0500

    Copyright year update.

--- test_regress/driver.pl
+++ test_regress/driver.pl
@@ -1896,7 +1896,7 @@ Command to use to invoke VCS.
 
 The latest version is available from L<http://www.veripool.org/>.
 
-Copyright 2003-2013 by Wilson Snyder.  Verilator is free software; you can
+Copyright 2003-2014 by Wilson Snyder.  Verilator is free software; you can
 redistribute it and/or modify it under the terms of either the GNU Lesser
 General Public License Version 3 or the Perl Artistic License Version 2.0.
 
--- test_verilated/vgen.pl
+++ test_verilated/vgen.pl
@@ -1133,7 +1133,7 @@ Include some signed arithmetic in the generated code.  Experimental.
 
 =head1 DISTRIBUTION
 
-Copyright 2001-2013 by Wilson Snyder.  Verilator is free software; you can
+Copyright 2001-2014 by Wilson Snyder.  Verilator is free software; you can
 redistribute it and/or modify it under the terms of either the GNU Lesser
 General Public License Version 3 or the Perl Artistic License Version 2.0.
 
