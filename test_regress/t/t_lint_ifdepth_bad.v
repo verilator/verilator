d56ca250895d6d586b84d17dbe1468afeecc6146
Todd Strader <todd.strader@twosigma.com>
2015-12-01 18:17:26 -0500

    Tests: Fix false hidden module warning.
    Signed-off-by: Wilson Snyder <wsnyder@wsnyder.org>

--- test_regress/t/t_lint_ifdepth_bad.v
+++ test_regress/t/t_lint_ifdepth_bad.v
@@ -5,36 +5,36 @@
 
 module t;
 
-   integer v = 19;
+   integer value = 19;
 
    initial begin
-      if (v==1) begin end
-      else if (v==2) begin end
-      else if (v==3) begin end
-      else if (v==4) begin end
-      else if (v==5) begin end
-      else if (v==6) begin end
-      else if (v==7) begin end
-      else if (v==8) begin end
-      else if (v==9) begin end
-      else if (v==10) begin end
-      else if (v==11) begin end  // Warn about this one
-      else if (v==12) begin end
+      if (value==1) begin end
+      else if (value==2) begin end
+      else if (value==3) begin end
+      else if (value==4) begin end
+      else if (value==5) begin end
+      else if (value==6) begin end
+      else if (value==7) begin end
+      else if (value==8) begin end
+      else if (value==9) begin end
+      else if (value==10) begin end
+      else if (value==11) begin end  // Warn about this one
+      else if (value==12) begin end
    end
 
    initial begin
-      unique0 if (v==1) begin end
-      else if (v==2) begin end
-      else if (v==3) begin end
-      else if (v==4) begin end
-      else if (v==5) begin end
-      else if (v==6) begin end
-      else if (v==7) begin end
-      else if (v==8) begin end
-      else if (v==9) begin end
-      else if (v==10) begin end
-      else if (v==11) begin end  // Warn about this one
-      else if (v==12) begin end
+      unique0 if (value==1) begin end
+      else if (value==2) begin end
+      else if (value==3) begin end
+      else if (value==4) begin end
+      else if (value==5) begin end
+      else if (value==6) begin end
+      else if (value==7) begin end
+      else if (value==8) begin end
+      else if (value==9) begin end
+      else if (value==10) begin end
+      else if (value==11) begin end  // Warn about this one
+      else if (value==12) begin end
    end
 
 endmodule
