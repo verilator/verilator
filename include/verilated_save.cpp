e8b5001cfe9cae1e0c0c5be7073d809bbf40044b
Wilson Snyder <wsnyder@wsnyder.org>
2015-12-14 19:58:22 -0500

    Fix model restore crash, bug1013.

--- include/verilated_save.cpp
+++ include/verilated_save.cpp
@@ -201,7 +201,7 @@ void VerilatedRestore::fill() {
     if (VL_UNLIKELY(!isOpen())) return;
     // Move remaining characters down to start of buffer.  (No memcpy, overlaps allowed)
     vluint8_t* rp = m_bufp;
-    for (vluint8_t* sp=m_cp; rp < m_endp;) *rp++ = *sp++;  // Overlaps
+    for (vluint8_t* sp=m_cp; sp < m_endp;) *rp++ = *sp++;  // Overlaps
     m_endp = m_bufp + (m_endp - m_cp);
     m_cp = m_bufp; // Reset buffer
     // Read into buffer starting at m_endp 
