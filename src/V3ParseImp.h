b2623b98412d8e30983c5f952e8311a194a05778
Stefan Wallentowitz <stefan@wallentowitz.de>
2016-03-23 07:48:32 -0400

    Internals: Add success parameter to parseDouble. No functional change intended.
    Signed-off-by: Wilson Snyder <wsnyder@wsnyder.org>

--- src/V3ParseImp.h
+++ src/V3ParseImp.h
@@ -140,7 +140,7 @@ public:
     void verilatorCmtLintSave();
     void verilatorCmtLintRestore();
     void verilatorCmtBad(const char* text);
-    double parseDouble(const char* text, size_t length);
+    static double parseDouble(const char* text, size_t length, bool* successp = NULL);
     void pushBeginKeywords(int state) { m_inBeginKwd++; m_lastVerilogState=state; }
     bool popBeginKeywords() { if (m_inBeginKwd) { m_inBeginKwd--; return true; } else return false; }
     int lastVerilogState() const { return m_lastVerilogState; }
