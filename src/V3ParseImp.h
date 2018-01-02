8e65d93d6db256cd36eaeca24c41a1fef7157129
Wilson Snyder <wsnyder@wsnyder.org>
2018-01-02 18:05:06 -0500

    Copyright year update. No functional change.

--- src/V3ParseImp.h
+++ src/V3ParseImp.h
@@ -6,7 +6,7 @@
 //
 //*************************************************************************
 //
-// Copyright 2009-2017 by Wilson Snyder.  This program is free software; you can
+// Copyright 2009-2018 by Wilson Snyder.  This program is free software; you can
 // redistribute it and/or modify it under the terms of either the GNU
 // Lesser General Public License Version 3 or the Perl Artistic License
 // Version 2.0.
--- test_regress/vgen.pl
+++ test_regress/vgen.pl
@@ -1037,7 +1037,7 @@ Include some signed arithmetic in the generated code.  Experimental.
 
 =head1 DISTRIBUTION
 
-Copyright 2001-2017 by Wilson Snyder.  Verilator is free software; you can
+Copyright 2001-2018 by Wilson Snyder.  Verilator is free software; you can
 redistribute it and/or modify it under the terms of either the GNU Lesser
 General Public License Version 3 or the Perl Artistic License Version 2.0.
 
