847dbbbaf011973dc5fc0802265d8bdf061c0ec0
Wilson Snyder <wsnyder@wsnyder.org>
2018-08-21 18:09:40 -0400

    Fix function inlining inside certain while loops, bug1330.

---
+++ test_regress/t/t_func_while.v
@@ -0,0 +1,32 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed into the Public Domain, for any use,
+// without warranty, 2018 by Julien Margetts.
+
+module t #(parameter sz = 4096)
+   (
+    input wire                     clk,
+    output reg [tdw(sz)-1:0] data
+    );
+
+   // bug1330
+   function integer clog2(input integer value);
+      integer tmp;
+      tmp   = value-1;
+      clog2 = 0;
+      for (clog2=0; (tmp>0) && (clog2<32); clog2=clog2+1)
+        tmp = tmp>>1;
+   endfunction
+
+   function integer tdw(input integer sz);
+      tdw = clog2(sz);
+   endfunction
+
+   integer b;
+
+   always @(posedge clk)
+     for (b=0; b<tdw(sz); b=b+1)
+       if ((data[b] === 1'bx))
+         $display("WARNING: %1t Writing X's to tag RAM [%m]", $time);
+
+endmodule
