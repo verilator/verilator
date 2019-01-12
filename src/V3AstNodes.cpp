a2a09253a29fab19a4e7dc08b568b78e94f99b04
Wilson Snyder <wsnyder@wsnyder.org>
2019-01-12 09:43:24 -0500

    Internals: Show static funcs.

--- src/V3AstNodes.cpp
+++ src/V3AstNodes.cpp
@@ -1197,6 +1197,7 @@ void AstCFunc::dump(std::ostream& str) {
     this->AstNode::dump(str);
     if (slow()) str<<" [SLOW]";
     if (pure()) str<<" [PURE]";
+    if (isStatic()) str<<" [STATIC]";
     if (dpiImport()) str<<" [DPII]";
     if (dpiExport()) str<<" [DPIX]";
     if (dpiExportWrapper()) str<<" [DPIXWR]";
