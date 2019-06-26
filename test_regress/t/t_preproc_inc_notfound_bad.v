6903c52ef7d63fcc1565d9018983e16f845f9477
Wilson Snyder <wsnyder@wsnyder.org>
2019-06-26 19:12:57 -0400

    Tests: Add coverage of missing include error.

---
+++ test_regress/t/t_preproc_inc_notfound_bad.v
@@ -0,0 +1,6 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed into the Public Domain, for any use,
+// without warranty, 2019 by Wilson Snyder.
+
+`include "this_file_is_not_found.vh"
