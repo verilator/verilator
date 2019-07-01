efe739c52f71afe32194afb10960b75df779b8f4
Wilson Snyder <wsnyder@wsnyder.org>
2019-06-30 21:36:57 -0400

    Tests: Cover --error-limit and update docs (found via coverage).

---
+++ test_regress/t/t_flag_errorlimit_bad.v
@@ -0,0 +1,16 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed into the Public Domain, for any use,
+// without warranty, 2019 by Wilson Snyder.
+
+module t (/*AUTOARG*/);
+
+   int u1;
+   int u1;
+   int u1;
+   int u1;
+   int u1;
+   int u1;
+   int u1;
+
+endmodule
