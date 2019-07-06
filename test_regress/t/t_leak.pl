a5f2ce0dadc52137c211df479d829ba79198579f
Wilson Snyder <wsnyder@wsnyder.org>
2019-07-06 13:47:17 -0400

    Tests: Disable t_leak on Travis-CI.

--- test_regress/t/t_leak.pl
+++ test_regress/t/t_leak.pl
@@ -9,15 +9,20 @@ if (!$::Driver) { use FindBin; exec("$FindBin::Bin/bootstrap.pl", @ARGV, $0); di
 
 scenarios(vlt_all => 1);
 
-compile(
-    make_top_shell => 0,
-    make_main => 0,
-    verilator_flags2 => ["--exe $Self->{t_dir}/$Self->{name}.cpp"],
-    );
+if ($ENV{TRAVIS} && $ENV{TRAVIS} eq 'true') {
+    skip("Fails occasionally on Travis-CI");
+} else {
+    compile(
+        make_top_shell => 0,
+        make_main => 0,
+        verilator_flags2 => ["--exe $Self->{t_dir}/$Self->{name}.cpp"],
+        );
 
-execute(
-    check_finished => 1,
-    );
+    execute(
+        check_finished => 1,
+        );
 
-ok(1);
+    ok(1);
+}
+
 1;
