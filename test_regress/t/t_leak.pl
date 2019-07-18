a3fd105acb8a323570d44a4b2417293abb47245e
Todd Strader <todd.strader@gmail.com>
2019-07-18 05:31:02 -0400

    Add more Travis coverage

--- test_regress/t/t_leak.pl
+++ test_regress/t/t_leak.pl
@@ -7,22 +7,23 @@ if (!$::Driver) { use FindBin; exec("$FindBin::Bin/bootstrap.pl", @ARGV, $0); di
 # Lesser General Public License Version 3 or the Perl Artistic License
 # Version 2.0.
 
-scenarios(vlt_all => 1);
-
-if ($ENV{TRAVIS} && $ENV{TRAVIS} eq 'true') {
-    skip("Fails occasionally on Travis-CI");
-} else {
-    compile(
-        make_top_shell => 0,
-        make_main => 0,
-        verilator_flags2 => ["--exe $Self->{t_dir}/$Self->{name}.cpp"],
-        );
-
-    execute(
-        check_finished => 1,
-        );
-
-    ok(1);
+if ($Self->{vltmt} && exists $ENV{TRAVIS_DIST} &&
+    $ENV{TRAVIS_DIST} eq "trusty")
+{
+    skip("Multithreaded test does not work under Travis w/ Ubuntu Trusty");
 }
 
+scenarios(vlt_all => 1);
+
+compile(
+    make_top_shell => 0,
+    make_main => 0,
+    verilator_flags2 => ["--exe $Self->{t_dir}/$Self->{name}.cpp"],
+    );
+
+execute(
+    check_finished => 1,
+    );
+
+ok(1);
 1;
