verilator/verilator

commit f133c4d0b35e1338cb12bc3fae0cbdd4488fae82

Fix mis-indenting AstComments with C++ symbols.

src/V3File.cpp
@@ -704,6 +704,15 @@ void V3OutFormatter::puts(const char *strg) {
         case '\t':
             wordstart = true;
             break;
+        case '/':
+            if (m_lang==LA_C || m_lang==LA_VERILOG) {
+                if (cp>strg && cp[-1]=='/') {
+                    // Output ignoring contents to EOL
+                    cp++;
+                    while (*cp && cp[1] && cp[1] != '\n') putcNoTracking(*cp++);
+                }
+            }
+            break;
         case '{':
             if (m_lang==LA_C && (equalsForBracket || m_bracketLevel)) {
                 // Break up large code inside "= { ..."

commit 1cf29c4d2085183113e2c80f03b668c5391a54b2

Fix truncation of comments, broke in f133c4d0b35e1338cb12bc3fae0cbdd4488fae82.

src/V3File.cpp
@@ -693,6 +693,7 @@ void V3OutFormatter::puts(const char *strg) {
                     // Output ignoring contents to EOL
                     cp++;
                     while (*cp && cp[1] && cp[1] != '\n') putcNoTracking(*cp++);
+                    if (*cp) putcNoTracking(*cp);
                 }
             }
             break;