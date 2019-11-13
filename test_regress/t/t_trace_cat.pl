a250decfaa0966fe45d787ef4a094b83a1aef8b3
Sebastien Van Cauwenberghe <svancau@gmail.com>
2019-11-13 06:43:15 -0500

    Tests: Fix t_trace_cat when non-English sort.

--- test_regress/t/t_trace_cat.pl
+++ test_regress/t/t_trace_cat.pl
@@ -19,7 +19,8 @@ execute(
     check_finished => 1,
     );
 
-system("cat $Self->{obj_dir}/simpart*.vcd > $Self->{obj_dir}/simall.vcd");
+system("cat $Self->{obj_dir}/simpart_0000.vcd "
+       ." $Self->{obj_dir}/simpart_0000_cat*.vcd > $Self->{obj_dir}/simall.vcd");
 
 vcd_identical("$Self->{obj_dir}/simall.vcd",
               $Self->{golden_filename});
