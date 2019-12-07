a59777aa7529981e9de7827116602dbbe41ea1e7
Driss Hafdi <driss@hudson-trading.com>
2019-12-07 13:15:46 -0500

    Tests: Unsupported test for bug1624.
    Signed-off-by: Wilson Snyder <wsnyder@wsnyder.org>

---
+++ test_regress/t/t_param_width_loc_bad.v
@@ -0,0 +1,25 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed into the Public Domain, for any use,
+// without warranty, 2019 by Driss Hafdi.
+
+module t (/*AUTOARG*/
+   // Inputs
+   clk
+   );
+
+   input clk;
+
+   test #(.param(32'd0)) test_i;
+
+   initial begin
+      $write("*-* All Finished *-*\n");
+      $finish;
+   end
+endmodule
+
+module test
+  #(
+    parameter logic param = 1'b0
+    ) ();
+endmodule
