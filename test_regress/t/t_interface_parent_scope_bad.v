d82b9128d082a629da2d6f5ee838cd4cd3581148
Driss Hafdi <driss@hudson-trading.com>
2019-12-07 12:53:01 -0500

    Tests: Unsupported test for bug1623.
    Signed-off-by: Wilson Snyder <wsnyder@wsnyder.org>

---
+++ test_regress/t/t_interface_parent_scope_bad.v
@@ -0,0 +1,32 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed into the Public Domain, for any use,
+// without warranty, 2019 by Driss Hafdi
+
+interface Foo();
+   logic quux;
+endinterface
+
+module Bar();
+   always_comb foo.quux = '0;
+endmodule
+
+module Baz();
+   Foo foo();
+   Bar bar();
+endmodule
+
+module t (/*AUTOARG*/
+   // Inputs
+   clk
+   );
+
+   input clk;
+
+   Baz baz();
+
+   initial begin
+      $write("*-* All Finished *-*\n");
+      $finish;
+   end
+endmodule
