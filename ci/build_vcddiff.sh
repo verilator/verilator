9cf5acc43e5d93ae0f933a9dc6e393ff97cd7f1e
Todd Strader <todd.strader@gmail.com>
2019-12-09 05:55:18 -0500

    Install vcddiff for Travis, bug1600.

---
+++ ci/build_vcddiff.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# DESCRIPTION: Verilator: Build script for vcddiff
+#
+# Copyright 2019 by Todd Strader. This program is free software; you can
+# redistribute it and/or modify it under the terms of either the GNU
+# Lesser General Public License Version 3 or the Perl Artistic License
+# Version 2.0.
+set -e
+
+# NB: it would be better to add this via a PPA
+
+TMP_DIR=$(mktemp -d)
+
+git -C ${TMP_DIR} clone https://github.com/veripool/vcddiff
+VCDDIFF_DIR=${TMP_DIR}/vcddiff
+git -C ${VCDDIFF_DIR} checkout 5112f88b7ba8818dce9dfb72619e64a1fc19542c
+make -C ${VCDDIFF_DIR}
+sudo cp ${VCDDIFF_DIR}/vcddiff /usr/local/bin
