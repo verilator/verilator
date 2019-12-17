f514049c0462b497ff105ebd1d1ad0d88f2d2c79
Wilson Snyder <wsnyder@wsnyder.org>
2019-12-16 21:54:20 -0500

    Add cleaner error on version control conflicts in sources.

---
+++ test_regress/t/t_lint_vcmarker_bad.v
@@ -0,0 +1,14 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed into the Public Domain, for any use,
+// without warranty, 2019 by Wilson Snyder.
+
+module t (/*AUTOARG*/);
+
+<<<<<<< HEAD  // Intentional test: This conflict marker should be here
+   initial $display("Hello");
+=======   // Intentional test: This conflict marker should be here
+   initial $display("Goodbye");
+>>>>>>> MERGE  // Intentional test: This conflict marker should be here
+
+endmodule
