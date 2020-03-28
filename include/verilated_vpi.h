510be53521f58575d0b414ba69c8c23d4249cf85
Matthew Ballance <matt.ballance@gmail.com>
2020-03-28 13:47:21 -0400

    Expose VPI cbNextDeadline via the public API (#2212)
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- include/verilated_vpi.h
+++ include/verilated_vpi.h
@@ -44,6 +44,9 @@ public:
     /// Call callbacks of arbitrary types
     /// Users can call this from their application code
     static bool callCbs(vluint32_t reason) VL_MT_UNSAFE_ONE;
+    /// Returns time of the next registered VPI callback, or
+    /// ~(0) if none are registered
+    static QData cbNextDeadline() VL_MT_UNSAFE_ONE;
     /// Self test, for internal use only
     static void selfTest() VL_MT_UNSAFE_ONE;
 };
