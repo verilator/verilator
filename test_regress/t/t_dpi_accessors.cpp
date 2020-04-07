08cfcfd9e71319f0daad2c2422d9ad71431f18ec
Geza Lore <gezalore@gmail.com>
2020-04-07 21:37:04 +0100

    Use C++ style cast int test.

--- test_regress/t/t_dpi_accessors.cpp
+++ test_regress/t/t_dpi_accessors.cpp
@@ -246,7 +246,7 @@ int main() {
     for (int i = 0; !Verilated::gotFinish() && (i < 4); i++) {
         dut->clk = 1 - dut->clk;
         a = 1 - (int)a_read();
-        a_write((const svBitVecVal*)&a);
+        a_write(reinterpret_cast<const svBitVecVal*>(&a));
         logReg(dut->clk, "write a", a, " (before clk)");
         a = a_read();
         logReg(dut->clk, "read  a", a, " (before clk)");
@@ -275,7 +275,7 @@ int main() {
     for (int i = 0; !Verilated::gotFinish() && (i < 4); i++) {
         dut->clk = 1 - dut->clk;
         b = (int)b_read() - 1;
-        b_write((const svBitVecVal*)&b);
+        b_write(reinterpret_cast<const svBitVecVal*>(&b));
         logRegHex(dut->clk, "write b", 8, b, " (before clk)");
         b = (int)b_read();
         logRegHex(dut->clk, "read  b", 8, b, " (before clk)");
@@ -304,7 +304,7 @@ int main() {
     for (int i = 0; !Verilated::gotFinish() && (i < 4); i++) {
         dut->clk = 1 - dut->clk;
         mem32 = (int)mem32_read() - 1;
-        mem32_write((const svBitVecVal*)&mem32);
+        mem32_write(reinterpret_cast<const svBitVecVal*>(&mem32));
         logRegHex(dut->clk, "write mem32", 8, mem32, " (before clk)");
         mem32 = (int)mem32_read();
         logRegHex(dut->clk, "read  mem32", 8, mem32, " (before clk)");
@@ -426,7 +426,7 @@ int main() {
         logRegHex(dut->clk, "read  b [3:0]", 4, b_slice, " (before write)");
 
         b_slice--;
-        b_slice_write((const svBitVecVal*)&b_slice);
+        b_slice_write(reinterpret_cast<const svBitVecVal*>(&b_slice));
         logRegHex(dut->clk, "write b [3:0]", 4, b_slice, " (before clk)");
 
         int b_after = (int)b_read();
@@ -465,7 +465,7 @@ int main() {
         logRegHex(dut->clk, "read  mem32 [7:6,2:0]", 5, mem32_slice, " (before write)");
 
         mem32_slice--;
-        mem32_slice_write((const svBitVecVal*)&mem32_slice);
+        mem32_slice_write(reinterpret_cast<const svBitVecVal*>(&mem32_slice));
         logRegHex(dut->clk, "write mem32 [7:6,2:0]", 5, mem32_slice, " (before clk)");
 
         int mem32_after = (int)mem32_read();
@@ -545,8 +545,8 @@ int main() {
 
         e = 0x05 | (i << 4);
         f = 0xa0 | i;
-        e_write((const svBitVecVal*)&e);
-        f_write((const svBitVecVal*)&f);
+        e_write(reinterpret_cast<const svBitVecVal*>(&e));
+        f_write(reinterpret_cast<const svBitVecVal*>(&f));
 
         e = (int)e_read();
         f = (int)f_read();
@@ -592,7 +592,7 @@ int main() {
         logRegHex(dut->clk, "read  e    ", 8, e, " (before write)");
 
         int l1 = 0x5a5a;
-        l1_write((const svBitVecVal*)&l1);
+        l1_write(reinterpret_cast<const svBitVecVal*>(&l1));
         logRegHex(dut->clk, "write l1   ", 15, l1, " (before clk)");
 
         int b_after = (int)b_read();
@@ -641,7 +641,7 @@ int main() {
         logRegHex(dut->clk, "read  f ", 8, f, " (before write)");
 
         int l2 = 0xa5 + i;
-        l2_write((const svBitVecVal*)&l2);
+        l2_write(reinterpret_cast<const svBitVecVal*>(&l2));
         logRegHex(dut->clk, "write l2", 8, l2, " (before clk)");
 
         int e_after = (int)e_read();
