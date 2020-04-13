191a67a7c0514dd58a9d2ef610d5a303f512598c
Yutetsu TAKATSUKASA <y.takatsukasa@gmail.com>
2020-04-13 22:26:23 +0900

    Revert "delete file object to flush its contents."
    ea3acc2d3ad4f024deb8fcee8baee9554adf0e3c covers this commit.
    
    This reverts commit ffcab8bf6fa30f062e49596c2d97a7881258d281.

--- src/V3EmitCSyms.cpp
+++ src/V3EmitCSyms.cpp
@@ -354,7 +354,6 @@ public:
         m_ofpBase = NULL;
         iterate(nodep);
     }
-    virtual ~EmitCSyms() VL_OVERRIDE { VL_DO_DANGLING(delete m_ofpBase, m_ofpBase); }
 };
 
 void EmitCSyms::emitSymHdr() {
