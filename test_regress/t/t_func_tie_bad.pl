a88b7890bb952fcc40111540e55be267a38ea35e
Tim Snyder <snyder.tim@gmail.com>
2020-05-04 15:02:39 -0500

    Use /usr/bin/env perl in lieu of /usr/bin/perl
    Enables scripts to work where perl is not installed at /usr/bin/perl

--- test_regress/t/t_func_tie_bad.pl
+++ test_regress/t/t_func_tie_bad.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 if (!$::Driver) { use FindBin; exec("$FindBin::Bin/bootstrap.pl", @ARGV, $0); die; }
 # DESCRIPTION: Verilator: Verilog Test driver/expect definition
 #
