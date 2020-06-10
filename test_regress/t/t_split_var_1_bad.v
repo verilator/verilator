54b9ddbf76773b07326a32b9920cffc9f2bbd4be
Yutetsu TAKATSUKASA <y.takatsukasa@gmail.com>
2020-06-10 10:33:10 +0900

    improve code coverage of SplitVar

--- test_regress/t/t_split_var_1_bad.v
+++ test_regress/t/t_split_var_1_bad.v
@@ -60,9 +60,11 @@ endmodule
 
 module sub1(input [3:0]addr, output logic [7:0] rd_data);
    genvar cannot_split_genvar /*verilator split_var*/;
-   logic [15:0] [7:0] cannot_split  /*verilator split_var*/;
-   always_comb
-      rd_data = cannot_split[addr];
+   logic [15:0] [8:0] cannot_split  /*verilator split_var*/;
+   always_comb begin
+      logic [8:0] rd_tmp /*verilator split_var*/ = cannot_split[addr];
+      rd_data = rd_tmp[{3'b0, addr[0]}+:8];
+   end
 
 endmodule
 
