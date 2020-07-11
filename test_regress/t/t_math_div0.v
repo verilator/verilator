7e7447812cec4c34d2a027e3038bdf2763e846e2
Wilson Snyder <wsnyder@wsnyder.org>
2020-07-11 08:17:16 -0400

    Fix modulus exception (#2460)

--- test_regress/t/t_math_div0.v
+++ test_regress/t/t_math_div0.v
@@ -5,20 +5,24 @@
 
 module t(/*AUTOARG*/
    // Outputs
-   y, y2, y3
+   y, d2, m2, d3, m3
    );
    output [3:0] y;
-   output [31:0] y2;
-   output [63:0] y3;
+   output [31:0] d2;
+   output [31:0] m2;
+   output [63:0] d3;
+   output [63:0] m3;
    // bug775
    // verilator lint_off WIDTH
    assign y = ((0/0) ? 1 : 2) % 0;
 
    // bug2460
    reg [31:0]    b;
-   assign y2 = $signed(32'h80000000) / $signed(b);
+   assign d2 = $signed(32'h80000000) / $signed(b);
+   assign m2 = $signed(32'h80000000) % $signed(b);
    reg [63:0]    b3;
-   assign y3 = $signed(64'h80000000_00000000) / $signed(b3);
+   assign d3 = $signed(64'h80000000_00000000) / $signed(b3);
+   assign m3 = $signed(64'h80000000_00000000) % $signed(b3);
 
    initial begin
       b = 32'hffffffff;
