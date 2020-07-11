af3ab13012f483a2ae26d58ce67294253a8b037e
Wilson Snyder <wsnyder@wsnyder.org>
2020-07-10 22:32:35 -0400

    Tests: Fix 64-bit code coverage, part of last commit

--- test_regress/t/t_math_div0.v
+++ test_regress/t/t_math_div0.v
@@ -3,9 +3,13 @@
 // any use, without warranty, 2020 by Wilson Snyder.
 // SPDX-License-Identifier: CC0-1.0
 
-module t(y, y2);
+module t(/*AUTOARG*/
+   // Outputs
+   y, y2, y3
+   );
    output [3:0] y;
    output [31:0] y2;
+   output [63:0] y3;
    // bug775
    // verilator lint_off WIDTH
    assign y = ((0/0) ? 1 : 2) % 0;
@@ -13,9 +17,12 @@ module t(y, y2);
    // bug2460
    reg [31:0]    b;
    assign y2 = $signed(32'h80000000) / $signed(b);
+   reg [63:0]    b3;
+   assign y3 = $signed(64'h80000000_00000000) / $signed(b3);
 
    initial begin
       b = 32'hffffffff;
+      b3 = 64'hffffffff_ffffffff;
       $write("*-* All Finished *-*\n");
       $finish;
    end
