6a9f358382e20801355dd0df8227298f936d2bae
Tomasz Gorochowik <tgorochowik@gmail.com>
2020-07-23 12:41:54 +0200

    Add rudimentary class definitions

--- manual_process_work/vbuild/Vtop__Slow.cpp
+++ manual_process_work/vbuild/Vtop__Slow.cpp
@@ -5,6 +5,49 @@
 #include "Vtop.h"
 #include "Vtop__Syms.h"
 
+#include <functional>
+#include <vector>
+#include <set>
+
+//==========
+
+
+class Event
+{
+    public:
+        bool triggered = false;
+};
+
+
+class Process
+{
+    public:
+        std::vector <std::function<bool()>> run_conditions; // waits for arbitrary conditions
+        std::vector <std::function<void()>> eval_steps; // unbreakable steps to eval
+        unsigned int current_step = 0;
+};
+
+
+class Timer
+{
+    public:
+        unsigned long simulation_time;
+
+        std::set<unsigned long> requested_slots;
+
+        void advance_time() {
+            if (requested_slots.size() == 0) {
+                VL_DBG_MSGF("+    No next time slot requested\n");
+            }
+
+            auto next_time = requested_slots.begin();
+            simulation_time = *next_time;
+            requested_slots.erase(next_time);
+
+            VL_DBG_MSGF("+    Simulation time advanced to %lu\n", simulation_time);
+        }
+};
+
 //==========
 
 VL_CTOR_IMP(Vtop) {
