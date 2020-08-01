37428b25de7f2c323e919b2bd3c930516f47e3da
Yutetsu TAKATSUKASA <y.takatsukasa@gmail.com>
2020-08-01 10:38:49 +0900

    Update test_regress/t/t_hier_block_sc.pl
    Co-authored-by: Wilson Snyder <wsnyder@wsnyder.org>

--- test_regress/t/t_hier_block_sc.pl
+++ test_regress/t/t_hier_block_sc.pl
@@ -15,7 +15,7 @@ top_filename("t/t_hier_block.v");
 # Travis environment offers 2 VCPUs, 2 thread setting causes the following warning.
 # %Warning-UNOPTTHREADS: Thread scheduler is unable to provide requested parallelism; consider asking for fewer threads.
 # So use 6 threads here though it's not optimal in performace wise, but ok.
-scenarios(simulator => 1);
+scenarios(vlt_all => 1);
 
 compile(
     v_flags2 => ['t/t_hier_block.cpp'],
