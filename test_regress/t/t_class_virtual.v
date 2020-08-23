fefe731105e9c394a989c6105d501ba7733f3b24
Wilson Snyder <wsnyder@wsnyder.org>
2020-08-23 11:50:42 -0400

    Tests: Better virtual test

--- test_regress/t/t_class_virtual.v
+++ test_regress/t/t_class_virtual.v
@@ -4,5 +4,36 @@
 // any use, without warranty, 2019 by Wilson Snyder.
 // SPDX-License-Identifier: CC0-1.0
 
-virtual class VC;
+virtual class VBase;
+   virtual function int hello;
+      return 1;
+   endfunction
 endclass
+
+`ifndef VERILATOR
+virtual class VA extends VBase;
+   virtual function int hello;
+      return 2;
+   endfunction
+endclass
+
+virtual class VB extends VBase;
+   virtual function int hello;
+      return 3;
+   endfunction
+endclass
+
+module t;
+   initial begin
+      VA va = new;
+      VB vb = new;
+      VBase b;
+      b = va;
+      if (b.hello() != 2) $stop;
+      b = vb;
+      if (b.hello() != 3) $stop;
+      $write("*-* All Finished *-*\n");
+      $finish;
+   end
+endmodule
+`endif
