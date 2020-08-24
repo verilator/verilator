6acd5847e70e5b15383a042ae0f9c65a1c4b486d
Yutetsu TAKATSUKASA <y.takatsukasa@gmail.com>
2020-08-25 08:11:20 +0900

    Fix range check in V3SplitVar to be consistent with #2507 (#2511)
    * test:Add more tests for checking split_var for unpacked array.
    
    * Fix range calculation of SliceSel and change to UASSERT_OBJ because the range check is done in V3Width beforehand.

--- test_regress/t/t_split_var_2_trace.pl
+++ test_regress/t/t_split_var_2_trace.pl
@@ -25,7 +25,7 @@ execute(
 
 vcd_identical("$Self->{obj_dir}/simx.vcd", $Self->{golden_filename});
 file_grep($Self->{stats}, qr/SplitVar,\s+Split packed variables\s+(\d+)/i, 12);
-file_grep($Self->{stats}, qr/SplitVar,\s+Split unpacked arrays\s+(\d+)/i, 23);
+file_grep($Self->{stats}, qr/SplitVar,\s+Split unpacked arrays\s+(\d+)/i, 27);
 
 ok(1);
 1;
