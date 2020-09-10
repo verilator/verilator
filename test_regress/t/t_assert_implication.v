1e1a06b8501184ef84770492882959e8365f7c27
Peter Monsson <peter@monsson.dk>
2020-09-10 09:46:31 +0200

    Expanding unit testing to handle all conditions

--- test_regress/t/t_assert_implication.v
+++ test_regress/t/t_assert_implication.v
@@ -20,7 +20,9 @@ module t (/*AUTOARG*/
    always @ (posedge clk) begin
       if (cyc!=0) begin
          cyc <= cyc + 1;
+`ifdef TEST_VERBOSE
          $display("cyc=%0d", cyc);
+`endif
          if (cyc==10) begin
             $write("*-* All Finished *-*\n");
             $finish;
@@ -41,8 +43,35 @@ module Test
      @(posedge clk)
      1 |-> 0
    ) else $display("[%0t] wrong implication", $time);
+
+   assert property (
+     @(posedge clk)
+     1 |=> 0
+   ) else $display("[%0t] wrong implication", $time);
+
+   assert property (
+     @(posedge clk)
+     cyc%3==1 |=> cyc%3==1
+   ) else $display("[%0t] wrong implication (step)", $time);
+
+   assert property (
+     @(posedge clk)
+     cyc%3==1 |=> cyc%3==0
+   ) else $display("[%0t] wrong implication (step)", $time);
+
+   assert property (
+     @(posedge clk) disable iff (cyc == 3)
+     (cyc == 4) |=> 0
+   ) else $display("[%0t] wrong implication (disable)", $time);
+
+   assert property (
+     @(posedge clk) disable iff (cyc == 6)
+     (cyc == 4) |=> 0
+   ) else $display("[%0t] wrong implication (disable)", $time);
+
 `endif
 
+   // Test |->
    assert property (
      @(posedge clk)
      1 |-> 1
@@ -58,17 +87,44 @@ module Test
      0 |-> 1
    );
 
+   // Test |=>
+   assert property (
+     @(posedge clk)
+     1 |=> 1
+   );
+
+   assert property (
+     @(posedge clk)
+     0 |=> 0
+   );
+
+   assert property (
+     @(posedge clk)
+     0 |=> 1
+   );
+
    // Test correct handling of time step in |=>
    assert property (
      @(posedge clk)
-     cyc > 2 |=> cyc > 3
+     cyc%3==1 |=> cyc%3==2
    );
 
-   // Test correct handling of disable iff in |=>
+   // Test correct handling of disable iff
    assert property (
      @(posedge clk) disable iff (cyc < 3)
      1 |=> cyc > 3
    );
 
+   // Test correct handling of disable iff in current cycle
+   assert property (
+     @(posedge clk) disable iff (cyc == 4)
+     (cyc == 4) |=> 0
+   );
+
+   // Test correct handling of disable iff in previous cycle
+   assert property (
+     @(posedge clk) disable iff (cyc == 5)
+     (cyc == 4) |=> 0
+   );
 
 endmodule
