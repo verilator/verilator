d20517f2e9f8e5ca618701951280fa4aeeab434d
Yutetsu TAKATSUKASA <y.takatsukasa@gmail.com>
2020-10-23 23:02:03 +0900

    Add a test to use string for $fgets

--- test_regress/t/t_sys_file_basic.v
+++ test_regress/t/t_sys_file_basic.v
@@ -134,6 +134,12 @@ module t;
 	 if (chars != 10) $stop;
 	 if (letterw != "\0\0\0\0\0\0widestuff\n") $stop;
 
+	 s = "";
+	 chars = $fgets(s, file);
+	 if (`verbose) $write("c=%0d w=%s", chars, s); // Output includes newline
+	 if (chars != 7) $stop;
+	 if (s != "string\n") $stop;
+
 	 // $sscanf
 	 if ($sscanf("x","")!=0) $stop;
 	 if ($sscanf("z","z")!=0) $stop;
