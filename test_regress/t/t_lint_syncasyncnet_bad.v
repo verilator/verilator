ffbae97a3d2bf553f673645a82b34284e559abc9
Wilson Snyder <wsnyder@wsnyder.org>
2020-11-25 21:42:47 -0500

    Tests: Make t_lint_syncasyncnet_bad etc tolerate -Oi.

--- test_regress/t/t_lint_syncasyncnet_bad.v
+++ test_regress/t/t_lint_syncasyncnet_bad.v
@@ -6,40 +6,15 @@
 
 module t (/*AUTOARG*/
    // Inputs
-   rst_sync_l, rst_both_l, rst_async_l, d, clk
+   clk, rst_both_l, rst_sync_l, rst_async_l, d
    );
    /*AUTOINPUT*/
-   // Beginning of automatic inputs (from unused autoinst inputs)
-   input                clk;                    // To sub1 of sub1.v, ...
-   input                d;                      // To sub1 of sub1.v, ...
-   input                rst_async_l;            // To sub2 of sub2.v
-   input                rst_both_l;             // To sub1 of sub1.v, ...
-   input                rst_sync_l;             // To sub1 of sub1.v
-   // End of automatics
-
-   sub1 sub1 (/*AUTOINST*/
-              // Inputs
-              .clk                      (clk),
-              .rst_both_l               (rst_both_l),
-              .rst_sync_l               (rst_sync_l),
-              .d                        (d));
-   sub2 sub2 (/*AUTOINST*/
-              // Inputs
-              .clk                      (clk),
-              .rst_both_l               (rst_both_l),
-              .rst_async_l              (rst_async_l),
-              .d                        (d));
-endmodule
-
-module sub1 (/*AUTOARG*/
-   // Inputs
-   clk, rst_both_l, rst_sync_l, d
-   );
 
    input clk;
    input rst_both_l;
    input rst_sync_l;
-   //input rst_async_l;
+   input rst_async_l;
+
    input d;
    reg q1;
    reg q2;
@@ -60,40 +35,27 @@ module sub1 (/*AUTOARG*/
       if (0 && q1 && q2) ;
    end
 
-endmodule
-
-module sub2 (/*AUTOARG*/
-   // Inputs
-   clk, rst_both_l, rst_async_l, d
-   );
-
-   input clk;
-   input rst_both_l;
-   //input rst_sync_l;
-   input rst_async_l;
-   input d;
-   reg   q1;
-   reg   q2;
    reg   q3;
-
    always @(posedge clk or negedge rst_async_l) begin
       if (~rst_async_l) begin
          /*AUTORESET*/
          // Beginning of autoreset for uninitialized flops
-         q1 <= 1'h0;
+         q3 <= 1'h0;
          // End of automatics
       end else begin
-         q1 <= d;
+         q3 <= d;
       end
    end
 
+   reg q4;
    always @(posedge clk or negedge rst_both_l) begin
-      q2 <= (~rst_both_l) ? 1'b0 : d;
+      q4 <= (~rst_both_l) ? 1'b0 : d;
    end
    // Make there be more async uses than sync uses
+   reg q5;
    always @(posedge clk or negedge rst_both_l) begin
-      q3 <= (~rst_both_l) ? 1'b0 : d;
-      if (0 && q1 && q2 && q3) ;
+      q5 <= (~rst_both_l) ? 1'b0 : d;
+      if (0 && q3 && q4 && q5) ;
    end
 
 endmodule
