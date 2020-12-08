c23de458ed6cdbd8d0af3f9efed94d3f0d393a02
Wilson Snyder <wsnyder@wsnyder.org>
2020-12-08 08:40:22 -0500

    Misc internal coverage cleanups

--- test_regress/t/t_flag_stats.v
+++ test_regress/t/t_flag_stats.v
@@ -4,10 +4,12 @@
 // any use, without warranty, 2014 by Wilson Snyder.
 // SPDX-License-Identifier: CC0-1.0
 
-module t (b);
+module t (b, b2);
    output reg [31:0] b;
+   output reg [31:0] b2;  // Need 2 vars of same width to cover V3Stats
    initial begin
-      b = 22;
+      b = 11;
+      b2 = 22;
       $write("*-* All Finished *-*\n");
       $finish;
    end
