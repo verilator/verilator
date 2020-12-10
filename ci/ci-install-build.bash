2273fa62d94a1042524e52ec7b85a4533272fffa
Wilson Snyder <wsnyder@wsnyder.org>
2020-12-09 22:00:30 -0500

    CI: coverage action parallelism

--- ci/ci-install-build.bash
+++ ci/ci-install-build.bash
@@ -8,9 +8,17 @@
 #
 # SPDX-License-Identifier: LGPL-3.0-only OR Artistic-2.0
 
+echo "::group::build's ci-install"
 export CI_BUILD_STAGE_NAME=build
 bash ci/ci-install.bash
+echo "::endgroup"
+
+echo "::group::build's ci-script"
 mkdir -p $CCACHE_DIR && bash ci/ci-ccache-maint.bash
 bash ci/ci-script.bash
+echo "::endgroup"
+
+echo "::group::test's ci-install"
 export CI_BUILD_STAGE_NAME=test
 bash ci/ci-install.bash
+echo "::endgroup"
