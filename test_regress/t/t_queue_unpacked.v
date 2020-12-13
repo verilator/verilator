6061744c4d1d39fd76b072348e2c85ab3553a6f7
Wilson Snyder <wsnyder@wsnyder.org>
2020-12-12 20:56:35 -0500

    Tests: Add t_dynarray_unpacked (#2314)

--- test_regress/t/t_queue_unpacked.v
+++ test_regress/t/t_queue_unpacked.v
@@ -7,15 +7,7 @@
 `define stop $stop
 `define checks(gotv,expv) do if ((gotv) !== (expv)) begin $write("%%Error: %s:%0d:  got='%s' exp='%s'\n", `__FILE__,`__LINE__, (gotv), (expv)); `stop; end while(0);
 
-module t (/*AUTOARG*/
-   // Inputs
-   clk
-   );
-   input clk;
-
-   integer cyc=0;
-
-   integer i;
+module t (/*AUTOARG*/);
 
    typedef string sarray_t[2];
    typedef sarray_t q_sarray_t[$];
