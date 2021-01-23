64941538b76941f2ddab318a726d996b98776592
Wilson Snyder <wsnyder@wsnyder.org>
2021-01-23 10:31:27 -0500

    Fix TIMESCALE warnings on primitives (#2763).

--- src/V3LinkLevel.cpp
+++ src/V3LinkLevel.cpp
@@ -103,10 +103,12 @@ void V3LinkLevel::timescaling(const ModVec& mods) {
     for (AstNodeModule* nodep : mods) {
         if (nodep->timeunit().isNone()) {
             if (modTimedp && !VN_IS(nodep, Iface)
+                && !VN_IS(nodep, Primitive)
                 && !(VN_IS(nodep, Package) && VN_CAST(nodep, Package)->isDollarUnit())) {
                 nodep->v3warn(TIMESCALEMOD,
                               "Timescale missing on this module as other modules have "
                               "it (IEEE 1800-2017 3.14.2.2)\n"
+                                  << nodep->warnContextPrimary() << '\n'
                                   << modTimedp->warnOther()
                                   << "... Location of module with timescale\n"
                                   << modTimedp->warnContextSecondary());
