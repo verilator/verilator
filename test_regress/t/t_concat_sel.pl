d559190e9ecbad96229ca962ba5e0293ea086769
Wilson Snyder <wsnyder@wsnyder.org>
2021-02-17 22:57:52 -0500

    Tests for future concat select (#2721)

--- test_regress/t/t_concat_sel.pl
+++ test_regress/t/t_concat_sel.pl
@@ -8,11 +8,16 @@ if (!$::Driver) { use FindBin; exec("$FindBin::Bin/bootstrap.pl", @ARGV, $0); di
 # Version 2.0.
 # SPDX-License-Identifier: LGPL-3.0-only OR Artistic-2.0
 
-scenarios(linter => 1);
+scenarios(simulator => 1);
 
-lint(
-    fails => 1,
+compile(
+    fails => $Self->{vlt_all},
+    expect_filename => $Self->{golden_filename},
     );
+
+execute(
+    check_finished => 1,
+    ) if !$Self->{vlt_all};
 
 ok(1);
 1;
