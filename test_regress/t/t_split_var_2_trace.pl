4f36e3e6c9c651af801f1ba7147a3cd89fef93b7
Geza Lore <gezalore@gmail.com>
2021-04-03 17:57:06 +0100

    Fix incorrect condition in varNotReferenced (#2873)
    The intention was to not merge impure assignments, but the actual
    predicate failed if the assignment was indeed pure.
    
    This fix gains 1.5% speed on SweRV EH1.

--- test_regress/t/t_split_var_2_trace.pl
+++ test_regress/t/t_split_var_2_trace.pl
@@ -24,7 +24,7 @@ execute(
     );
 
 vcd_identical("$Self->{obj_dir}/simx.vcd", $Self->{golden_filename});
-file_grep($Self->{stats}, qr/SplitVar,\s+Split packed variables\s+(\d+)/i, 12);
+file_grep($Self->{stats}, qr/SplitVar,\s+Split packed variables\s+(\d+)/i, 10);
 file_grep($Self->{stats}, qr/SplitVar,\s+Split unpacked arrays\s+(\d+)/i, 27);
 
 ok(1);
