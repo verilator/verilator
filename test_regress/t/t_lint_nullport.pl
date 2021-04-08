921c81bcfad3d6bc1d5d75df151db8603bd030e4
Udi Finkelstein <github@udifink.com>
2021-04-08 23:11:22 +0300

    Added expected output log and fixed test script.

--- test_regress/t/t_lint_nullport.pl
+++ test_regress/t/t_lint_nullport.pl
@@ -11,7 +11,8 @@ if (!$::Driver) { use FindBin; exec("$FindBin::Bin/bootstrap.pl", @ARGV, $0); di
 scenarios(linter => 1);
 
 lint(
-	#    expect_filename => $Self->{golden_filename},
+    fails => 1,
+    expect_filename => $Self->{golden_filename},
     );
 
 ok(1);
