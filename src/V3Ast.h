69ab1b9f752b068a1ef1db3ca5beb441f2c53207
Geza Lore <gezalore@gmail.com>
2021-06-17 18:49:45 +0100

    Localize variables used in multiple functions
    Teach V3Localize how to localize variables that are used in multiple
    functions, if in all functions where they are used, they are always
    written in whole before being consumed. This allows a lot more variables
    to be localized (+20k variables on OpenTitan - when building without
    --trace), and can cause significant performance improvement (OpenTitan
    simulates 8.5% - build single threaded and withuot --trace).

--- src/V3Ast.h
+++ src/V3Ast.h
@@ -156,6 +156,7 @@ public:
         return (m_e == READWRITE) ? VAccess(m_e) : (m_e == WRITE ? VAccess(READ) : VAccess(WRITE));
     }
     bool isReadOnly() const { return m_e == READ; }  // False with READWRITE
+    bool isWriteOnly() const { return m_e == WRITE; }  // False with READWRITE
     bool isReadOrRW() const { return m_e == READ || m_e == READWRITE; }
     bool isWriteOrRW() const { return m_e == WRITE || m_e == READWRITE; }
     bool isRW() const { return m_e == READWRITE; }
