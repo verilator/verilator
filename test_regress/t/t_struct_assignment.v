a1b2d0b61bdf175b2074aeb782111389b3abf3dc
Aj0SK <andrejkorman@gmail.com>
2021-06-23 23:46:21 +0200

    Tests: Basic test for typed struct assignment

--- test_regress/t/t_struct_assignment.v
+++ test_regress/t/t_struct_assignment.v
@@ -1,46 +1,35 @@
-// DESCRIPTION: Verilator: Verilog Test module
-//
-// Use this file as a template for submitting bugs, etc.
-// This module takes a single clock input, and should either
-//      $write("*-* All Finished *-*\n");
-//      $finish;
-// on success, or $stop.
-//
-// The code as shown applies a random vector to the Test
-// module, then calculates a CRC on the Test module's outputs.
-//
-// **If you do not wish for your code to be released to the public
-// please note it here, otherwise:**
+// DESCRIPTION: Verilator: Verilog Test module for specialized type default values
 //
 // This file ONLY is placed under the Creative Commons Public Domain, for
-// any use, without warranty, 2021 by ____YOUR_NAME_HERE____.
+// any use, without warranty, 2021 by Andrej Korman.
 // SPDX-License-Identifier: CC0-1.0
 
 /* verilator lint_off UNPACKED */
 
 module t;
-   typedef struct packed { // [3:0]
-      bit       b3;
-      bit       b2;
-      bit       b1;
-      bit       b0;
-   } b4_t;
    typedef struct packed {
-      bit x;
-      b4_t y;
-   } type1;
-      typedef struct {
-      int a;
-      real b;
-      int c;
-      int d;
-   } type2;
+      bit       a;
+      bit       b;
+      bit       c;
+      bit       d;
+   } helper_type1;
+   typedef struct packed {
+      bit       a;
+      int       b;
+   } helper_type2;
 
-   const b4_t helper = '{1'b1, 1'b1, 1'b0, 1'b1};
-   const type1 ex1 = '{x: 1'b1, y: helper};
+   const helper_type1 ex1 = '{default: 1'b0, bit: 1'b1};
+   const helper_type1 ex2 = '{bit: 1'b1, bit: 1'b0};
+   const helper_type2 ex3 = '{default: 1'b1, int: 20};
+   const helper_type2 ex4 = '{bit: 1'b1, int: 20};
+   const helper_type1 ex5 = '{a: 1'b1, b: 1'b0, bit: 1'b1};
 
    initial begin
-      if (ex1 != 5'b11101) $stop;
+      if (ex1 != 4'b1111) $stop;
+      if (ex2 != 4'b1111) $stop;
+      if (ex3.b != 20) $stop;
+      if (ex4.a != 1'b1 || ex4.b != 20) $stop;
+      if (ex5 != 4'b1011) $stop;
       $write("*-* All Finished *-*\n");
       $finish;
    end
