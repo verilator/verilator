c5c5f11e1641bbfb28a7a8dfb1f426e92d100b70
Dan Petrisko <petrisko@cs.washington.edu>
2021-07-07 11:00:17 -0700

    Tests: Adding failing test case for source synchronous signals (#3038)

---
+++ test_regress/t/t_source_sync.v
@@ -0,0 +1,12 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain, for
+// any use, without warranty, 2020 by Dan Petrisko.
+// SPDX-License-Identifier: CC0-1.0
+
+typedef struct packed {
+   logic clk /*verilator clocker*/;
+   logic data;
+} ss_s;
+
+endmodule
