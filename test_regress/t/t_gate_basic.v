c6d09124470d43186f63455013557e4a611dfa51
Wilson Snyder <wsnyder@wsnyder.org>
2021-07-10 07:11:50 -0400

    Tests: Fix coverage holes. No functional change.

--- test_regress/t/t_gate_basic.v
+++ test_regress/t/t_gate_basic.v
@@ -16,8 +16,8 @@ module t (/*AUTOARG*/
    reg [31:0] b;
 
    wire [2:0] bf;  buf   BF0 (bf[0], a[0]),
-		         BF1 (bf[1], a[1]),
-		         BF2 (bf[2], a[2]);
+                         BF1 (bf[1], a[1]),
+                         BF2 (bf[2], a[2]);
 
    // verilator lint_off IMPLICIT
    not   #(0.108) NT0 (nt0, a[0]);
@@ -48,43 +48,66 @@ module t (/*AUTOARG*/
     // path delays
     (A1 *> Q) = (a$A1$Y, a$A1$Y);
     (A0 *> Q) = (b$A0$Y, a$A0$Z);
+
+    if (C1) (IN => OUT) = (1,1);
+    ifnone (IN => OUT) = (2,2);
+
+    showcancelled Q;
+    noshowcancelled Q;
+    pulsestyle_ondetect Q;
+    pulsestyle_onevent Q;
+
+    // other unimplemented stuff
+    $fullskew();
+    $hold();
+    $nochange();
+    $period();
+    $recovery();
+    $recrem();
+    $removal();
+    $setup();
+    $setuphold();
+    $skew();
+    $timeskew();
+    $width();
+
   endspecify
 `endif
 
    always @ (posedge clk) begin
       if (cyc!=0) begin
-	 cyc <= cyc + 1;
-	 if (cyc==1) begin
-	    a <= 32'h18f6b034;
-	    b <= 32'h834bf892;
-	 end
-	 if (cyc==2) begin
-	    a <= 32'h529ab56f;
-	    b <= 32'h7835a237;
-	    if (bf !== 3'b100) $stop;
-	    if (nt0 !== 1'b1) $stop;
-	    if (an0 !== 1'b0) $stop;
-	    if (nd0 !== 1'b1) $stop;
-	    if (or0 !== 1'b0) $stop;
-	    if (nr0 !== 1'b1) $stop;
-	    if (xo0 !== 1'b0) $stop;
-	    if (xn0 !== 1'b1) $stop;
-	    if (ba != 32'h18f6b034) $stop;
-	 end
-	 if (cyc==3) begin
-	    if (bf !== 3'b111) $stop;
-	    if (nt0 !== 1'b0) $stop;
-	    if (an0 !== 1'b1) $stop;
-	    if (nd0 !== 1'b0) $stop;
-	    if (or0 !== 1'b1) $stop;
-	    if (nr0 !== 1'b0) $stop;
-	    if (xo0 !== 1'b0) $stop;
-	    if (xn0 !== 1'b0) $stop;
-	 end
-	 if (cyc==4) begin
-	    $write("*-* All Finished *-*\n");
-	    $finish;
-	 end
+         cyc <= cyc + 1;
+         if (cyc==1) begin
+            a <= 32'h18f6b034;
+            b <= 32'h834bf892;
+         end
+         if (cyc==2) begin
+            a <= 32'h529ab56f;
+            b <= 32'h7835a237;
+            if (bf !== 3'b100) $stop;
+            if (nt0 !== 1'b1) $stop;
+            if (an0 !== 1'b0) $stop;
+            if (nd0 !== 1'b1) $stop;
+            if (or0 !== 1'b0) $stop;
+            if (nr0 !== 1'b1) $stop;
+            if (xo0 !== 1'b0) $stop;
+            if (xn0 !== 1'b1) $stop;
+            if (ba != 32'h18f6b034) $stop;
+         end
+         if (cyc==3) begin
+            if (bf !== 3'b111) $stop;
+            if (nt0 !== 1'b0) $stop;
+            if (an0 !== 1'b1) $stop;
+            if (nd0 !== 1'b0) $stop;
+            if (or0 !== 1'b1) $stop;
+            if (nr0 !== 1'b0) $stop;
+            if (xo0 !== 1'b0) $stop;
+            if (xn0 !== 1'b0) $stop;
+         end
+         if (cyc==4) begin
+            $write("*-* All Finished *-*\n");
+            $finish;
+         end
       end
    end
 
