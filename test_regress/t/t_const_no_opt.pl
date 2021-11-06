b08c694cd626e349f4bad8048ab946485c5ba6b0
Yutetsu TAKATSUKASA <y.takatsukasa@gmail.com>
2021-11-06 12:31:50 +0900

    Fix wrong bit op tree optimization (#3185)
    * Add a test to reproduce bug3182. Run the same HDL with -Oo to confirm the result is same.
    
    * Hopefully fix #3182. The result can be 0 only when polarity is true (no AstNot is found during traversal).

--- test_regress/t/t_const_no_opt.pl
+++ test_regress/t/t_const_no_opt.pl
@@ -9,9 +9,11 @@ if (!$::Driver) { use FindBin; exec("$FindBin::Bin/bootstrap.pl", @ARGV, $0); di
 # SPDX-License-Identifier: LGPL-3.0-only OR Artistic-2.0
 
 scenarios(simulator => 1);
+top_filename("t/t_const_opt.v");
 
+# Run the same design as t_const_opt.pl without bitopt tree optimization to make sure that the result is same.
 compile(
-    verilator_flags2 => ["-Wno-UNOPTTHREADS", "--stats", "$Self->{t_dir}/$Self->{name}.cpp"],
+    verilator_flags2 => ["-Wno-UNOPTTHREADS", "--stats", "-Oo", "$Self->{t_dir}/t_const_opt.cpp"],
     );
 
 execute(
