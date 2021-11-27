8843726dac182d8b5912b45c78696d1a99fe9129
Yutetsu TAKATSUKASA <y.takatsukasa@gmail.com>
2021-11-28 00:07:33 +0900

    Delete comment-out line.

--- test_regress/t/t_const_opt.v
+++ test_regress/t/t_const_opt.v
@@ -135,7 +135,6 @@ endmodule
 module bug3197(input wire clk, input wire [31:0] in, output out);
    logic [63:0] d;
    always_ff @(posedge clk)
-      //d <= {d[31:0], in};
       d <= {d[31:0], in[0] ? in : 32'b0};
 
    wire tmp0 = (|d[38:0]);
