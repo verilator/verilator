e814855357b0007cdadf748c33f149e5e1b36494
Wilson Snyder <wsnyder@wsnyder.org>
2021-11-28 15:33:33 -0500

    Tests: Skip gdb tests if no gdb

--- test_regress/t/t_debug_sigsegv_bt_bad.pl
+++ test_regress/t/t_debug_sigsegv_bt_bad.pl
@@ -9,18 +9,22 @@ if (!$::Driver) { use FindBin; exec("$FindBin::Bin/bootstrap.pl", @ARGV, $0); di
 # SPDX-License-Identifier: LGPL-3.0-only OR Artistic-2.0
 
 scenarios(vlt => 1);
-$ENV{VERILATOR_TEST_NO_GDB} and skip("Skipping due to VERILATOR_TEST_NO_GDB");
-
-lint(
-    verilator_flags2 => ["--lint-only --debug --gdbbt --debug-sigsegv"],
-    sanitize => 0,
-    fails => $Self->{vlt_all},
-    expect =>
+if ($ENV{VERILATOR_TEST_NO_GDB}) {
+    skip("Skipping due to VERILATOR_TEST_NO_GDB");
+} elsif (system("gdb --version")) {
+    skip("No gdb installed");
+} else {
+    lint(
+        verilator_flags2 => ["--lint-only --debug --gdbbt --debug-sigsegv"],
+        sanitize => 0,
+        fails => $Self->{vlt_all},
+        expect =>
 '.*
 Program received signal SIGSEGV, Segmentation fault.
 .*in V3Options::.*
 .*%Error: Command Failed.*',
-    );
+        );
 
-ok(1);
+    ok(1);
+}
 1;
