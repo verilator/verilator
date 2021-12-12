55976dbc25c794b87d19c4198de9b94feb54f8a0
Wilson Snyder <wsnyder@wsnyder.org>
2021-12-12 15:26:29 -0500

    Tests: Add t_class_param_nconst_bad test.

--- test_regress/t/t_class_param.v
+++ test_regress/t/t_class_param.v
@@ -4,8 +4,6 @@
 // any use, without warranty, 2020 by Wilson Snyder.
 // SPDX-License-Identifier: CC0-1.0
 
-module t (/*AUTOARG*/);
-
 class Cls #(parameter P = 12);
    bit [P-1:0] member;
    function bit [P-1:0] get_member;
@@ -16,14 +14,21 @@ class Cls #(parameter P = 12);
    endfunction
 endclass
 
+typedef Cls#(5) Cls5_t;
+
+module t (/*AUTOARG*/);
+
    Cls c12;
    Cls #(.P(4)) c4;
+   Cls5_t c5;
 
    initial begin
       c12 = new;
       c4 = new;
+      c5 = new;
       if (c12.P != 12) $stop;
       if (c4.P != 4) $stop;
+      if (c5.P != 5) $stop;
       if (c12.get_p() != 12) $stop;
       if (c4.get_p() != 4) $stop;
       // verilator lint_off WIDTH
@@ -34,6 +39,9 @@ endclass
       if (c4.member != 4'ha) $stop;
       if (c12.get_member() != 12'haaa) $stop;
       if (c4.get_member() != 4'ha) $stop;
+      if ($sformatf("%p", c12) != "'{member:'haaa}") $stop;
+      if ($sformatf("%p", c4) != "'{member:'ha}") $stop;
+
       $write("*-* All Finished *-*\n");
       $finish;
    end
