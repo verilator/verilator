73374a03033fe7d9860700757afb5ebe97843fd6
github action <action@example.com>
2022-01-02 18:36:52 +0000

    Apply 'make format'

--- src/V3EmitV.cpp
+++ src/V3EmitV.cpp
@@ -99,9 +99,7 @@ class EmitVBaseVisitor VL_NOT_FINAL : public EmitCBaseVisitor {
         iterateChildren(nodep);
         putqs(nodep, "end\n");
     }
-    virtual void visit(AstInitialAutomatic* nodep) override {
-        iterateChildren(nodep);
-    }
+    virtual void visit(AstInitialAutomatic* nodep) override { iterateChildren(nodep); }
     virtual void visit(AstAlways* nodep) override {
         putfs(nodep, "always ");
         if (m_sensesp) {
