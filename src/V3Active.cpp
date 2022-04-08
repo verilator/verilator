83e1d5f41617fb44381042e09af3c1d4e560b708
Julien Margetts <julien.margetts@kioxia.com>
2022-04-08 19:21:23 +0100

    Fix merge resolution error

--- src/V3Active.cpp
+++ src/V3Active.cpp
@@ -173,7 +173,7 @@ public:
         for (const auto& vrp : m_outputs) {
             LatchDetectGraphVertex* const vertp = castVertexp(vrp->varp()->user1p());
             vertp->user(true);  // Identify the output vertex we are checking paths _to_
-            if (!latchCheckInternal(castVertexp(verticesBeginp()))) { latch_detected = true; }
+            if (!latchCheckInternal(castVertexp(verticesBeginp()))) latch_detected = true;
             if (latch_detected && !latch_expected) {
                 nodep->v3warn(
                     LATCH,
