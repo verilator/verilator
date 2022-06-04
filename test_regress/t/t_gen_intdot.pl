e2525d84404fffd30acc7041754a446cfb57e26c
Wilson Snyder <wsnyder@wsnyder.org>
2022-06-04 12:22:11 -0400

    Tests: Fix racy tests for develop-v5.

--- test_regress/t/t_gen_intdot.pl
+++ test_regress/t/t_gen_intdot.pl
@@ -10,9 +10,6 @@ if (!$::Driver) { use FindBin; exec("$FindBin::Bin/bootstrap.pl", @ARGV, $0); di
 
 scenarios(simulator => 1);
 
-# FIXMEV5: fix
-skip("This test is racy, needs fixing");
-
 compile(
     );
 
