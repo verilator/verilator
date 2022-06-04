09f3f404624151d62b1dd1ea183586d1450cef54
Wilson Snyder <wsnyder@wsnyder.org>
2022-06-04 12:27:44 -0400

    Fix clang-discovered missing comma.

--- src/V3Ast.h
+++ src/V3Ast.h
@@ -316,7 +316,7 @@ public:
                                             "POS",
                                             "NEG",
                                             "EVENT",
-                                            "DPIEXPORT"
+                                            "DPIEXPORT",
                                             "TRUE",
                                             "COMBO",
                                             "HYBRID",
