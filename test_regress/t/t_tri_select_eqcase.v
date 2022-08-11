f69f55332f124ac82352b955fd3b2dd5ea3bba4d
Ryszard Rozak <rrozak@antmicro.com>
2022-08-11 17:44:21 +0200

    Add handling of new tristate node types in visitCaseEq
    These node types are:
    * AstSel
    * AstExtend
    * AstCond
    and I also made use of user1p if it exists.
    
    Signed-off-by: Ryszard Rozak <rrozak@antmicro.com>

---
+++ test_regress/t/t_tri_select_eqcase.v
@@ -0,0 +1,16 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain, for
+// any use, without warranty, 2022 by Antmicro Ltd.
+// SPDX-License-Identifier: CC0-1.0
+
+module t (/*AUTOARG*/);
+   reg [3:0] a = 4'b11z1;
+   logic     b = 1'z === a[1];
+   always begin
+      if (b) begin
+         $write("*-* All Finished *-*\n");
+         $finish;
+      end
+   end
+endmodule
