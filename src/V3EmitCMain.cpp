1a1d2ecfd9905df62d7521dcd3ac5ff781eb1552
Krzysztof Bieganski <kbieganski@antmicro.com>
2022-08-25 15:55:37 +0200

    Enable tracing in generated main (#3578)
    Signed-off-by: Krzysztof Bieganski <kbieganski@antmicro.com>

--- src/V3EmitCMain.cpp
+++ src/V3EmitCMain.cpp
@@ -62,6 +62,7 @@ private:
         puts("int main(int argc, char** argv, char**) {\n");
         puts("// Setup context, defaults, and parse command line\n");
         puts("Verilated::debug(0);\n");
+        if (v3Global.opt.trace()) puts("Verilated::traceEverOn(true);\n");
         puts("const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};\n");
         puts("contextp->commandArgs(argc, argv);\n");
         puts("\n");
