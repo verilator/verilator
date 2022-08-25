5869fdf7f6f43103c5a7d09b9d0a4f21774bcb29
Varun Koyyalagunta <vkoyyalagunta@tenstorrent.com>
2022-08-25 18:29:11 -0500

    Fix $dump systemtask with --output-split-cfuncs (#3495) (#3497)

---
+++ test_regress/t/t_trace_split_cfuncs_dpi_export.v
@@ -0,0 +1,18 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain, for
+// any use, without warranty, 2022 by Varun Koyyalagunta.
+// SPDX-License-Identifier: CC0-1.0
+
+module t ();
+
+	function automatic void func();
+	endfunction
+	export "DPI-C" function func;
+
+	initial begin
+		$dumpfile("dump.vcd");
+		$dumpvars();
+	end
+
+endmodule
