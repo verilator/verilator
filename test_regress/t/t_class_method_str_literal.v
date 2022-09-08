e97b1d85c5ff75c8609f3c64bd539748db70f906
Gustav Svensk <grulfen3@gmail.com>
2022-09-08 10:24:18 +0200

    Tests: add more tests for #3547
    Add test for static functions

--- test_regress/t/t_class_method_str_literal.v
+++ test_regress/t/t_class_method_str_literal.v
@@ -4,12 +4,18 @@ class T;
     function automatic void print_str(input string a_string);
         $display(a_string);
     endfunction
+
+    static function automatic void static_print_str(input string a_string);
+        $display(a_string);
+    endfunction
 endclass
 
 
 initial begin
     T t_c = new;
-    t_c.print_str("print me");
+    t_c.print_str("function though member");
+    t_c.static_print_str("static function through member");
+    T::static_print_str("static function through class");
     $write("*-* All Finished *-*\n");
     $finish;
 end
