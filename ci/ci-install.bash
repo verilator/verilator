9a27004ae547757579a87734fd4bc4a26f90589a
Wilson Snyder <wsnyder@wsnyder.org>
2022-09-07 22:49:09 -0400

    Github actions: Avoid cpan cert expirations.

--- ci/ci-install.bash
+++ ci/ci-install.bash
@@ -19,6 +19,9 @@ set -x
 
 cd $(dirname "$0")/..
 
+# Avoid occasional cpan failures "Issued certificate has expired."
+export PERL_LWP_SSL_VERIFY_HOSTNAME=0
+
 fatal() {
   echo "ERROR: $(basename "$0"): $1" >&2; exit 1;
 }
