495d52d30fb9b9d51b9fef582c2d2de99f3026e9
Wilson Snyder <wsnyder@wsnyder.org>
2022-10-22 12:17:56 -0400

    Test and fix extra end_keywords

---
+++ test_regress/t/t_preproc_kwd_bad.v
@@ -0,0 +1,11 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain, for
+// any use, without warranty, 2003 by Wilson Snyder.
+// SPDX-License-Identifier: CC0-1.0
+
+`end_keywords
+`end_keywords  // BAD
+
+module t;
+endmodule
