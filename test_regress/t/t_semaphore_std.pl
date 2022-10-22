2df886b2b056b1d0be06f57119be13408bc9a717
Wilson Snyder <wsnyder@wsnyder.org>
2022-10-22 10:58:10 -0400

    Tests: Check std:: package for mailbox/process/semaphore

--- test_regress/t/t_semaphore_std.pl
+++ test_regress/t/t_semaphore_std.pl
@@ -2,19 +2,25 @@
 if (!$::Driver) { use FindBin; exec("$FindBin::Bin/bootstrap.pl", @ARGV, $0); die; }
 # DESCRIPTION: Verilator: Verilog Test driver/expect definition
 #
-# Copyright 2022 by Wilson Snyder. This program is free software; you
+# Copyright 2020 by Wilson Snyder. This program is free software; you
 # can redistribute it and/or modify it under the terms of either the GNU
 # Lesser General Public License Version 3 or the Perl Artistic License
 # Version 2.0.
 # SPDX-License-Identifier: LGPL-3.0-only OR Artistic-2.0
 
-scenarios(vlt => 1);
+scenarios(simulator => 1);
 
-lint(
-    verilator_flags2 => ["--xml-only"],
-    fails => 1,
+top_filename("t/t_semaphore.pl");
+
+compile(
+    v_flags2 => ["+define+T_SEMAPHORE+std::semaphore"],
+    fails => $Self->{vlt_all},
     expect_filename => $Self->{golden_filename},
     );
+
+execute(
+    check_finished => 1,
+    ) if !$Self->{vlt_all};
 
 ok(1);
 1;
