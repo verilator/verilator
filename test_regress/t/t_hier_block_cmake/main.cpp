45391b83b72f918f05ca1dffa97bfc5f7453e7e2
Kamil Rakoczy <krakoczy@antmicro.com>
2022-10-24 14:36:12 +0200

    Remove usage of VL_THREADED in tests
    Signed-off-by: Kamil Rakoczy <krakoczy@antmicro.com>

--- test_regress/t/t_hier_block_cmake/main.cpp
+++ test_regress/t/t_hier_block_cmake/main.cpp
@@ -14,9 +14,8 @@
 
 int main(int argc, char *argv[]) {
     const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
-#if VL_THREADED
-    contextp->threads(6);
-#endif
+    // TEST_THREADS is set in t_hier_block_cmake.pl
+    contextp->threads(TEST_THREADS);
     contextp->commandArgs(argc, argv);
     std::unique_ptr<Vt_hier_block> top{new Vt_hier_block{contextp.get(), "top"}};
     for (int i = 0; i < 100 && !contextp->gotFinish(); ++i) {
