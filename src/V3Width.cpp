b2e61425d6eb55e0a9a56558cd707aa38f69cc7a
Wilson Snyder <wsnyder@wsnyder.org>
2022-11-11 17:45:34 -0500

    Fix const initial assignments.

--- src/V3Width.cpp
+++ src/V3Width.cpp
@@ -2116,6 +2116,7 @@ private:
             nodep->v3error("Assigning to const ref variable: " << nodep->prettyNameQ());
         } else if (nodep->access().isWriteOrRW() && nodep->varp()->isConst() && !m_paramsOnly
                    && (!m_ftaskp || !m_ftaskp->isConstructor())
+                   && !VN_IS(m_procedurep, InitialAutomatic)
                    && !VN_IS(m_procedurep, InitialStatic)) {
             // Too loose, but need to allow our generated first assignment
             // Move this to a property of the AstInitial block
