240ce4466f030304ff258f1bed9a717dadaba9f7
G-A. Kamendje <gkamendje@gmail.com>
2022-11-29 11:55:25 -0500

    fixing whitespace

--- test_regress/t/t_case_enum_incomplete_bad.v
+++ test_regress/t/t_case_enum_incomplete_bad.v
@@ -3,7 +3,7 @@
 // This file ONLY is placed under the Creative Commons Public Domain, for
 // any use, without warranty, 2022 by Wilson Snyder.
 // SPDX-License-Identifier: CC0-1.0
-
+// G.A Kamendje
 module t(/*AUTOARG*/
    // Inputs
    clk
