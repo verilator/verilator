KingkingLiu/verilator-1

Clocking block support
Signed-off-by: Krzysztof Bieganski <kbieganski@antmicro.com>

src/V3AstNodeOther.h
@@ -814,17 +814,50 @@ public:
     AstClass* classp() const;  // Class being extended (after link)
 };
 class AstClocking final : public AstNode {
-    // Set default clock region
     // Parents:  MODULE
-    // @astgen op1 := sensesp : List[AstSenItem]
-    // @astgen op2 := bodysp : List[AstNode]
+    // Children: SENITEM, CLOCKING ITEMs, VARs
+    // @astgen op1 := sensesp : AstSenItem
+    // @astgen op2 := itemsp : List[AstClockingItem]
+    // @astgen op3 := varsp : List[AstVar]
+    // @astgen op4 := eventp : Optional[AstVar]
+    std::string m_name;
+    const bool m_isDefault = false;
+
 public:
-    AstClocking(FileLine* fl, AstSenItem* sensesp, AstNode* bodysp)
-        : ASTGEN_SUPER_Clocking(fl) {
-        this->addSensesp(sensesp);
-        this->addBodysp(bodysp);
+    AstClocking(FileLine* fl, const std::string& name, AstSenItem* sensesp,
+                AstClockingItem* itemsp, bool isDefault)
+        : ASTGEN_SUPER_Clocking(fl)
+        , m_isDefault{isDefault} {
+        m_name = name;
+        this->sensesp(sensesp);
+        addItemsp(itemsp);
     }
     ASTGEN_MEMBERS_AstClocking;
+    std::string name() const override { return m_name; }
+    bool isDefault() const { return m_isDefault; }
+};
+class AstClockingItem final : public AstNode {
+    // Parents:  CLOCKING
+    // Children: EXPRs, ASSIGNs, VARs
+    // @astgen op1 := skewp : Optional[AstNodeExpr]
+    // @astgen op2 := exprp : Optional[AstNodeExpr]
+    // @astgen op3 := assignp : Optional[AstAssign]
+    // @astgen op4 := varp : Optional[AstVar]
+    VDirection m_direction;
+
+public:
+    AstClockingItem(FileLine* fl, VDirection direction, AstNodeExpr* skewp, AstNode* clockingDeclp)
+        : ASTGEN_SUPER_ClockingItem(fl) {
+        m_direction = direction;
+        this->skewp(skewp);
+        if (auto* const assignp = VN_CAST(clockingDeclp, Assign)) {
+            this->assignp(assignp);
+        } else {
+            exprp(VN_AS(clockingDeclp, NodeExpr));
+        }
+    }
+    ASTGEN_MEMBERS_AstClockingItem;
+    VDirection direction() const { return m_direction; }
 };
 class AstConstPool final : public AstNode {
     // Container for const static data
@@ -2182,6 +2215,17 @@ public:
     void dump(std::ostream& str) const override;
     VAlwaysKwd keyword() const { return m_keyword; }
 };
+class AstAlwaysObserved final : public AstNodeProcedure {
+    // Like always but Observed scheduling region
+    // @astgen op1 := sensesp : Optional[AstSenTree] // Sensitivity list, removed in V3Active
+
+public:
+    AstAlwaysObserved(FileLine* fl, AstSenTree* sensesp, AstNode* bodysp)
+        : ASTGEN_SUPER_AlwaysObserved(fl, bodysp) {
+        this->sensesp(sensesp);
+    }
+    ASTGEN_MEMBERS_AstAlwaysObserved;
+};
 class AstAlwaysPost final : public AstNodeProcedure {
     // Like always but post assignments for memory assignment IFs
     // @astgen op1 := sensesp : Optional[AstSenTree] // Sensitivity list iff clocked
@@ -2199,6 +2243,17 @@ public:
     AstAlwaysPostponed(FileLine* fl, AstNode* stmtsp)
         : ASTGEN_SUPER_AlwaysPostponed(fl, stmtsp) {}
     ASTGEN_MEMBERS_AstAlwaysPostponed;
+};
+class AstAlwaysReactive final : public AstNodeProcedure {
+    // Like always but Reactive scheduling region
+    // @astgen op1 := sensesp : Optional[AstSenTree] // Sensitivity list, removed in V3Active
+
+public:
+    AstAlwaysReactive(FileLine* fl, AstSenTree* sensesp, AstNode* bodysp)
+        : ASTGEN_SUPER_AlwaysReactive(fl, bodysp) {
+        this->sensesp(sensesp);
+    }
+    ASTGEN_MEMBERS_AstAlwaysReactive;
 };
 class AstFinal final : public AstNodeProcedure {
 public:
@@ -2496,13 +2551,18 @@ class AstDelay final : public AstNodeStmt {
     // Delay statement
     // @astgen op1 := lhsp : AstNodeExpr // Delay value
     // @astgen op2 := stmtsp : List[AstNode] // Statements under delay
+    const bool m_isCycle;  // True if it is a cycle delay
+
 public:
-    AstDelay(FileLine* fl, AstNodeExpr* lhsp)
-        : ASTGEN_SUPER_Delay(fl) {
+    AstDelay(FileLine* fl, AstNodeExpr* lhsp, bool isCycle)
+        : ASTGEN_SUPER_Delay(fl)
+        , m_isCycle{isCycle} {
         this->lhsp(lhsp);
     }
     ASTGEN_MEMBERS_AstDelay;
+    void dump(std::ostream& str) const override;
     bool isTimingControl() const override { return true; }
+    bool isCycleDelay() const { return m_isCycle; }
     bool same(const AstNode* /*samep*/) const override { return true; }
 };
 class AstDisable final : public AstNodeStmt {

src/V3LinkDot.cpp
@@ -740,6 +740,7 @@ class LinkDotFindVisitor final : public VNVisitor {
     // STATE
     LinkDotState* const m_statep;  // State to pass between visitors, including symbol table
     AstNodeModule* m_classOrPackagep = nullptr;  // Current package
+    AstClocking* m_clockingp = nullptr;  // Current clocking block
     VSymEnt* m_modSymp = nullptr;  // Symbol Entry for current module
     VSymEnt* m_curSymp = nullptr;  // Symbol Entry for current table, where to lookup/insert
     string m_scope;  // Scope text
@@ -1123,6 +1124,49 @@ class LinkDotFindVisitor final : public VNVisitor {
             m_ftaskp = nullptr;
         }
     }
+    void visit(AstClocking* nodep) override {
+        VL_RESTORER(m_clockingp);
+        m_clockingp = nodep;
+        iterate(nodep->sensesp());
+        iterateAndNextNull(nodep->itemsp());
+        // If the block has no name, one cannot reference the clockvars
+        if (nodep->name().empty()) return;
+        VL_RESTORER(m_curSymp);
+        m_curSymp = m_statep->insertBlock(m_curSymp, nodep->name(), nodep, m_classOrPackagep);
+        m_curSymp->fallbackp(nullptr);
+        iterateAndNextNull(nodep->itemsp());
+    }
+    void visit(AstClockingItem* nodep) override {
+        if (nodep->varp()) {
+            if (m_curSymp->nodep() == m_clockingp) iterate(nodep->varp());
+            return;
+        }
+        std::string varname;
+        AstNodeDType* dtypep;
+        if (auto* const assignp = nodep->assignp()) {
+            auto* const rhsp = assignp->rhsp()->unlinkFrBack();
+            dtypep = new AstRefDType{nodep->fileline(), AstRefDType::FlagTypeOfExpr{},
+                                     rhsp->cloneTree(false)};
+            nodep->exprp(rhsp);
+            varname = assignp->lhsp()->name();
+            VL_DO_DANGLING(assignp->deleteTree(), assignp);
+        } else {
+            auto* const refp = nodep->exprp();
+            const VSymEnt* foundp = m_curSymp->findIdFallback(refp->name());
+            if (!foundp || !foundp->nodep()) {
+                refp->v3error("Corresponding variable " << refp->prettyNameQ()
+                                                        << " does not exist");
+                VL_DO_DANGLING(nodep->unlinkFrBack()->deleteTree(), nodep);
+                return;
+            }
+            varname = refp->name();
+            dtypep = VN_AS(foundp->nodep(), Var)->childDTypep()->cloneTree(false);
+        }
+        auto* const newvarp = new AstVar{nodep->fileline(), VVarType::MODULETEMP, varname,
+                                         VFlagChildDType{}, dtypep};
+        nodep->varp(newvarp);
+        iterate(nodep->exprp());
+    }
     void visit(AstVar* nodep) override {
         // Var: Remember its name for later resolution
         UASSERT_OBJ(m_curSymp && m_modSymp, nodep, "Var not under module?");
@@ -1150,6 +1194,10 @@ class LinkDotFindVisitor final : public VNVisitor {
                                              << cvtToHex(foundp->parentp()) << endl);
                 if (foundp->parentp() == m_curSymp  // Only when on same level
                     && !foundp->imported()) {  // and not from package
+                    if (VN_IS(m_curSymp->nodep(), Clocking)) {
+                        nodep->v3error("Multiple clockvars with the same name not allowed");
+                        return;
+                    }
                     const bool nansiBad
                         = ((findvarp->isDeclTyped() && nodep->isDeclTyped())
                            || (findvarp->isIO() && nodep->isIO()));  // e.g. !(output && output)
@@ -1946,6 +1994,7 @@ private:
     AstNodeModule* m_modp = nullptr;  // Current module
     AstNodeFTask* m_ftaskp = nullptr;  // Current function/task
     int m_modportNum = 0;  // Uniqueify modport numbers
+    bool m_inSens = false;  // True if in senitem
 
     struct DotStates {
         DotPosition m_dotPos;  // Scope part of dotted resolution
@@ -2061,6 +2110,24 @@ private:
         } else {
             refp->user5p(nodep);
         }
+    }
+    VSymEnt* getCreateClockingEventSymEnt(AstClocking* clockingp) {
+        if (!clockingp->eventp()) {
+            auto* const eventp = new AstVar{
+                clockingp->fileline(), VVarType::MODULETEMP, clockingp->name(), VFlagChildDType{},
+                new AstBasicDType{clockingp->fileline(), VBasicDTypeKwd::EVENT}};
+            clockingp->eventp(eventp);
+            // Trigger the clocking event in Observed (IEEE 1800-2017 14.13)
+            clockingp->addNextHere(new AstAlwaysObserved{
+                clockingp->fileline(),
+                new AstSenTree{clockingp->fileline(), clockingp->sensesp()->cloneTree(false)},
+                new AstFireEvent{clockingp->fileline(),
+                                 new AstVarRef{clockingp->fileline(), eventp, VAccess::WRITE},
+                                 false}});
+            v3Global.setHasEvents();
+            eventp->user1p(new VSymEnt{m_statep->symsp(), eventp});
+        }
+        return reinterpret_cast<VSymEnt*>(clockingp->eventp()->user1p());
     }
 
     bool isParamedClassRef(const AstNode* nodep) {
@@ -2311,6 +2378,11 @@ private:
             m_ds.m_dotp = lastStates.m_dotp;
         }
     }
+    void visit(AstSenItem* nodep) override {
+        VL_RESTORER(m_inSens);
+        m_inSens = true;
+        iterateChildren(nodep);
+    }
     void visit(AstParseRef* nodep) override {
         if (nodep->user3SetOnce()) return;
         UINFO(9, "   linkPARSEREF " << m_ds.ascii() << "  n=" << nodep << endl);
@@ -2417,6 +2489,12 @@ private:
             }
             // What fell out?
             bool ok = false;
+            // Special case: waiting on clocking event
+            if (m_inSens && foundp && m_ds.m_dotPos != DP_SCOPE) {
+                if (auto* const clockingp = VN_CAST(foundp->nodep(), Clocking)) {
+                    foundp = getCreateClockingEventSymEnt(clockingp);
+                }
+            }
             if (!foundp) {
             } else if (VN_IS(foundp->nodep(), Cell) || VN_IS(foundp->nodep(), Begin)
                        || VN_IS(foundp->nodep(), Netlist)  // for $root
@@ -2582,6 +2660,9 @@ private:
                     m_ds.m_dotPos = DP_MEMBER;
                     m_ds.m_dotText = "";
                 }
+            } else if (VN_IS(foundp->nodep(), Clocking)) {
+                m_ds.m_dotSymp = foundp;
+                ok = m_ds.m_dotPos == DP_SCOPE;
             }
             //
             if (!ok) {

src/V3LinkParse.cpp
@@ -60,6 +60,8 @@ private:
     AstNodeModule* m_modp = nullptr;  // Current module
     AstNodeFTask* m_ftaskp = nullptr;  // Current task
     AstNodeDType* m_dtypep = nullptr;  // Current data type
+    AstNodeExpr* m_defaultInSkewp = nullptr;  // Current default input skew
+    AstNodeExpr* m_defaultOutSkewp = nullptr;  // Current default output skew
     int m_genblkAbove = 0;  // Begin block number of if/case/for above
     int m_genblkNum = 0;  // Begin block number, 0=none seen
     VLifetime m_lifetime = VLifetime::STATIC;  // Propagating lifetime
@@ -631,6 +633,66 @@ private:
         if (nodep->name() == "std" && !nodep->classOrPackagep()) {
             nodep->classOrPackagep(m_stdPackagep);
         }
+    }
+    void visit(AstClocking* nodep) override {
+        VL_RESTORER(m_defaultInSkewp);
+        VL_RESTORER(m_defaultOutSkewp);
+        // Find default input and output skews
+        auto* nextItemp = nodep->itemsp();
+        for (auto* itemp = nextItemp; itemp; itemp = nextItemp) {
+            nextItemp = VN_AS(itemp->nextp(), ClockingItem);
+            if (itemp->exprp()) continue;
+            if (itemp->skewp()) {
+                if (itemp->direction() == VDirection::INPUT) {
+                    // Disallow default redefinition; note some simulators allow this
+                    if (m_defaultInSkewp) {
+                        itemp->skewp()->v3error("Multiple default input skews not allowed");
+                    }
+                    m_defaultInSkewp = itemp->skewp();
+                } else if (itemp->direction() == VDirection::OUTPUT) {
+                    if (auto* const constp = VN_CAST(itemp->skewp(), Const)) {
+                        if (constp->num().is1Step()) {
+                            itemp->skewp()->v3error("1step not allowed as output skew");
+                        }
+                    }
+                    // Disallow default redefinition; note some simulators allow this
+                    if (m_defaultOutSkewp) {
+                        itemp->skewp()->v3error("Multiple default output skews not allowed");
+                    }
+                    m_defaultOutSkewp = itemp->skewp();
+                } else {
+                    itemp->v3fatalSrc("Incorrect direction");
+                }
+            }
+            pushDeletep(itemp->unlinkFrBack());
+        }
+        iterateChildren(nodep);
+    }
+    void visit(AstClockingItem* nodep) override {
+        if (nodep->direction() == VDirection::OUTPUT) {
+            if (!nodep->skewp()) {
+                if (m_defaultOutSkewp) {
+                    nodep->skewp(m_defaultOutSkewp->cloneTree(false));
+                } else {
+                    // Default is 0 (IEEE 1800-2017 14.3)
+                    nodep->skewp(new AstConst{nodep->fileline(), 0});
+                }
+            } else if (auto* const constp = VN_CAST(nodep->skewp(), Const)) {
+                if (constp->num().is1Step()) {
+                    nodep->skewp()->v3error("1step not allowed as output skew");
+                }
+            }
+        } else if (nodep->direction() == VDirection::INPUT) {
+            if (!nodep->skewp()) {
+                if (m_defaultInSkewp) {
+                    nodep->skewp(m_defaultInSkewp->cloneTree(false));
+                } else {
+                    // Default is 1step (IEEE 1800-2017 14.3)
+                    nodep->skewp(new AstConst{nodep->fileline(), AstConst::OneStep{}});
+                }
+            }
+        }
+        iterateChildren(nodep);
     }
 
     void visit(AstNode* nodep) override {

verilator/verilator

src/V3AstNodeOther.h
@@ -814,17 +814,50 @@ public:
     AstClass* classp() const;  // Class being extended (after link)
 };
 class AstClocking final : public AstNode {
-    // Set default clock region
     // Parents:  MODULE
-    // @astgen op1 := sensesp : List[AstSenItem]
-    // @astgen op2 := bodysp : List[AstNode]
+    // Children: SENITEM, CLOCKING ITEMs, VARs
+    // @astgen op1 := sensesp : AstSenItem
+    // @astgen op2 := itemsp : List[AstClockingItem]
+    // @astgen op3 := varsp : List[AstVar]
+    // @astgen op4 := eventp : Optional[AstVar]
+    std::string m_name;  // Clocking block name
+    const bool m_isDefault = false;  // True if default clocking
+
 public:
-    AstClocking(FileLine* fl, AstSenItem* sensesp, AstNode* bodysp)
-        : ASTGEN_SUPER_Clocking(fl) {
-        this->addSensesp(sensesp);
-        this->addBodysp(bodysp);
+    AstClocking(FileLine* fl, const std::string& name, AstSenItem* sensesp,
+                AstClockingItem* itemsp, bool isDefault)
+        : ASTGEN_SUPER_Clocking(fl)
+        , m_isDefault{isDefault} {
+        m_name = name;
+        this->sensesp(sensesp);
+        addItemsp(itemsp);
     }
     ASTGEN_MEMBERS_AstClocking;
+    std::string name() const override { return m_name; }
+    bool isDefault() const { return m_isDefault; }
+};
+class AstClockingItem final : public AstNode {
+    // Parents:  CLOCKING
+    // Children: EXPRs, ASSIGNs, VARs
+    // @astgen op1 := skewp : Optional[AstNodeExpr]
+    // @astgen op2 := exprp : Optional[AstNodeExpr]
+    // @astgen op3 := assignp : Optional[AstAssign]
+    // @astgen op4 := varp : Optional[AstVar]
+    VDirection m_direction;
+
+public:
+    AstClockingItem(FileLine* fl, VDirection direction, AstNodeExpr* skewp, AstNode* clockingDeclp)
+        : ASTGEN_SUPER_ClockingItem(fl) {
+        m_direction = direction;
+        this->skewp(skewp);
+        if (AstAssign* const assignp = VN_CAST(clockingDeclp, Assign)) {
+            this->assignp(assignp);
+        } else {
+            exprp(VN_AS(clockingDeclp, NodeExpr));
+        }
+    }
+    ASTGEN_MEMBERS_AstClockingItem;
+    VDirection direction() const { return m_direction; }
 };
 class AstConstPool final : public AstNode {
     // Container for const static data
@@ -2182,6 +2215,17 @@ public:
     void dump(std::ostream& str) const override;
     VAlwaysKwd keyword() const { return m_keyword; }
 };
+class AstAlwaysObserved final : public AstNodeProcedure {
+    // Like always but Observed scheduling region
+    // @astgen op1 := sensesp : Optional[AstSenTree] // Sensitivity list, removed in V3Active
+
+public:
+    AstAlwaysObserved(FileLine* fl, AstSenTree* sensesp, AstNode* bodysp)
+        : ASTGEN_SUPER_AlwaysObserved(fl, bodysp) {
+        this->sensesp(sensesp);
+    }
+    ASTGEN_MEMBERS_AstAlwaysObserved;
+};
 class AstAlwaysPost final : public AstNodeProcedure {
     // Like always but post assignments for memory assignment IFs
     // @astgen op1 := sensesp : Optional[AstSenTree] // Sensitivity list iff clocked
@@ -2199,6 +2243,17 @@ public:
     AstAlwaysPostponed(FileLine* fl, AstNode* stmtsp)
         : ASTGEN_SUPER_AlwaysPostponed(fl, stmtsp) {}
     ASTGEN_MEMBERS_AstAlwaysPostponed;
+};
+class AstAlwaysReactive final : public AstNodeProcedure {
+    // Like always but Reactive scheduling region
+    // @astgen op1 := sensesp : Optional[AstSenTree] // Sensitivity list, removed in V3Active
+
+public:
+    AstAlwaysReactive(FileLine* fl, AstSenTree* sensesp, AstNode* bodysp)
+        : ASTGEN_SUPER_AlwaysReactive(fl, bodysp) {
+        this->sensesp(sensesp);
+    }
+    ASTGEN_MEMBERS_AstAlwaysReactive;
 };
 class AstFinal final : public AstNodeProcedure {
 public:
@@ -2496,13 +2551,18 @@ class AstDelay final : public AstNodeStmt {
     // Delay statement
     // @astgen op1 := lhsp : AstNodeExpr // Delay value
     // @astgen op2 := stmtsp : List[AstNode] // Statements under delay
+    const bool m_isCycle;  // True if it is a cycle delay
+
 public:
-    AstDelay(FileLine* fl, AstNodeExpr* lhsp)
-        : ASTGEN_SUPER_Delay(fl) {
+    AstDelay(FileLine* fl, AstNodeExpr* lhsp, bool isCycle)
+        : ASTGEN_SUPER_Delay(fl)
+        , m_isCycle{isCycle} {
         this->lhsp(lhsp);
     }
     ASTGEN_MEMBERS_AstDelay;
+    void dump(std::ostream& str) const override;
     bool isTimingControl() const override { return true; }
+    bool isCycleDelay() const { return m_isCycle; }
     bool same(const AstNode* /*samep*/) const override { return true; }
 };
 class AstDisable final : public AstNodeStmt {

src/V3LinkDot.cpp
@@ -740,6 +740,7 @@ class LinkDotFindVisitor final : public VNVisitor {
     // STATE
     LinkDotState* const m_statep;  // State to pass between visitors, including symbol table
     AstNodeModule* m_classOrPackagep = nullptr;  // Current package
+    AstClocking* m_clockingp = nullptr;  // Current clocking block
     VSymEnt* m_modSymp = nullptr;  // Symbol Entry for current module
     VSymEnt* m_curSymp = nullptr;  // Symbol Entry for current table, where to lookup/insert
     string m_scope;  // Scope text
@@ -1123,6 +1124,49 @@ class LinkDotFindVisitor final : public VNVisitor {
             m_ftaskp = nullptr;
         }
     }
+    void visit(AstClocking* nodep) override {
+        VL_RESTORER(m_clockingp);
+        m_clockingp = nodep;
+        iterate(nodep->sensesp());
+        iterateAndNextNull(nodep->itemsp());
+        // If the block has no name, one cannot reference the clockvars
+        if (nodep->name().empty()) return;
+        VL_RESTORER(m_curSymp);
+        m_curSymp = m_statep->insertBlock(m_curSymp, nodep->name(), nodep, m_classOrPackagep);
+        m_curSymp->fallbackp(nullptr);
+        iterateAndNextNull(nodep->itemsp());
+    }
+    void visit(AstClockingItem* nodep) override {
+        if (nodep->varp()) {
+            if (m_curSymp->nodep() == m_clockingp) iterate(nodep->varp());
+            return;
+        }
+        std::string varname;
+        AstNodeDType* dtypep;
+        if (AstAssign* const assignp = nodep->assignp()) {
+            AstNodeExpr* const rhsp = assignp->rhsp()->unlinkFrBack();
+            dtypep = new AstRefDType{nodep->fileline(), AstRefDType::FlagTypeOfExpr{},
+                                     rhsp->cloneTree(false)};
+            nodep->exprp(rhsp);
+            varname = assignp->lhsp()->name();
+            VL_DO_DANGLING(assignp->unlinkFrBack()->deleteTree(), assignp);
+        } else {
+            AstNodeExpr* const refp = nodep->exprp();
+            const VSymEnt* foundp = m_curSymp->findIdFallback(refp->name());
+            if (!foundp || !foundp->nodep()) {
+                refp->v3error("Corresponding variable " << refp->prettyNameQ()
+                                                        << " does not exist");
+                VL_DO_DANGLING(nodep->unlinkFrBack()->deleteTree(), nodep);
+                return;
+            }
+            varname = refp->name();
+            dtypep = VN_AS(foundp->nodep(), Var)->childDTypep()->cloneTree(false);
+        }
+        AstVar* const newvarp = new AstVar{nodep->fileline(), VVarType::MODULETEMP, varname,
+                                           VFlagChildDType{}, dtypep};
+        nodep->varp(newvarp);
+        iterate(nodep->exprp());
+    }
     void visit(AstVar* nodep) override {
         // Var: Remember its name for later resolution
         UASSERT_OBJ(m_curSymp && m_modSymp, nodep, "Var not under module?");
@@ -1150,6 +1194,10 @@ class LinkDotFindVisitor final : public VNVisitor {
                                              << cvtToHex(foundp->parentp()) << endl);
                 if (foundp->parentp() == m_curSymp  // Only when on same level
                     && !foundp->imported()) {  // and not from package
+                    if (VN_IS(m_curSymp->nodep(), Clocking)) {
+                        nodep->v3error("Multiple clockvars with the same name not allowed");
+                        return;
+                    }
                     const bool nansiBad
                         = ((findvarp->isDeclTyped() && nodep->isDeclTyped())
                            || (findvarp->isIO() && nodep->isIO()));  // e.g. !(output && output)
@@ -1946,6 +1994,7 @@ private:
     AstNodeModule* m_modp = nullptr;  // Current module
     AstNodeFTask* m_ftaskp = nullptr;  // Current function/task
     int m_modportNum = 0;  // Uniqueify modport numbers
+    bool m_inSens = false;  // True if in senitem
 
     struct DotStates {
         DotPosition m_dotPos;  // Scope part of dotted resolution
@@ -2061,6 +2110,24 @@ private:
         } else {
             refp->user5p(nodep);
         }
+    }
+    VSymEnt* getCreateClockingEventSymEnt(AstClocking* clockingp) {
+        if (!clockingp->eventp()) {
+            AstVar* const eventp = new AstVar{
+                clockingp->fileline(), VVarType::MODULETEMP, clockingp->name(), VFlagChildDType{},
+                new AstBasicDType{clockingp->fileline(), VBasicDTypeKwd::EVENT}};
+            clockingp->eventp(eventp);
+            // Trigger the clocking event in Observed (IEEE 1800-2017 14.13)
+            clockingp->addNextHere(new AstAlwaysObserved{
+                clockingp->fileline(),
+                new AstSenTree{clockingp->fileline(), clockingp->sensesp()->cloneTree(false)},
+                new AstFireEvent{clockingp->fileline(),
+                                 new AstVarRef{clockingp->fileline(), eventp, VAccess::WRITE},
+                                 false}});
+            v3Global.setHasEvents();
+            eventp->user1p(new VSymEnt{m_statep->symsp(), eventp});
+        }
+        return reinterpret_cast<VSymEnt*>(clockingp->eventp()->user1p());
     }
 
     bool isParamedClassRef(const AstNode* nodep) {
@@ -2311,6 +2378,11 @@ private:
             m_ds.m_dotp = lastStates.m_dotp;
         }
     }
+    void visit(AstSenItem* nodep) override {
+        VL_RESTORER(m_inSens);
+        m_inSens = true;
+        iterateChildren(nodep);
+    }
     void visit(AstParseRef* nodep) override {
         if (nodep->user3SetOnce()) return;
         UINFO(9, "   linkPARSEREF " << m_ds.ascii() << "  n=" << nodep << endl);
@@ -2417,6 +2489,12 @@ private:
             }
             // What fell out?
             bool ok = false;
+            // Special case: waiting on clocking event
+            if (m_inSens && foundp && m_ds.m_dotPos != DP_SCOPE) {
+                if (AstClocking* const clockingp = VN_CAST(foundp->nodep(), Clocking)) {
+                    foundp = getCreateClockingEventSymEnt(clockingp);
+                }
+            }
             if (!foundp) {
             } else if (VN_IS(foundp->nodep(), Cell) || VN_IS(foundp->nodep(), Begin)
                        || VN_IS(foundp->nodep(), Netlist)  // for $root
@@ -2582,6 +2660,9 @@ private:
                     m_ds.m_dotPos = DP_MEMBER;
                     m_ds.m_dotText = "";
                 }
+            } else if (VN_IS(foundp->nodep(), Clocking)) {
+                m_ds.m_dotSymp = foundp;
+                ok = m_ds.m_dotPos == DP_SCOPE;
             }
             //
             if (!ok) {

src/V3LinkParse.cpp
@@ -60,6 +60,8 @@ private:
     AstNodeModule* m_modp = nullptr;  // Current module
     AstNodeFTask* m_ftaskp = nullptr;  // Current task
     AstNodeDType* m_dtypep = nullptr;  // Current data type
+    AstNodeExpr* m_defaultInSkewp = nullptr;  // Current default input skew
+    AstNodeExpr* m_defaultOutSkewp = nullptr;  // Current default output skew
     int m_genblkAbove = 0;  // Begin block number of if/case/for above
     int m_genblkNum = 0;  // Begin block number, 0=none seen
     VLifetime m_lifetime = VLifetime::STATIC;  // Propagating lifetime
@@ -631,6 +633,66 @@ private:
         if (nodep->name() == "std" && !nodep->classOrPackagep()) {
             nodep->classOrPackagep(m_stdPackagep);
         }
+    }
+    void visit(AstClocking* nodep) override {
+        VL_RESTORER(m_defaultInSkewp);
+        VL_RESTORER(m_defaultOutSkewp);
+        // Find default input and output skews
+        AstClockingItem* nextItemp = nodep->itemsp();
+        for (AstClockingItem* itemp = nextItemp; itemp; itemp = nextItemp) {
+            nextItemp = VN_AS(itemp->nextp(), ClockingItem);
+            if (itemp->exprp() || itemp->assignp()) continue;
+            if (itemp->skewp()) {
+                if (itemp->direction() == VDirection::INPUT) {
+                    // Disallow default redefinition; note some simulators allow this
+                    if (m_defaultInSkewp) {
+                        itemp->skewp()->v3error("Multiple default input skews not allowed");
+                    }
+                    m_defaultInSkewp = itemp->skewp();
+                } else if (itemp->direction() == VDirection::OUTPUT) {
+                    if (AstConst* const constp = VN_CAST(itemp->skewp(), Const)) {
+                        if (constp->num().is1Step()) {
+                            itemp->skewp()->v3error("1step not allowed as output skew");
+                        }
+                    }
+                    // Disallow default redefinition; note some simulators allow this
+                    if (m_defaultOutSkewp) {
+                        itemp->skewp()->v3error("Multiple default output skews not allowed");
+                    }
+                    m_defaultOutSkewp = itemp->skewp();
+                } else {
+                    itemp->v3fatalSrc("Incorrect direction");
+                }
+            }
+            pushDeletep(itemp->unlinkFrBack());
+        }
+        iterateChildren(nodep);
+    }
+    void visit(AstClockingItem* nodep) override {
+        if (nodep->direction() == VDirection::OUTPUT) {
+            if (!nodep->skewp()) {
+                if (m_defaultOutSkewp) {
+                    nodep->skewp(m_defaultOutSkewp->cloneTree(false));
+                } else {
+                    // Default is 0 (IEEE 1800-2017 14.3)
+                    nodep->skewp(new AstConst{nodep->fileline(), 0});
+                }
+            } else if (AstConst* const constp = VN_CAST(nodep->skewp(), Const)) {
+                if (constp->num().is1Step()) {
+                    nodep->skewp()->v3error("1step not allowed as output skew");
+                }
+            }
+        } else if (nodep->direction() == VDirection::INPUT) {
+            if (!nodep->skewp()) {
+                if (m_defaultInSkewp) {
+                    nodep->skewp(m_defaultInSkewp->cloneTree(false));
+                } else {
+                    // Default is 1step (IEEE 1800-2017 14.3)
+                    nodep->skewp(new AstConst{nodep->fileline(), AstConst::OneStep{}});
+                }
+            }
+        }
+        iterateChildren(nodep);
     }
 
     void visit(AstNode* nodep) override {