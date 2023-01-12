07801fcfa5ec6b4d5e30e3541f33ac38fb4776c6
Joey Liu <joeyjyliu@gmail.com>
2023-01-11 20:00:10 -0500

    add copyright header

--- test_regress/t/t_expand_word_sel.v
+++ test_regress/t/t_expand_word_sel.v
@@ -1,3 +1,9 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain, for
+// any use, without warranty, 2023 by Joey Liu.
+// SPDX-License-Identifier: CC0-1.0
+
 module t
   (
    output logic [255:0] data_out
