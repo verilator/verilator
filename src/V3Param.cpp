41e5f6b7d5e374ed73bbb3ac2e15cd9368b44a9a
github action <action@example.com>
2023-01-19 23:22:37 +0000

    Apply 'make format'

--- src/V3Param.cpp
+++ src/V3Param.cpp
@@ -330,9 +330,7 @@ class ParamProcessor final {
             key += " ";
             key += paramValueKey(dtypep->subDTypep());
         } else if (const AstBasicDType* const dtypep = VN_CAST(nodep, BasicDType)) {
-            if (dtypep->isSigned()) {
-                key += " signed";
-            }
+            if (dtypep->isSigned()) { key += " signed"; }
             if (dtypep->isRanged()) {
                 key += "[" + cvtToStr(dtypep->left()) + ":" + cvtToStr(dtypep->right()) + "]";
             }
