64d5dd3c904ec4fd98bfc9901918a44d5e6570ee
Ilya Barkov <barkovian@gmail.com>
2023-01-21 17:29:19 +0300

    Add associative array comparison

--- include/verilated_types.h
+++ include/verilated_types.h
@@ -694,6 +694,8 @@ public:
     VlAssocArray(VlAssocArray&&) = default;
     VlAssocArray& operator=(const VlAssocArray&) = default;
     VlAssocArray& operator=(VlAssocArray&&) = default;
+    bool operator==(const VlAssocArray& rhs) const { return m_map == rhs.m_map; }
+    bool operator!=(const VlAssocArray& rhs) const { return m_map != rhs.m_map; }
 
     // METHODS
     T_Value& atDefault() { return m_defaultValue; }
