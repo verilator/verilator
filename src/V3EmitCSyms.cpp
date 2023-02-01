032debda5b9007270b5ce0e2f55fa12b09c112de
Andrew Nolte <anolte@hudson-trading.com>
2023-02-01 12:41:27 -0500

    fixes from defname pr

--- src/V3EmitCSyms.cpp
+++ src/V3EmitCSyms.cpp
@@ -159,17 +159,16 @@ class EmitCSyms final : EmitCBaseVisitor {
 
         // If there's more than one ident and an escape, find the true last ident
         if (pos != string::npos && scpname.find('\\') != string::npos) {
-            int i = 0;
+            size_t i = 0;
             // always makes progress
             while (i < scpname.length()) {
                 if (scpname[i] == '\\') {
                     while (i < scpname.length() && scpname[i] != ' ') { i++; }
-                    i++;  // proc ' ', it should always be there. Then grab '.' on next cycle
+                    ++i;  // proc ' ', it should always be there. Then grab '.' on next cycle
                 } else {
                     while (i < scpname.length() && scpname[i] != '.') { i++; }
                     if (i < scpname.length()) {
-                        pos = i;
-                        i++;
+                        pos = i++;
                     }
                 }
             }
