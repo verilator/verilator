6188083baa3a36b654445db06dccf19535278632
Wilson Snyder <wsnyder@wsnyder.org>
2023-05-07 15:08:44 -0400

    Tests: Improve randc tests

--- test_regress/t/t_randc.pl
+++ test_regress/t/t_randc.pl
@@ -8,16 +8,11 @@ if (!$::Driver) { use FindBin; exec("$FindBin::Bin/bootstrap.pl", @ARGV, $0); di
 # Version 2.0.
 # SPDX-License-Identifier: LGPL-3.0-only OR Artistic-2.0
 
-scenarios(vlt => 1);
+scenarios(linter => 1);
 
-top_filename("t/t_randc.v");
-
-compile(
-    verilator_flags2 => ['-Wno-RANDC -DTEST_IGNORE_RANDC'],
-    );
-
-execute(
-    check_finished => 1,
+lint(
+    fails => $Self->{vlt_all},
+    expect_filename => $Self->{golden_filename},
     );
 
 ok(1);
--- test_regress/t/t_randc_oversize_bad.v
+++ test_regress/t/t_randc_oversize_bad.v
@@ -1,11 +1,11 @@
 // DESCRIPTION: Verilator: Verilog Test module
 //
 // This file ONLY is placed under the Creative Commons Public Domain, for
-// any use, without warranty, 2019 by Wilson Snyder.
+// any use, without warranty, 2023 by Wilson Snyder.
 // SPDX-License-Identifier: CC0-1.0
 
 class Cls;
-   randc int i;
+   randc bit [33:0] i;
 endclass
 
 module t (/*AUTOARG*/);
