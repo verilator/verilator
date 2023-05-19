883d81809ee885de45b1aee9aa0a3db4e8852261
Aleksander Kiryk <akiryk@antmicro.com>
2023-05-19 13:37:27 +0200

    Warn on usage of whole std::process without timing

--- src/V3Width.cpp
+++ src/V3Width.cpp
@@ -3510,10 +3510,7 @@ private:
                     AstPackage* const packagep = getItemPackage(classp);
                     // Check if it's std
                     if (packagep && packagep->name() == "std") {
-                        if (classp->name() == "process"
-                            && (nodep->name() == "self" || nodep->name() == "status"
-                                || nodep->name() == "kill" || nodep->name() == "await"
-                                || nodep->name() == "suspend" || nodep->name() == "resume")) {
+                        if (classp->name() == "process") {
                             methodCallWarnTiming(nodep, "process");
                         } else if (classp->name() == "semaphore" && nodep->name() == "get") {
                             methodCallWarnTiming(nodep, "semaphore");
