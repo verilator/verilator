42beaf467c4ed381085ae49bd210fb79a58cb937
Aleksander Kiryk <akiryk@antmicro.com>
2023-05-25 11:06:37 -0700

    Tests: Add incomplete type definition test (#4230)

---
+++ test_regress/t/t_no_typedef_bad.v
@@ -0,0 +1,11 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain, for
+// any use, without warranty, 2023 by Antmicro Ltd.
+// SPDX-License-Identifier: CC0-1.0
+
+typedef sometype;
+
+module t(/*AUTOARG*/);
+   sometype p;
+endmodule
