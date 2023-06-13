1986e5a2435fc0b59e24f9d27bb7e57280760ced
Krzysztof Boronski <kboronski@antmicro.com>
2023-06-13 20:19:08 +0200

    Fixes to tests
    Signed-off-by: Krzysztof Boronski <kboronski@antmicro.com>

--- test_regress/t/t_clocking_sched_timing_forkproc.pl
+++ test_regress/t/t_clocking_sched_timing_forkproc.pl
@@ -2,26 +2,20 @@
 if (!$::Driver) { use FindBin; exec("$FindBin::Bin/bootstrap.pl", @ARGV, $0); die; }
 # DESCRIPTION: Verilator: Verilog Test driver/expect definition
 #
-# Copyright 2022 by Antmicro Ltd. This program is free software; you
+# Copyright 2023 by Wilson Snyder. This program is free software; you
 # can redistribute it and/or modify it under the terms of either the GNU
 # Lesser General Public License Version 3 or the Perl Artistic License
 # Version 2.0.
 # SPDX-License-Identifier: LGPL-3.0-only OR Artistic-2.0
-
 scenarios(linter => 1);
 
-if (!$Self->have_coroutines) {
-    skip("No coroutine support");
-}
-else {
-    top_filename("t/t_clocking_sched.v");
+top_filename("t/t_clocking_sched.v");
 
-    lint(
-        verilator_flags2 => ["--timing", "--ftaskify-all-forked"],
-        expect_filename => $Self->{golden_filename},
-        fails => 1,
-        );
-}
+lint(
+    verilator_flags2 => ["--timing", "--ftaskify-all-forked"],
+    expect_filename => $Self->{golden_filename},
+    fails => 1,
+    );
 
 ok(1);
 1;
