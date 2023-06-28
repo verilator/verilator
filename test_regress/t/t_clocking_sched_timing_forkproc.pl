01d9f8bbcfd24267a94cc5e1489ae0c86c5f77fc
Krzysztof Boronski <kboronski@antmicro.com>
2023-06-28 16:44:36 +0200

    Update t_clocking_sched_timing_forkproc, so it's no longer expected to fail
    Signed-off-by: Krzysztof Boronski <kboronski@antmicro.com>

--- test_regress/t/t_clocking_sched_timing_forkproc.pl
+++ test_regress/t/t_clocking_sched_timing_forkproc.pl
@@ -7,14 +7,18 @@ if (!$::Driver) { use FindBin; exec("$FindBin::Bin/bootstrap.pl", @ARGV, $0); di
 # Lesser General Public License Version 3 or the Perl Artistic License
 # Version 2.0.
 # SPDX-License-Identifier: LGPL-3.0-only OR Artistic-2.0
-scenarios(linter => 1);
+scenarios(vlt => 1);
 
 top_filename("t/t_clocking_sched.v");
 
-lint(
-    verilator_flags2 => ["--timing", "--ftaskify-all-forked"],
-    expect_filename => $Self->{golden_filename},
-    fails => 1,
+compile(
+    timing_loop => 1,
+    verilator_flags2 => ["--timing"],
+    );
+
+execute(
+    check_finished => 1,
+    expect_filename => $Self->{golden_filename}
     );
 
 ok(1);
