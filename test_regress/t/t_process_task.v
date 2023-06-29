9b32e0b6caacd83c62b1a048f80d497a790dc3a1
Aleksander Kiryk <akiryk@antmicro.com>
2023-06-29 10:38:54 +0200

    Add missing copyright note

--- test_regress/t/t_process_task.v
+++ test_regress/t/t_process_task.v
@@ -1,3 +1,9 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain, for
+// any use, without warranty, 2023 by Antmicro Ltd.
+// SPDX-License-Identifier: CC0-1.0
+
 module t();
    std::process proc;
    logic clk = 0;
