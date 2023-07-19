844c79e1fa4b64cdcad986151a9f0644f216ee28
Aleksander Kiryk <akiryk@antmicro.com>
2023-07-19 10:46:21 +0200

    Cast non-booleans in `wait`

--- src/V3Width.cpp
+++ src/V3Width.cpp
@@ -5690,7 +5690,8 @@ private:
         }
         if (nodep->fileline()->timingOn()) {
             if (v3Global.opt.timing().isSetTrue()) {
-                userIterate(nodep->condp(), WidthVP{SELF, PRELIM}.p());
+                iterateCheckBool(nodep, "Wait", nodep->condp(),
+                                 BOTH);  // it's like an if() condition.
                 iterateNull(nodep->stmtsp());
                 return;
             } else if (v3Global.opt.timing().isSetFalse()) {
