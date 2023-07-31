365d6365c0726b5c19b90ae84aa53c613eaf7214
Ryszard Rozak <rrozak@antmicro.com>
2023-07-31 14:49:51 +0200

    Use a queue in some tests
    Signed-off-by: Ryszard Rozak <rrozak@antmicro.com>

--- test_regress/t/t_stream_dynamic.v
+++ test_regress/t/t_stream_dynamic.v
@@ -10,8 +10,9 @@
 module t (/*AUTOARG*/);
    initial begin
       bit arr[];
-      bit [1:0] arr2[];
-      bit [5:0] arr6[];
+      bit [1:0] arr2[$];
+      bit [4:0] arr5[];
+      bit [5:0] arr6[$];
       string v;
       bit [5:0] bit6 = 6'b111000;
 
@@ -28,10 +29,10 @@ module t (/*AUTOARG*/);
       v = $sformatf("%p", arr2); `checks(v, "'{'h3, 'h2, 'h0} ");
 
       { >> bit [5:0] {arr6} } = bit6;
-      if (arr6[0] != bit6) $stop;
+      v = $sformatf("%p", arr6); `checks(v, "'{'h38} ");
 
       { << bit [5:0] {arr6} } = bit6;
-      if (arr6[0] != bit6) $stop;
+      v = $sformatf("%p", arr6); `checks(v, "'{'h38} ");
 
       $write("*-* All Finished *-*\n");
       $finish;
