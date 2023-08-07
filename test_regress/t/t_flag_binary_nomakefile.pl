6d8bde92bb6a6ab627462fe6a50a23b41b616f00
Ethan Sifferman <ethan@sifferman.dev>
2023-08-07 13:02:59 -0700

    added test for --binary

---
+++ test_regress/t/t_flag_binary_nomakefile.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/env perl
+if (!$::Driver) { use FindBin; exec("$FindBin::Bin/bootstrap.pl", @ARGV, $0); die; }
+# DESCRIPTION: Verilator: Verilog Test driver/expect definition
+#
+# Copyright 2019 by Wilson Snyder. This program is free software; you
+# can redistribute it and/or modify it under the terms of either the GNU
+# Lesser General Public License Version 3 or the Perl Artistic License
+# Version 2.0.
+# SPDX-License-Identifier: LGPL-3.0-only OR Artistic-2.0
+
+scenarios(vlt => 1);
+
+run(cmd => ["../bin/verilator",
+            "-E",
+            "--binary",
+            "t/t_timing_always.v",
+    ]);
+
+run(cmd => ["../bin/verilator",
+            "--dpi-hdr-only",
+            "--binary",
+            "t/t_timing_always.v",
+    ]);
+
+run(cmd => ["../bin/verilator",
+            "--lint-only",
+            "--binary",
+            "t/t_timing_always.v",
+    ]);
+
+run(cmd => ["../bin/verilator",
+            "--xml-only",
+            "--binary",
+            "t/t_timing_always.v",
+    ]);
+
+ok(1);
+1;
