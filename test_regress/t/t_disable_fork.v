105db70bf3601beb6b0e0d95a7915fde9d8a9943
Aleksander Kiryk <akiryk@antmicro.com>
2023-08-17 15:52:46 +0200

    Extend disable fork test

--- test_regress/t/t_disable_fork.v
+++ test_regress/t/t_disable_fork.v
@@ -6,17 +6,40 @@
 
 `define N 5
 
+class Cls;
+   task runforks(integer n);
+      for (integer i = 0; i < `N; i++) fork
+         #1 $stop;
+      join_none
+   endtask
+endclass
+
 module t;
-   integer n = 0;
+   Cls cls = new;
 
    initial begin
+      // run forks
       for (integer i = 0; i < `N; i++) fork
-            #1 n = n + 1;
+         #1 $stop;
       join_none
+
+      // run forks inside a method
+      cls.runforks(`N);
+
+      // run forks in forks
+      for (integer i = 0; i < `N; i++) fork
+         for (integer j = 0; j < `N; j++) fork
+            #1 $stop;
+         join_none
+      join_none
+
+      // kill them all
       disable fork;
-      #2;
-      if (n > 0) $stop;
-      $write("*-* All Finished *-*\n");
-      $finish;
+
+      // check if we can still fork
+      fork
+         #2 $write("*-* All Finished *-*\n");
+         #3 $finish;
+      join_none
    end
 endmodule
