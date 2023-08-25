00d63883f887cb24f7b0404d97dca709cf2edd9e
Wilson Snyder <wsnyder@wsnyder.org>
2023-08-25 08:21:15 -0400

    Fix false MULTITOP on bound interfaces (#4438).

--- src/V3LinkLevel.cpp
+++ src/V3LinkLevel.cpp
@@ -53,7 +53,7 @@ void V3LinkLevel::modSortByLevel() {
     ModVec tops;  // Top level modules
     for (AstNodeModule* nodep = v3Global.rootp()->modulesp(); nodep;
          nodep = VN_AS(nodep->nextp(), NodeModule)) {
-        if (nodep->level() <= 2) tops.push_back(nodep);
+        if (nodep->level() <= 2 && !VN_IS(nodep, NotFoundModule)) tops.push_back(nodep);
         mods.push_back(nodep);
     }
     if (tops.size() >= 2) {
