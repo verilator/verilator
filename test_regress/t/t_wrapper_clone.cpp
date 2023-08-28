920b196f1a8e2ac9bb5a5e4a935ddaa829698a6f
Yinan Xu <xuyinan@ict.ac.cn>
2023-08-28 21:07:54 +0800

    Update testcase for atClone

---
+++ test_regress/t/t_wrapper_clone.cpp
@@ -0,0 +1,67 @@
+//
+// DESCRIPTION: Verilator: Verilog Test module for the atClone API
+//
+// This file ONLY is placed under the Creative Commons Public Domain, for
+// any use, without warranty, 2020-2021 by Andreas Kuster.
+// SPDX-License-Identifier: CC0-1.0
+//
+
+#include <verilated.h>
+
+#include <unistd.h>
+#include <sys/wait.h>
+
+// These require the above. Comment prevents clang-format moving them
+#include "TestCheck.h"
+
+#include VM_PREFIX_INCLUDE
+
+// Check we properly define the version integer
+#if VERILATOR_VERSION_INTEGER < 5015000  // Added in 5.015
+#error "VERILATOR_VERSION_INTEGER not set"
+#endif
+
+double sc_time_stamp() { return 0; }
+
+void single_cycle(VM_PREFIX* top) {
+    top->clock = 1;
+    top->eval();
+
+    top->clock = 0;
+    top->eval();
+}
+
+
+int main(int argc, char** argv) {
+    std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
+    std::unique_ptr<VM_PREFIX> top{new VM_PREFIX{contextp.get()}};
+
+    top->reset = 1;
+    top->is_parent = 0;
+    for (int i = 0; i < 5; i++) {
+        single_cycle(top.get());
+    }
+
+    top->reset = 0;
+    while (!contextp->gotFinish()) {
+        single_cycle(top.get());
+
+        if (top->do_clone) {
+            int pid = fork();
+            if (pid < 0) {
+                printf("fork failed\n");
+            }
+            else if (pid == 0) {
+                printf("child: here we go\n");
+                top->atClone();
+            }
+            else {
+                while (wait(NULL) > 0);
+                printf("parent: here we go\n");
+                top->is_parent = 1;
+            }
+        }
+    }
+
+    return 0;
+}
---
+++ test_regress/t/t_wrapper_clone.v
@@ -0,0 +1,40 @@
+// DESCRIPTION: Verilator: Verilog Test module for the atClone API
+//
+// This model counts from 0 to 8. It forks a child process (in C++) at 6
+// and waits for the child to simulate and exit for resumption (of the parent).
+//
+// This file ONLY is placed under the Creative Commons Public Domain, for
+// any use, without warranty, 2020-2021 by Andreas Kuster.
+// SPDX-License-Identifier: CC0-1.0
+
+module top(
+  input clock,
+  input reset,
+  input is_parent,
+  output do_clone
+);
+
+reg [3:0] counter;
+
+assign do_clone = counter == 4'h6;
+
+always @(posedge clock) begin
+  if (reset)
+    counter <= 4'h0;
+  else begin
+    counter <= counter + 4'h1;
+    $write("counter = %d\n", counter);
+    $fflush();
+  end
+
+  if (counter[3]) begin
+    if (is_parent) begin
+      $write("*-* All Finished *-*\n");
+      $fflush();
+    end
+    $finish(0);
+  end
+end
+
+
+endmodule
