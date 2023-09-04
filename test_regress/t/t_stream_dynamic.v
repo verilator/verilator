6e9570446e79f99db6709d5861415a025b353df9
Ryszard Rozak <rrozak@antmicro.com>
2023-09-04 13:54:25 +0200

    Add tests of left stream
    Signed-off-by: Ryszard Rozak <rrozak@antmicro.com>

--- test_regress/t/t_stream_dynamic.v
+++ test_regress/t/t_stream_dynamic.v
@@ -5,6 +5,7 @@
 // SPDX-License-Identifier: CC0-1.0
 
 `define stop $stop
+`define checkh(gotv,expv) do if ((gotv) !== (expv)) begin $write("%%Error: %s:%0d:  got='h%x exp='h%x\n", `__FILE__,`__LINE__, (gotv), (expv)); $stop; end while(0);
 `define checks(gotv,expv) do if ((gotv) !== (expv)) begin $write("%%Error: %s:%0d:  got='%s' exp='%s'\n", `__FILE__,`__LINE__, (gotv), (expv)); `stop; end while(0);
 
 module t (/*AUTOARG*/);
@@ -15,6 +16,7 @@ module t (/*AUTOARG*/);
       bit [5:0] arr6[$];
       string v;
       bit [5:0] bit6 = 6'b111000;
+      bit [5:0] ans;
 
       { >> bit {arr}} = bit6;
       v = $sformatf("%p", arr); `checks(v, "'{'h0, 'h0, 'h0, 'h1, 'h1, 'h1} ");
@@ -22,17 +24,26 @@ module t (/*AUTOARG*/);
       { << bit {arr}} = bit6;
       v = $sformatf("%p", arr); `checks(v, "'{'h1, 'h1, 'h1, 'h0, 'h0, 'h0} ");
 
+      ans = { << bit {arr} };
+      `checkh(ans, bit6);
+
       { >> bit[1:0] {arr2}} = bit6;
       v = $sformatf("%p", arr2); `checks(v, "'{'h0, 'h2, 'h3} ");
 
       { << bit[1:0] {arr2}} = bit6;
       v = $sformatf("%p", arr2); `checks(v, "'{'h3, 'h2, 'h0} ");
 
+      ans = { << bit[1:0] {arr2} };
+      `checkh(ans, bit6);
+
       { >> bit [5:0] {arr6} } = bit6;
       v = $sformatf("%p", arr6); `checks(v, "'{'h38} ");
 
       { << bit [5:0] {arr6} } = bit6;
       v = $sformatf("%p", arr6); `checks(v, "'{'h38} ");
+
+      ans = { << bit[5:0] {arr6} };
+      `checkh(ans, bit6);
 
       $write("*-* All Finished *-*\n");
       $finish;
