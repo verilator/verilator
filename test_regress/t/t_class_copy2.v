1c738c6b83867d220f2338f2ea9eaa4e9e3d20b0
Ryszard Rozak <rrozak@antmicro.com>
2023-10-05 13:21:33 +0200

    Fix object destruction after a copy constructor (#4540) (#4541)

---
+++ test_regress/t/t_class_copy2.v
@@ -0,0 +1,36 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain, for
+// any use, without warranty, 2023 by Antmicro Ltd.
+// SPDX-License-Identifier: CC0-1.0
+
+`define stop $stop
+`define checkh(gotv,expv) do if ((gotv) !== (expv)) begin $write("%%Error: %s:%0d:  got='h%x exp='h%x\n", `__FILE__,`__LINE__, (gotv), (expv)); $stop; end while(0);
+
+class Cls;
+   bit x = 1;
+endclass
+
+module t (/*AUTOARG*/);
+   Cls obj1;
+   Cls obj2;
+
+   initial begin
+      obj1 = new;
+      `checkh(obj1.x, 1);
+
+      obj1.x = 0;
+      obj2 = new obj1;
+      `checkh(obj2.x, 0);
+
+      obj2.x = 1;
+      `checkh(obj1.x, 0);
+      `checkh(obj2.x, 1);
+
+      obj2.x = 0;
+      `checkh(obj2.x, 0);
+
+      $write("*-* All Finished *-*\n");
+      $finish;
+   end
+endmodule
