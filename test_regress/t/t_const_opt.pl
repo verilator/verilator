7ff05331dbf00908c53ceea4a940bea27879bd49
Ryszard Rozak <rrozak@antmicro.com>
2023-10-10 16:26:49 +0200

    Update a test
    Signed-off-by: Ryszard Rozak <rrozak@antmicro.com>

--- test_regress/t/t_const_opt.pl
+++ test_regress/t/t_const_opt.pl
@@ -20,7 +20,7 @@ execute(
     );
 
 if ($Self->{vlt}) {
-    file_grep($Self->{stats}, qr/Optimizations, Const bit op reduction\s+(\d+)/i, 39);
+    file_grep($Self->{stats}, qr/Optimizations, Const bit op reduction\s+(\d+)/i, 40);
 }
 ok(1);
 1;
