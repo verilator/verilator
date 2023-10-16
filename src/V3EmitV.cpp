6937264ac43c70940b547346e4ac9c86ce05e6b1
Aleksander Kiryk <akiryk@antmicro.com>
2023-10-16 17:16:45 +0200

    Fix Verilog emission of C expressions

--- src/V3EmitV.cpp
+++ src/V3EmitV.cpp
@@ -412,7 +412,7 @@ class EmitVBaseVisitorConst VL_NOT_FINAL : public EmitCBaseVisitorConst {
     void visit(AstCExpr* nodep) override {
         putfs(nodep, "$_CEXPR(");
         iterateAndNextConstNull(nodep->exprsp());
-        puts(");\n");
+        puts(")");
     }
     void visit(AstUCStmt* nodep) override {
         putfs(nodep, "$c(");
