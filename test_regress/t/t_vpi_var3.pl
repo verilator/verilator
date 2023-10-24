23f76bccd6f0f212b8f2540febb1eae18f92b8fa
Marlon James <marlon.james@gmail.com>
2023-10-23 21:49:25 -0700

    VPI: Add testing of top-level ports

---
+++ test_regress/t/t_vpi_var3.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/env perl
+if (!$::Driver) { use FindBin; exec("$FindBin::Bin/bootstrap.pl", @ARGV, $0); die; }
+# DESCRIPTION: Verilator: Verilog Test driver/expect definition
+#
+# Copyright 2023 by Wilson Snyder. This program is free software; you
+# can redistribute it and/or modify it under the terms of either the GNU
+# Lesser General Public License Version 3 or the Perl Artistic License
+# Version 2.0.
+# SPDX-License-Identifier: LGPL-3.0-only OR Artistic-2.0
+
+scenarios(simulator => 1);
+
+pli_filename("t_vpi_var.cpp");
+compile(
+    make_top_shell => 0,
+    make_main => 0,
+    make_pli => 1,
+    sim_time => 2100,
+    iv_flags2 => ["-g2005-sv -D USE_VPI_NOT_DPI -DWAVES -DT_VPI_VAR3"],
+    v_flags2 => ["+define+USE_VPI_NOT_DPI"],
+    verilator_flags2 => ["--exe --vpi --no-l2name --public-flat-rw $Self->{t_dir}/t_vpi_var.cpp"],
+    );
+
+execute(
+    use_libvpi => 1,
+    check_finished => 1,
+    all_run_flags => ['+PLUS +INT=1234 +STRSTR']
+    );
+
+ok(1);
+1;
