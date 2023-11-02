9305fdb67e93fb5f68ed8d5834d1e3218ce231f0
Geza Lore <gezalore@gmail.com>
2023-11-02 20:25:12 +0000

    Remove dead code from V3EmitV

--- src/V3EmitV.h
+++ src/V3EmitV.h
@@ -30,8 +30,6 @@ class AstSenTree;
 class V3EmitV final {
 public:
     static void verilogForTree(const AstNode* nodep, std::ostream& os = std::cout);
-    static void verilogPrefixedTree(const AstNode* nodep, std::ostream& os, const string& prefix,
-                                    int flWidth, AstSenTree* domainp, bool user3mark);
     static void emitvFiles();
     static void debugEmitV(const string& filename);
 };
