9ea0914232b02ed91a6647bfea40afeb584d08a5
Shupei Fan <dymarkfan@outlook.com>
2023-12-22 13:22:17 +0800

    fix dist error (no code change)

--- test_regress/t/t_flag_lib_dpi_main.cpp
+++ test_regress/t/t_flag_lib_dpi_main.cpp
@@ -1,4 +1,12 @@
-// Verilated -*- C++ -*-
+// -*- mode: C++; c-file-style: "cc-mode" -*-
+//*************************************************************************
+//
+// This file ONLY is placed under the Creative Commons Public Domain, for
+// any use, without warranty, 2023 by Shupei Fan.
+// SPDX-License-Identifier: CC0-1.0
+//
+//*************************************************************************
+//
 // DESCRIPTION: main() calling loop, created with Verilator --main
 
 #include "verilated.h"
