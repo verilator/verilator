68c97921793f3c456eec8a10569a0c6459404327
Larry Doolittle <LRDoolittle@lbl.gov>
2024-01-12 08:04:02 -0800

    Fix issue #4820 - maybe-uninitialized when building user code

--- include/verilated_fst_c.cpp
+++ include/verilated_fst_c.cpp
@@ -174,7 +174,7 @@ void VerilatedFst::declare(uint32_t code, const char* name, int dtypenum,
 
     if (dtypenum > 0) fstWriterEmitEnumTableRef(m_fst, m_local2fstdtype[dtypenum]);
 
-    fstVarDir varDir;
+    fstVarDir varDir = FST_VD_IMPLICIT;
     switch (direction) {
     case VerilatedTraceSigDirection::INOUT: varDir = FST_VD_INOUT; break;
     case VerilatedTraceSigDirection::OUTPUT: varDir = FST_VD_OUTPUT; break;
