6d03159f0008f04d7796e26809bc66c6521fcb9e
Krzysztof Bieganski <kbieganski@antmicro.com>
2024-02-06 12:10:29 +0100

    Fix timing with expr on assign LHS
    Signed-off-by: Krzysztof Bieganski <kbieganski@antmicro.com>

--- src/V3SchedTiming.cpp
+++ src/V3SchedTiming.cpp
@@ -247,7 +247,6 @@ TimingKit prepareTiming(AstNetlist* const netlistp) {
         void visit(AstExprStmt* nodep) override { iterateChildren(nodep); }
 
         //--------------------
-        void visit(AstNodeExpr*) override {}  // Accelerate
         void visit(AstNode* nodep) override { iterateChildren(nodep); }
 
     public:
