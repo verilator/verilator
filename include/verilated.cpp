07b74e5545cd2dc1ba9e0f5e63b795a5e3e4d4e7
Wilson Snyder <wsnyder@wsnyder.org>
2024-03-10 11:20:25 -0400

    Fix suggestion of ulimit -s (#4956)

--- include/verilated.cpp
+++ include/verilated.cpp
@@ -3031,7 +3031,7 @@ void Verilated::stackCheck(QData needSize) VL_MT_UNSAFE {
     // if the user follows the suggestions
     if (VL_UNLIKELY(haveSize && needSize && haveSize < (needSize + needSize / 2))) {
         VL_PRINTF_MT("%%Warning: System has stack size %" PRIu64 " kb"
-                     " which may be too small; suggest 'ulimit -c %" PRIu64 "' or larger\n",
+                     " which may be too small; suggest 'ulimit -s %" PRIu64 "' or larger\n",
                      haveSize / 1024, (needSize * 2) / 1024);
     }
 #else
