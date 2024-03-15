71775eae4c775412daca9c97b8b4a434fe73885e
Wilson Snyder <wsnyder@wsnyder.org>
2024-03-15 10:32:31 -0400

    sanitize

--- ci/ci-script.bash
+++ ci/ci-script.bash
@@ -85,7 +85,8 @@ elif [ "$CI_BUILD_STAGE_NAME" = "test" ]; then
   fi
 
   # Run sanitize on Ubuntu 22.04 only
-  [ "$CI_RUNS_ON" = 'ubuntu-22.04' ] && sanitize='--sanitize' || sanitize=''
+  #[ "$CI_RUNS_ON" = 'ubuntu-22.04' ] && sanitize='--sanitize' || sanitize=''
+  sanitize=''
 
   TEST_REGRESS=test_regress
   if [ "$CI_RELOC" == 1 ]; then
