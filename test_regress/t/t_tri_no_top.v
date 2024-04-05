79cc0b6efe5907a2c5b1a5fb24e37240c1ddf9d8
Paul Wright <paul.wright@analog.com>
2024-04-05 17:33:20 +0100

    Updated whitespace

--- test_regress/t/t_tri_no_top.v
+++ test_regress/t/t_tri_no_top.v
@@ -4,7 +4,7 @@
 // any use, without warranty, 2024 by Paul Wright.
 // SPDX-License-Identifier: CC0-1.0
 
-// The module t_tri_top_en_out is used to test that we can 
+// The module t_tri_top_en_out is used to test that we can
 // force verilator to include __en and __out variables for
 // inouts. This test checks that the tests within that module
 // pass. They should pass regardless of the presence of C or
@@ -18,25 +18,25 @@ module
    timeprecision 1ps;
 
    wire single_bit_io;
-   
+
    wire bidir_single_bit_io;
-   
+
    wire [63:0] bus_64_io;
-   
+
    wire [63:0] bidir_bus_64_io;
-   
+
    wire [127:0] bus_128_io;
-   
+
    wire [127:0] bidir_bus_128_io;
-   
+
    reg [3:0]     drv_en;
-   
+
    reg         test_en;
-   
+
    wire     loop_done;
-   
+
    wire     sub_io;
-   
+
    t_tri_top_en_out
      t_tri_top_en_out
        (.*);
