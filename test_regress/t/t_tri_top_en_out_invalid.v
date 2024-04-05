037425461b81dea9c1ce2daf4ecf86bca46248bf
Paul Wright <paul.wright@analog.com>
2024-04-05 18:21:11 +0100

    Added the missing copyright

--- test_regress/t/t_tri_top_en_out_invalid.v
+++ test_regress/t/t_tri_top_en_out_invalid.v
@@ -1,2 +1,10 @@
+// DESCRIPTION: Verilator: Verilog Test module
+// 
+// This file ONLY is placed under the Creative Commons Public Domain, for
+// any use, without warranty, 2024 by Paul Wright.
+// SPDX-License-Identifier: CC0-1.0
+//
+// A submodule to ensure that __en and __out propagate upwards
+// This version of the test should fail
 `define T_TRI_TOP_NAME t_tri_top_en_out_invalid
 `include "t_tri_top_en_out.v"
