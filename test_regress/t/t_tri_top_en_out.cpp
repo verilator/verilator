eec3808ab1721fdfa35d4b33e7d465af327f722e
Paul Wright <paul.wright@analog.com>
2024-04-06 21:17:57 +0100

    Using TEST_CHECK and removed gotFinish check

--- test_regress/t/t_tri_top_en_out.cpp
+++ test_regress/t/t_tri_top_en_out.cpp
@@ -5,27 +5,19 @@
 // SPDX-License-Identifier: CC0-1.0
 
 #include "verilated.h"
-#ifdef T_TRI_TOP_EN_OUT_INVALID
-#include "Vt_tri_top_en_out_invalid.h"
-#else
 #include "Vt_tri_top_en_out.h"
-#endif
-#include <cstdio>
 #include <ctime>
+#include "TestCheck.h"
 
 int main(int argc, char** argv, char**) {
-    int error_cnt;
+    int errors;
     // Setup context, defaults, and parse command line
     Verilated::debug(0);
     const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
     contextp->commandArgs(argc, argv);
     // Construct the Verilated model, from Vtop.h generated from Verilating
-#ifdef T_TRI_TOP_EN_OUT_INVALID
-    const std::unique_ptr<Vt_tri_top_en_out_invalid> topp{
-        new Vt_tri_top_en_out_invalid{contextp.get()}};
-#else
     const std::unique_ptr<Vt_tri_top_en_out> topp{new Vt_tri_top_en_out{contextp.get()}};
-#endif
+
     // Initial input
     topp->drv_en = 0;
     topp->single_bit_io = rand() & 1;
@@ -43,19 +35,7 @@ int main(int argc, char** argv, char**) {
     topp->sub_io = rand() & 1;
     topp->test_en = 1;
 
-    error_cnt = 0;
-
-#ifdef T_TRI_TOP_EN_OUT_INVALID
-    // These should make the compile fail
-    // These variables should not be visible
-    //
-    // Use the + operator to satisfy the static code checks
-    // Variable reassigned value before old value used.
-    // These variables will never be used, as the compile should
-    // fail!
-    error_cnt += topp->internal_sub_io__en;
-    error_cnt += topp->internal_sub_io__out;
-#endif
+    errors = 0;
 
     // Simulate until $finish
     while (!contextp->gotFinish()) {
@@ -107,55 +87,23 @@ int main(int argc, char** argv, char**) {
             // Check the expected __en output
 
             if (topp->drv_en & 0x1) {
-                if (topp->sub_io__en != 1) {
-                    printf("%%Error:(cpp):: sub_io__en = %d expect 1\n", topp->sub_io__en);
-                    error_cnt++;
-                }
-                if (topp->bidir_single_bit_io__en != 1) {
-                    printf("%%Error:(cpp):: bidir_single_bit_io__en = %d expect 1\n",
-                           topp->bidir_single_bit_io__en);
-                    error_cnt++;
-                }
+		TEST_CHECK_EQ(uint64_t(topp->sub_io__en), 1);
+                TEST_CHECK_EQ(uint64_t(topp->bidir_single_bit_io__en), 1);
             } else {
-                if (topp->sub_io__en != 0) {
-                    printf("%%Error:(cpp):: sub_io__en = %d expect 0\n", topp->sub_io__en);
-                    error_cnt++;
-                }
-                if (topp->bidir_single_bit_io__en != 0) {
-                    printf("%%Error:(cpp):: bidir_single_bit_io__en = %d expect 0\n",
-                           topp->bidir_single_bit_io__en);
-                    error_cnt++;
-                }
+                TEST_CHECK_EQ(uint64_t(topp->sub_io__en), 0);
+                TEST_CHECK_EQ(uint64_t(topp->bidir_single_bit_io__en), 0);
             }
 
             for (int i = 0; i < 4; i++) {
                 // __en enabled?
                 if ((topp->drv_en & (1 << i)) != 0) {
-                    if ((topp->bidir_bus_64_io__en >> (i * 16) & 0xffff) != 0xffff) {
-                        printf("%%Error:(cpp):: bidir_bus_64_io__en not as expected %x (%d) %x\n",
-                               (unsigned int)topp->bidir_bus_64_io__en, i,
-                               (unsigned int)(0xffff << i));
-                        error_cnt++;
-                    }
-                    if (topp->bidir_bus_128_io__en[i] != 0xffffffff) {
-                        printf("%%Error:(cpp):: bidir_bus_128_io__en not as expected %x (%d) %x\n",
-                               (unsigned int)topp->bidir_bus_128_io__en[i], i, 0xffffffff);
-                        error_cnt++;
-                    }
+                    TEST_CHECK_EQ(uint64_t(topp->bidir_bus_64_io__en >> (i * 16) & 0xffff), 0xffff);
+                    TEST_CHECK_EQ(uint64_t(topp->bidir_bus_128_io__en[i]), 0xffffffff);
                 }
                 // __en not enabled
                 else {
-                    if ((topp->bidir_bus_64_io__en >> (i * 16) & 0xffff) != 0x0000) {
-                        printf("%%Error:(cpp):: bidir_bus_64_io__en not as expected %x (%d) %x\n",
-                               (unsigned int)topp->bidir_bus_64_io__out, i,
-                               (unsigned int)(0xffff << i));
-                        error_cnt++;
-                    }
-                    if (topp->bidir_bus_128_io__en[i] != 0x00000000) {
-                        printf("%%Error:(cpp):: bidir_bus_128_io__en not as expected %x (%d) %x\n",
-                               (unsigned int)topp->bidir_bus_128_io__en[i], i, 0);
-                        error_cnt++;
-                    }
+                    TEST_CHECK_EQ(uint64_t(topp->bidir_bus_64_io__en >> (i * 16) & 0xffff), 0x0000);
+                    TEST_CHECK_EQ(uint64_t(topp->bidir_bus_128_io__en[i]), 0x00000000);
                 }
             }  // for
             if (topp->drv_en == 15) {
@@ -208,11 +156,7 @@ int main(int argc, char** argv, char**) {
             topp->bidir_bus_64_io = ~topp->bidir_bus_64_io;
             for (int i = 0; i < 4; i++) { topp->bidir_bus_128_io[i] = ~topp->bidir_bus_128_io[i]; }
         }  // if (loop_done)
-        if (error_cnt != 0) break;
-    }
-    if (!contextp->gotFinish()) {
-        printf("Exiting without $finish; no events left, time was %ld\n", contextp->time());
-        puts("final next:");
+        if (errors != 0) break;
     }
     // Final model cleanup
     topp->final();
