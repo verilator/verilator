2e4cdd59774cca25b431482cc4584c86249cc591
Geza Lore <gezalore@gmail.com>
2024-04-29 14:38:50 +0100

    Refactor V3Delayed to enable algorithmic extension
    No functional change.
    
    This patch is just cleanup with some non-functional changes to enable
    the next patch. Most importantly createDlyOnSet, which implements NBAs
    for arrays, has a new streamlined implementation that does the same
    thing. Some output code is perturbed due to statement/local variable
    insertion order.
    
    Also renamed Vdlyvfoo to VdlyFoo for easier readability of the generated
    code.

--- src/V3SchedVirtIface.cpp
+++ src/V3SchedVirtIface.cpp
@@ -128,7 +128,7 @@ private:
         if (writesToVirtIface(nodep)) {
             // Convert to always, as we have to assign the trigger var
             FileLine* const flp = nodep->fileline();
-            AstAlwaysPost* const postp = new AstAlwaysPost{flp, nullptr, nullptr};
+            AstAlwaysPost* const postp = new AstAlwaysPost{flp};
             nodep->replaceWith(postp);
             postp->addStmtsp(
                 new AstAssign{flp, nodep->lhsp()->unlinkFrBack(), nodep->rhsp()->unlinkFrBack()});
