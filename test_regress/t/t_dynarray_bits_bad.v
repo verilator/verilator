3e35d97319348585cc8a97d79fd68a7f722a3404
Valentin Atepalikhin <valentin.atep@gmail.com>
2024-05-23 15:13:04 +0300

    Added bad test for $bits(dynarray)

---
+++ test_regress/t/t_dynarray_bits_bad.v
@@ -0,0 +1,15 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain, for
+// any use, without warranty, 2020 by Wilson Snyder.
+// SPDX-License-Identifier: CC0-1.0
+
+module t (/*AUTOARG*/);
+
+   integer a[];
+
+   initial begin
+      $display($bits(a)); // Bad
+   end
+
+endmodule
