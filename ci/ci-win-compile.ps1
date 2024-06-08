44acb08ea9cd8a2f2a43ae57f403d63ca28105d3
Wilson Snyder <wsnyder@wsnyder.org>
2024-06-08 19:36:50 -0400

    Tests: Commentary - enable msbuild debug

--- ci/ci-win-compile.ps1
+++ ci/ci-win-compile.ps1
@@ -1,3 +1,15 @@
+# DESCRIPTION: Verilator: CI Windows Power Shell - Compile Verilator
+#
+# Copyright 2024 by Wilson Snyder. This program is free software; you
+# can redistribute it and/or modify it under the terms of either the GNU
+# Lesser General Public License Version 3 or the Perl Artistic License
+# Version 2.0.
+#
+# SPDX-License-Identifier: LGPL-3.0-only OR Artistic-2.0
+################################################################################
+
+Set-PSDebug -Trace 1
+
 if (-Not (Test-Path $PWD/../.ccache/win_bison.exe)) {
 	git clone --depth 1 https://github.com/lexxmark/winflexbison
 	cd winflexbison
@@ -8,6 +20,7 @@ if (-Not (Test-Path $PWD/../.ccache/win_bison.exe)) {
 	cmake --install . --prefix $PWD/../../../.ccache
 	cd ../..
 }
+
 mkdir build
 cd build
 cmake .. --install-prefix $PWD/../install
