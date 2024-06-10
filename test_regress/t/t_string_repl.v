df40f8f857e8408ea249d6768c462396594e807e
Arkadiusz Kozdra <akozdra@antmicro.com>
2024-06-10 14:49:43 +0200

    Non-constant replication in concats
    Signed-off-by: Arkadiusz Kozdra <akozdra@antmicro.com>

--- test_regress/t/t_string_repl.v
+++ test_regress/t/t_string_repl.v
@@ -24,12 +24,12 @@ module t(/*AUTOARG*/
 
    integer cyc = 0;
 
-   string s;
+   string s, s2;
 
    // Test loop
    always @ (posedge clk) begin
       cyc <= cyc + 1;
-      s = {cyc{"*"}};
+      s = {s2, {cyc{"*"}}};
       if (cyc != s.len()) $stop;
       if (cyc == 0 && s != "") $stop;
       if (cyc == 1 && s != "*") $stop;
