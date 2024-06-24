5d6df83600258eba10b172875e75e34a64a30db3
Bartłomiej Chmiel <bchmiel@antmicro.com>
2024-06-24 15:46:27 +0200

    Failing test for clock case
    Signed-off-by: Bartłomiej Chmiel <bchmiel@antmicro.com>

---
+++ test_regress/t/t_var_pins_sc_uint_bool_nomain.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/env perl
+if (!$::Driver) { use FindBin; exec("$FindBin::Bin/bootstrap.pl", @ARGV, $0); die; }
+# DESCRIPTION: Verilator: Verilog Test driver/expect definition
+#
+# Copyright 2024 by Wilson Snyder. This program is free software; you
+# can redistribute it and/or modify it under the terms of either the GNU
+# Lesser General Public License Version 3 or the Perl Artistic License
+# Version 2.0.
+# SPDX-License-Identifier: LGPL-3.0-only OR Artistic-2.0
+
+# This test runs the very first time we've executed Verilator --sc
+# after building so we make sure to run with --gdbbt, so if it dumps we'll
+# get a trace.
+
+scenarios(simulator => 1);
+
+top_filename("t/t_a1_first_cc.v");
+
+$DEBUG_QUIET = "--debug --debugi 0 --gdbbt --no-dump-tree";
+
+compile(
+    verilator_flags2 => [$DEBUG_QUIET, "-sc --trace --pins-sc-uint-bool"],
+    );
+
+execute(
+    check_finished => 1,
+    );
+
+ok(1);
+1;
