63f6e57246e7bd22e616093fdaab0aa9faea33dd
Ryan Ziegler <rzig408@gmail.com>
2024-07-04 17:26:37 -0400

    update copyright

--- test_regress/t/t_emit_accessors.cpp
+++ test_regress/t/t_emit_accessors.cpp
@@ -3,7 +3,7 @@
 // DESCRIPTION: Verilator: Verilog Test module
 //
 // This file ONLY is placed under the Creative Commons Public Domain, for
-// any use, without warranty, 2010 by Wilson Snyder.
+// any use, without warranty, 2024 by Wilson Snyder.
 // SPDX-License-Identifier: CC0-1.0
 
 #include <verilated.h>
