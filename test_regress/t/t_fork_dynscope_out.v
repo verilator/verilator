a1e34d784a04a2e0f4f6d0fca77bdc481e37f808
Krzysztof Bieganski <kbieganski@antmicro.com>
2024-07-11 11:57:29 +0200

    Fix forks capturing non-input ports in tasks
    Signed-off-by: Krzysztof Bieganski <kbieganski@antmicro.com>

---
+++ test_regress/t/t_fork_dynscope_out.v
@@ -0,0 +1,28 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain, for
+// any use, without warranty, 2024 by Antmicro.
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+  bit p = 0, q = 0;
+
+  initial begin
+    t1(p);
+    t2(q);
+    if (p != 1) $stop;
+    if (q != 1) $stop;
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+
+  task t1(inout p);
+    fork
+      p = 1;
+    join_none
+  endtask
+
+  task t2(output q);
+    q <= 1;
+  endtask
+endmodule
