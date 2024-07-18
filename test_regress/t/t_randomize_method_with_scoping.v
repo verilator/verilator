1b93b373fc1c02d6981dc54a6778567e19ba2c54
Arkadiusz Kozdra <akozdra@antmicro.com>
2024-07-18 15:39:49 +0200

    More tests
    Signed-off-by: Arkadiusz Kozdra <akozdra@antmicro.com>

--- test_regress/t/t_randomize_method_with_scoping.v
+++ test_regress/t/t_randomize_method_with_scoping.v
@@ -13,18 +13,32 @@ endclass
 
 class Cls;
    rand int x;
+   rand enum {
+      ONE_Y,
+      TWO_Y
+   } y;
    virtual function int get_x();
       return x;
    endfunction
 endclass
 class SubA extends Cls;
    c1 e = new;
+   enum {
+      AMBIG,
+      ONE_A,
+      TWO_A
+   } en;
    function c1 get_c();
       return e;
    endfunction
 endclass
 class SubB extends Cls;
    c2 e = new;
+   enum {
+      AMBIG,
+      ONE_B,
+      TWO_B
+   } en;
    SubA f = new;
    function c2 get_c();
       return e;
@@ -35,14 +49,21 @@ class SubB extends Cls;
 
       f.x = 4;
       x = 5;
-
       doit = f.randomize() with { x == local::x; };
       if (f.x != x) $stop;
 
       doit &= f.randomize() with { e.c1_f == local::e.c2_f; };
-      doit &= f.randomize() with { x == local::get_x() - get_x(); };
-      doit &= f.randomize() with { x == local::get_c().c2_f - get_c().c1_f; };
+      doit &= f.randomize() with { get_x() == local::get_x(); };
+      doit &= f.randomize() with { get_c().c1_f == local::get_c().c2_f; };
       doit &= f.randomize() with { (get_c).c1_f == (local::get_c).c2_f; };
+      doit &= f.randomize() with { (get_c).c1_f == (local::get_c).c2_f; };
+      doit &= f.randomize() with { y == local::y; };
+      doit &= f.randomize() with { en == AMBIG; };
+      f.en = SubA::ONE_A;
+      doit &= f.randomize() with { en == ONE_A; };
+      doit &= f.randomize() with { local::en == local::AMBIG; };
+      en = ONE_B;
+      doit &= f.randomize() with { local::en == ONE_B; };
    endfunction
 endclass
 
