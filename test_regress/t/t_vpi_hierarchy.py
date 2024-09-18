f4f65a8436404580398cb766153e05846d3bd809
gephaistos <olehmaksa@gmail.com>
2024-09-18 17:51:10 +0300

    Test with iteration over hierarchy using VPI

---
+++ test_regress/t/t_vpi_hierarchy.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# DESCRIPTION: Verilator: Verilog Test driver/expect definition
+#
+# Copyright 2024 by Wilson Snyder. This program is free software; you
+# can redistribute it and/or modify it under the terms of either the GNU
+# Lesser General Public License Version 3 or the Perl Artistic License
+# Version 2.0.
+# SPDX-License-Identifier: LGPL-3.0-only OR Artistic-2.0
+
+import vltest_bootstrap
+
+test.scenarios('simulator')
+
+test.compile(make_top_shell=False,
+             make_main=False,
+             make_pli=True,
+             verilator_flags2=[
+                 "--exe --vpi", test.pli_filename,
+                 test.t_dir + "/TestVpiMain.cpp"
+             ],
+             make_flags=['CPPFLAGS_ADD=-DVL_NO_LEGACY'])
+
+test.execute(use_libvpi=True,
+             expect_filename=test.golden_filename)
+
+test.passes()
