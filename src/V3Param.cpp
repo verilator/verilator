80e20a2bcc1dc5cb6b540d942aacce1038e67c5d
Todd Strader <tstrader@hudson-trading.com>
2024-09-20 10:13:23 -0400

    CR

--- src/V3Param.cpp
+++ src/V3Param.cpp
@@ -348,14 +348,21 @@ class ParamProcessor final {
                 key += "[" + cvtToStr(dtypep->left()) + ":" + cvtToStr(dtypep->right()) + "]";
             }
         } else if (const AstPackArrayDType* const dtypep = VN_CAST(nodep, PackArrayDType)) {
-            key += dtypep->prettyDTypeName(true);
+            key += "[";
+            key += cvtToStr(dtypep->left());
+            key += ":";
+            key += cvtToStr(dtypep->right());
+            key += "] ";
+            key += paramValueString(dtypep->subDTypep());
         } else if (const AstInitArray* const initp = VN_CAST(nodep, InitArray)) {
             key += "{";
             for (auto it : initp->map()) {
-                key += it.second->valuep()->name();
+                key += paramValueString(it.second->valuep());
                 key += ",";
             }
             key += "}";
+        } else if (const AstNodeDType* const dtypep = VN_CAST(nodep, NodeDType)) {
+            key += dtypep->prettyDTypeName(true);
         }
         UASSERT_OBJ(!key.empty(), nodep, "Parameter yielded no value string");
         return key;
--- test_regress/t/t_interface_derived_type.v
+++ test_regress/t/t_interface_derived_type.v
@@ -4,8 +4,22 @@
 // without warranty, 2012 by Iztok Jeras.
 // SPDX-License-Identifier: CC0-1.0
 
-interface intf #(parameter type data_t = bit) ();
+interface intf #(
+    parameter type data_t = bit,
+    parameter int arr[2][4]
+) ();
     data_t data;
+    // TODO -- some kind of issue with multi-dimensional array constness:
+    // %Error: t/t_interface_derived_type.v:12:12: Expecting expression to be constant, but variable isn't const: 'arr'
+    //                                           : ... note: In instance 't.sub16'
+    //    19 |     logic [arr[0][0]-1:0] other_data;
+    //       |            ^~~
+    // `define SHOW_2D_BUG
+    `ifdef SHOW_2D_BUG
+    logic [arr[0][0]-1:0] other_data;
+    `else
+    logic [$bits(data)-1:0] other_data;
+    `endif
 endinterface
 
 module t (/*AUTOARG*/
@@ -21,19 +35,28 @@ module t (/*AUTOARG*/
       $finish;
    end
 
-   sub #(.width(8)) sub8 ();
-   sub #(.width(16)) sub16 ();
+   sub #(.width(8), .arr('{'{8, 2, 3, 4}, '{1, 2, 3, 4}})) sub8 ();
+   sub #(.width(16), .arr('{'{16, 2, 3, 4}, '{1, 2, 3, 4}})) sub16 ();
 
 endmodule
 
-module sub #(parameter int width) ();
+module sub #(
+    parameter int width,
+    parameter int arr[2][4]
+) ();
     typedef struct packed {
-        logic [0:0] [width-1:0] field;
+        logic [3:3] [0:0] [width-1:0] field;
     } user_type_t;
 
-    intf #(.data_t(user_type_t)) the_intf ();
+    intf #(
+        .data_t(user_type_t),
+        .arr(arr)
+    ) the_intf ();
 
     logic [width-1:0] signal;
 
-    always_comb the_intf.data.field = signal;
+    always_comb begin
+        the_intf.data.field = signal;
+        the_intf.other_data = signal;
+    end
 endmodule
