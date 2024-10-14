1df20f7076afd696044ab94b0c68c63d98339731
Wilson Snyder <wsnyder@wsnyder.org>
2024-10-13 21:55:31 -0400

    Remove driver.pl - deprecated earlier with driver.py

--- test_regress/t/t_dist_copyright.py
+++ test_regress/t/t_dist_copyright.py
@@ -42,13 +42,8 @@ EXEMPT_FILES_LIST = """
     test_regress/t/t_flag_f__3.v
     test_regress/t/t_fuzz_eof_bad.v
     test_regress/t/t_incr_void.v
-    test_regress/t/t_timing_trace_fst.pl
     test_regress/t/t_uvm_pkg_all.vh
     test_regress/t/t_uvm_pkg_todo.vh
-    test_regress/t/t_wrapper_context.pl
-    test_regress/t/t_wrapper_context_fst.pl
-    test_regress/t/t_wrapper_context_seq.pl
-    test_regress/t/t_wrapper_del_context_bad.pl
     test_regress/t/tsub/t_flag_f_tsub.v
     test_regress/t/tsub/t_flag_f_tsub_inc.v
     verilator.pc.in
