620eab32d37006468284bdb64f0244a7da4c9741
Andrew Nolte <anolte@hudson-trading.com>
2024-10-31 17:07:57 -0400

    annotate hierarchy

--- src/VlcPoint.h
+++ src/VlcPoint.h
@@ -63,6 +63,7 @@ public:
     // KEY ACCESSORS
     string filename() const { return keyExtract(VL_CIK_FILENAME); }
     string comment() const { return keyExtract(VL_CIK_COMMENT); }
+    string hier() const { return keyExtract(VL_CIK_HIER); }
     string type() const { return keyExtract(VL_CIK_TYPE); }
     string thresh() const { return keyExtract(VL_CIK_THRESH); }  // string as maybe ""
     string linescov() const { return keyExtract(VL_CIK_LINESCOV); }
@@ -98,7 +99,7 @@ public:
     void dumpAnnotate(std::ostream& os, unsigned annotateMin) const {
         os << (ok(annotateMin) ? "+" : "-");
         os << std::setw(6) << std::setfill('0') << count();
-        os << "  point: comment=" << comment();
+        os << "  point: comment=" << comment() << " hier=" << hier();
         os << "\n";
     }
 };
