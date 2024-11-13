50f12242151c54a5082adc6963e5a3225cddd7cb
Zhou Shen <599239118@qq.com>
2024-11-14 00:23:43 +0800

    Add the support of UDP table for the combinatial and sequential logic.

---
+++ test_regress/t/t_nonsequential_udp.v
@@ -0,0 +1,68 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed into the Public Domain, for any use,
+// without warranty, 2015 by Mike Thyer.
+// SPDX-License-Identifier: CC0-1.0
+
+module t (/*AUTOARG*/
+   // Inputs
+   clk
+   );
+   input clk;
+   reg a, b, sel, z;
+   udp_mux2(z, a, b, sel);
+
+   int cycle=0;
+
+   always @(posedge clk) begin
+     cycle <= cycle+1;
+     if (cycle==0) begin
+        a = 0;
+        b = 1;
+        sel = 0;
+     end
+     else if (cycle==1) begin
+        a = 1;
+        b = 1;
+        sel = 0;
+        if (z != 0) $stop;
+     end
+     else if (cycle==2) begin
+        a = 0;
+        b = 1;
+        sel = 0;
+        if (z != 1) $stop;
+     end
+     else if (cycle==3) begin
+        a = 1;
+        b = 0;
+        sel = 0;
+        if (z != 0) $stop;
+     end
+     else if (cycle==4) begin
+        if (z != 1) $stop;
+     end
+     else if (cycle >= 5) begin
+         $write("*-* All Finished *-*\n");
+         $finish;
+     end
+   end
+endmodule
+
+primitive udp_mux2 (z, a, b, sel);
+   output z;
+   input  a, b, sel;
+   table
+      //a b  s   o
+      ?   1  1 : 1 ;
+      ?   0  1 : 0 ;
+      1   ?  0 : 1 ;
+      0   ?  0 : 0 ;
+      1   1  x : 1 ;
+      // Next blank line is intentional for parser
+
+      // Next \ at EOL is intentional for parser
+      0   0  x \
+               : 0 ;
+   endtable
+endprimitive
