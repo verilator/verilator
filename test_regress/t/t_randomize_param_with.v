verilator/verilator

commit 7a04a5b9a8d8cece01ea934625eb556ac00d197a

Internals: Refactor symIterate functions. No functional change intended

src/V3LinkDot.cpp
@@ -2448,15 +2448,30 @@ class LinkDotResolveVisitor final : public VNVisitor {
         // A class reference might be to a class that is later in Ast due to
         // e.g. parmaeterization or referring to a "class (type T) extends T"
         // Resolve it so later Class:: references into its base classes work
-        VL_RESTORER(m_ds);
-        VSymEnt* const srcp = m_statep->getNodeSym(nodep);
-        m_ds.init(srcp);
-        iterate(nodep);
+        symIterateNull(nodep, m_statep->getNodeSym(nodep));
     }
+
     void updateVarUse(AstVar* nodep) {
         // Avoid dotted.PARAM false positive when in a parameter block
         // that is if ()'ed off by same dotted name as another block
         if (nodep && nodep->isParam()) nodep->usedParam(true);
+    }
+
+    void symIterateChildren(AstNode* nodep, VSymEnt* symp) {
+        // Iterate children, changing to given context, with restore to old context
+        VL_RESTORER(m_ds);
+        VL_RESTORER(m_curSymp);
+        m_curSymp = symp;
+        m_ds.init(m_curSymp);
+        iterateChildren(nodep);
+    }
+    void symIterateNull(AstNode* nodep, VSymEnt* symp) {
+        // Iterate node, changing to given context, with restore to old context
+        VL_RESTORER(m_ds);
+        VL_RESTORER(m_curSymp);
+        m_curSymp = symp;
+        m_ds.init(m_curSymp);
+        iterateNull(nodep);
     }
 
 #define LINKDOT_VISIT_START() \
@@ -2494,14 +2509,12 @@ class LinkDotResolveVisitor final : public VNVisitor {
     void visit(AstScope* nodep) override {
         LINKDOT_VISIT_START();
         UINFO(8, indent() << "visit " << nodep << endl);
+        checkNoDot(nodep);
         VL_RESTORER(m_modSymp);
         VL_RESTORER(m_curSymp);
-        {
-            checkNoDot(nodep);
-            m_ds.m_dotSymp = m_curSymp = m_modSymp = m_statep->getScopeSym(nodep);
-            iterateChildren(nodep);
-            m_ds.m_dotSymp = m_curSymp = m_modSymp = nullptr;
-        }
+        m_ds.m_dotSymp = m_curSymp = m_modSymp = m_statep->getScopeSym(nodep);
+        iterateChildren(nodep);
+        m_ds.m_dotSymp = m_curSymp = m_modSymp = nullptr;
     }
     void visit(AstCellInline* nodep) override {
         LINKDOT_VISIT_START();
@@ -2670,6 +2683,8 @@ class LinkDotResolveVisitor final : public VNVisitor {
                     } else {
                         if (m_statep->forPrimary()
                             && m_extendsParam.find(classp) != m_extendsParam.end()) {
+                            UINFO(9, indent() << "deferring until post-V3Param: " << nodep->lhsp()
+                                              << endl);
                             m_ds.m_unresolvedClass = true;
                         } else {
                             const auto baseClassp = cextp->classp();
@@ -2696,6 +2711,7 @@ class LinkDotResolveVisitor final : public VNVisitor {
             }
             if (m_statep->forPrimary() && isParamedClassRef(nodep->lhsp())) {
                 // Dots of paramed classes will be linked after deparameterization
+                UINFO(9, indent() << "deferring until post-V3Param: " << nodep->lhsp() << endl);
                 m_ds.m_unresolvedClass = true;
             }
             if (m_ds.m_unresolvedCell
@@ -3024,6 +3040,8 @@ class LinkDotResolveVisitor final : public VNVisitor {
                                 refp->dotted(dotted.substr(0, pos));
                                 newp = refp;
                             } else {
+                                UINFO(9, indent()
+                                             << "deferring until post-V3Param: " << refp << endl);
                                 newp = new AstUnlinkedRef{nodep->fileline(), refp, refp->name(),
                                                           m_ds.m_unlinkedScopep->unlinkFrBack()};
                                 m_ds.m_unlinkedScopep = nullptr;
@@ -3475,9 +3493,7 @@ class LinkDotResolveVisitor final : public VNVisitor {
         if (m_ds.m_dotPos != DP_MEMBER || nodep->name() != "randomize") {
             // Visit arguments at the beginning.
             // They may be visitted even if the current node can't be linked now.
-            VL_RESTORER(m_ds);
-            m_ds.init(m_curSymp);
-            iterateChildren(nodep);
+            symIterateChildren(nodep, m_curSymp);
         }
 
         if (m_ds.m_super) {
@@ -3732,16 +3748,10 @@ class LinkDotResolveVisitor final : public VNVisitor {
             m_ds.m_unresolvedCell = true;
             // And pass up m_ds.m_dotText
         }
-        // Pass dot state down to fromp()
+        // Pass dot state down to only fromp()
         iterateAndNextNull(nodep->fromp());
-        {
-            VL_RESTORER(m_ds);
-            {
-                m_ds.init(m_curSymp);
-                iterateAndNextNull(nodep->bitp());
-                iterateAndNextNull(nodep->attrp());
-            }
-        }
+        symIterateNull(nodep->bitp(), m_curSymp);
+        symIterateNull(nodep->attrp(), m_curSymp);
         if (m_ds.m_unresolvedCell && (m_ds.m_dotPos == DP_SCOPE || m_ds.m_dotPos == DP_FIRST)) {
             AstNodeExpr* const exprp = nodep->bitp()->unlinkFrBack();
             AstCellArrayRef* const newp
@@ -3763,12 +3773,8 @@ class LinkDotResolveVisitor final : public VNVisitor {
             return;
         }
         iterateAndNextNull(nodep->fromp());
-        VL_RESTORER(m_ds);
-        {
-            m_ds.init(m_curSymp);
-            iterateAndNextNull(nodep->rhsp());
-            iterateAndNextNull(nodep->thsp());
-        }
+        symIterateNull(nodep->rhsp(), m_curSymp);
+        symIterateNull(nodep->thsp(), m_curSymp);
 
         if (nodep->attrp()) {
             AstNode* const attrp = nodep->attrp()->unlinkFrBack();
@@ -3792,15 +3798,15 @@ class LinkDotResolveVisitor final : public VNVisitor {
         LINKDOT_VISIT_START();
         UINFO(5, indent() << "visit " << nodep << endl);
         checkNoDot(nodep);
-        VL_RESTORER(m_curSymp);
         {
+            VL_RESTORER(m_curSymp);
+            VL_RESTORER(m_ds);
             if (nodep->name() != "") {
                 m_ds.m_dotSymp = m_curSymp = m_statep->getNodeSym(nodep);
                 UINFO(5, indent() << "cur=se" << cvtToHex(m_curSymp) << endl);
             }
             iterateChildren(nodep);
         }
-        m_ds.m_dotSymp = VL_RESTORER_PREV(m_curSymp);
         UINFO(5, indent() << "cur=se" << cvtToHex(m_curSymp) << endl);
     }
     void visit(AstNodeFTask* nodep) override {
@@ -3849,25 +3855,15 @@ class LinkDotResolveVisitor final : public VNVisitor {
         LINKDOT_VISIT_START();
         UINFO(5, indent() << "visit " << nodep << endl);
         checkNoDot(nodep);
-        VL_RESTORER(m_curSymp);
-        {
-            m_ds.m_dotSymp = m_curSymp = m_statep->getNodeSym(nodep);
-            iterateChildren(nodep);
-        }
-        m_ds.m_dotSymp = VL_RESTORER_PREV(m_curSymp);
+        symIterateChildren(nodep, m_statep->getNodeSym(nodep));
     }
     void visit(AstWith* nodep) override {
         LINKDOT_VISIT_START();
         UINFO(5, indent() << "visit " << nodep << endl);
         checkNoDot(nodep);
-        VL_RESTORER(m_curSymp);
         VL_RESTORER(m_inWith);
-        {
-            m_ds.m_dotSymp = m_curSymp = m_statep->getNodeSym(nodep);
-            m_inWith = true;
-            iterateChildren(nodep);
-        }
-        m_ds.m_dotSymp = VL_RESTORER_PREV(m_curSymp);
+        m_inWith = true;
+        symIterateChildren(nodep, m_statep->getNodeSym(nodep));
     }
     void visit(AstLambdaArgRef* nodep) override {
         LINKDOT_VISIT_START();

commit a8e06874bd1b98989b59d7cf9a9ac0d5a019a7d2

Fix `randomize..with` of parameterized classes (#5676)
Broke in 7a04a5b

src/V3LinkDot.cpp
@@ -3916,9 +3916,14 @@ class LinkDotResolveVisitor final : public VNVisitor {
         LINKDOT_VISIT_START();
         UINFO(5, indent() << "visit " << nodep << endl);
         checkNoDot(nodep);
+        VL_RESTORER(m_curSymp);
         VL_RESTORER(m_inWith);
-        m_inWith = true;
-        symIterateChildren(nodep, m_statep->getNodeSym(nodep));
+        {
+            m_ds.m_dotSymp = m_curSymp = m_statep->getNodeSym(nodep);
+            m_inWith = true;
+            iterateChildren(nodep);
+        }
+        m_ds.m_dotSymp = VL_RESTORER_PREV(m_curSymp);
     }
     void visit(AstLambdaArgRef* nodep) override {
         LINKDOT_VISIT_START();

test_regress/t/t_randomize_param_with.py
@@ -1,0 +1,21 @@
+#!/usr/bin/env python3
+# DESCRIPTION: Verilator: Verilog Test driver/expect definition
+#
+# Copyright 2024 by Wilson Snyder. This program is free software; you
+# can redistribute it and/or modify it under the terms of either the GNU
+# Lesser General Public License Version 3 or the Perl Artistic License
+# Version 2.0.
+# SPDX-License-Identifier: LGPL-3.0-only OR Artistic-2.0
+
+import vltest_bootstrap
+
+test.scenarios('simulator')
+
+if not test.have_solver:
+    test.skip("No constraint solver installed")
+
+test.compile()
+
+test.execute()
+
+test.passes()

test_regress/t/t_randomize_param_with.v
@@ -1,0 +1,48 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain, for
+// any use, without warranty, 2024 by Antmicro Ltd.
+// SPDX-License-Identifier: CC0-1.0
+
+`define check_rand(cl, field, constr, cond) \
+begin \
+   longint prev_result; \
+   int ok = 0; \
+   if (!bit'(cl.randomize() with { constr; })) $stop; \
+   prev_result = longint'(field); \
+   if (!(cond)) $stop; \
+   repeat(9) begin \
+      longint result; \
+      if (!bit'(cl.randomize() with { constr; })) $stop; \
+      result = longint'(field); \
+      if (!(cond)) $stop; \
+      if (result != prev_result) ok = 1; \
+      prev_result = result; \
+   end \
+   if (ok != 1) $stop; \
+end
+
+class Cls #(int LIMIT = 3);
+   rand int x;
+   int      y = -100;
+   constraint x_limit { x <= LIMIT; };
+endclass
+
+module t;
+   initial begin
+      Cls#() cd = new;
+      Cls#(5) c5 = new;
+
+      `check_rand(cd, cd.x, x > 0, cd.x > 0 && cd.x <= 3);
+      `check_rand(cd, cd.x, x > y, cd.x > -100 && cd.x <= 3);
+      if (cd.randomize() with {x > 3;} == 1) $stop;
+
+      `check_rand(c5, c5.x, x > 0, c5.x > 0 && c5.x <= 5);
+      `check_rand(c5, c5.x, x > y, c5.x > -100 && c5.x <= 5);
+      if (c5.randomize() with {x >= 5;} == 0) $stop;
+      if (c5.x != 5) $stop;
+
+      $write("*-* All Finished *-*\n");
+      $finish;
+   end
+endmodule