b9472bd1f6881c2e19399651e221ce001975ac7d
Bartłomiej Chmiel <bchmiel@antmicro.com>
2024-12-23 13:57:07 +0100

    Profile hier child sections from different threads
    Signed-off-by: Bartłomiej Chmiel <bchmiel@antmicro.com>

--- src/V3Order.cpp
+++ src/V3Order.cpp
@@ -122,9 +122,11 @@ AstCFunc* V3Order::order(AstNetlist* netlistp,  //
         return resp;
     }();
 
-    if (v3Global.opt.profExec() && !v3Global.opt.hierChild()) {
-        funcp->addStmtsp(new AstCStmt{flp, "VL_EXEC_TRACE_ADD_RECORD(vlSymsp).sectionPush(\"func "
-                                               + tag + "\");\n"});
+    if (v3Global.opt.profExec()) {
+        const string name
+            = (v3Global.opt.hierChild() ? (v3Global.opt.topModule() + " ") : "") + "func " + tag;
+        funcp->addStmtsp(new AstCStmt{flp, "VL_EXEC_TRACE_ADD_RECORD(vlSymsp).sectionPush(\""
+                                               + name + "\");\n"});
     }
 
     // Build the OrderGraph
@@ -152,7 +154,7 @@ AstCFunc* V3Order::order(AstNetlist* netlistp,  //
     // Dispose of the remnants of the inputs
     for (auto* const lbsp : logic) lbsp->deleteActives();
 
-    if (v3Global.opt.profExec() && !v3Global.opt.hierChild()) {
+    if (v3Global.opt.profExec()) {
         funcp->addStmtsp(new AstCStmt{flp, "VL_EXEC_TRACE_ADD_RECORD(vlSymsp).sectionPop();\n"});
     }
 
--- src/V3Sched.cpp
+++ src/V3Sched.cpp
@@ -156,7 +156,9 @@ AstNodeStmt* checkIterationLimit(AstNetlist* netlistp, const string& name, AstVa
     return ifp;
 }
 
-AstNodeStmt* profExecSectionPush(FileLine* flp, const string& name) {
+AstNodeStmt* profExecSectionPush(FileLine* flp, const string& section) {
+    const string name
+        = (v3Global.opt.hierChild() ? (v3Global.opt.topModule() + " ") : "") + section;
     return new AstCStmt{flp, "VL_EXEC_TRACE_ADD_RECORD(vlSymsp).sectionPush(\"" + name + "\");\n"};
 }
 
@@ -226,8 +228,7 @@ EvalLoop createEvalLoop(
     AstNodeStmt* stmtps = nullptr;
 
     // Prof-exec section push
-    if (v3Global.opt.profExec() && !v3Global.opt.hierChild())
-        stmtps = profExecSectionPush(flp, "loop " + tag);
+    if (v3Global.opt.profExec()) stmtps = profExecSectionPush(flp, "loop " + tag);
 
     const auto addVar = [&](const std::string& name, int width, uint32_t initVal) {
         AstVarScope* const vscp = scopeTopp->createTemp("__V" + tag + name, width);
@@ -274,8 +275,7 @@ EvalLoop createEvalLoop(
     }
 
     // Prof-exec section pop
-    if (v3Global.opt.profExec() && !v3Global.opt.hierChild())
-        stmtps->addNext(profExecSectionPop(flp));
+    if (v3Global.opt.profExec()) stmtps->addNext(profExecSectionPop(flp));
 
     return {firstIterFlagp, stmtps};
 }
@@ -632,8 +632,7 @@ const TriggerKit createTriggers(AstNetlist* netlistp, AstCFunc* const initFuncp,
 
     // Create the trigger computation function
     AstCFunc* const funcp = makeSubFunction(netlistp, "_eval_triggers__" + name, slow);
-    if (v3Global.opt.profExec() && !v3Global.opt.hierChild())
-        funcp->addStmtsp(profExecSectionPush(flp, "trig " + name));
+    if (v3Global.opt.profExec()) funcp->addStmtsp(profExecSectionPush(flp, "trig " + name));
 
     // Create the trigger dump function (for debugging, always 'slow')
     AstCFunc* const dumpp = makeSubFunction(netlistp, "_dump_triggers__" + name, true);
@@ -784,8 +783,7 @@ const TriggerKit createTriggers(AstNetlist* netlistp, AstCFunc* const initFuncp,
         add("#endif\n");
     }
 
-    if (v3Global.opt.profExec() && !v3Global.opt.hierChild())
-        funcp->addStmtsp(profExecSectionPop(flp));
+    if (v3Global.opt.profExec()) funcp->addStmtsp(profExecSectionPop(flp));
 
     // The debug code might leak signal names, so simply delete it when using --protect-ids
     if (v3Global.opt.protectIds()) dumpp->stmtsp()->unlinkFrBackWithNext()->deleteTree();
@@ -1114,8 +1112,7 @@ void createEval(AstNetlist* netlistp,  //
     AstCFunc* const funcp = makeTopFunction(netlistp, "_eval", false);
     netlistp->evalp(funcp);
 
-    if (v3Global.opt.profExec() && !v3Global.opt.hierChild())
-        funcp->addStmtsp(profExecSectionPush(flp, "eval"));
+    if (v3Global.opt.profExec()) funcp->addStmtsp(profExecSectionPush(flp, "eval"));
 
     // Start with the ico loop, if any
     if (icoLoop) funcp->addStmtsp(icoLoop);
@@ -1126,8 +1123,7 @@ void createEval(AstNetlist* netlistp,  //
     // Add the Postponed eval call
     if (postponedFuncp) funcp->addStmtsp(callVoidFunc(postponedFuncp));
 
-    if (v3Global.opt.profExec() && !v3Global.opt.hierChild())
-        funcp->addStmtsp(profExecSectionPop(flp));
+    if (v3Global.opt.profExec()) funcp->addStmtsp(profExecSectionPop(flp));
 }
 
 }  // namespace
