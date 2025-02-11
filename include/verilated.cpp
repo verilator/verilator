d04779afb705e3eae419f22dcaae2472a1d67ddc
Yilou Wang <wangyilou123@gmail.com>
2025-02-11 17:45:40 +0100

    fix, not stringp, but string var as input

--- include/verilated.cpp
+++ include/verilated.cpp
@@ -918,9 +918,9 @@ void _vl_vsformat(std::string& output, const std::string& format, va_list ap) VL
             }
             case 'p': {  // 'x' but parameter is string
                 const int lbits = va_arg(ap, int);
-                const std::string* const cstrp = va_arg(ap, const std::string*);
+                const std::string const cstr = va_arg(ap, std::string);
                 std::ostringstream oss;
-                for (unsigned char c : *cstrp) { oss << std::hex << static_cast<int>(c); }
+                for (unsigned char c : cstr) { oss << std::hex << static_cast<int>(c); }
                 std::string hex_str = oss.str();
                 if (width > 0 && widthSet) {
                     hex_str = hex_str.size() > width
