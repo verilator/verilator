d24f26c083c1dd363f4461009d05535416003abd
github action <action@example.com>
2025-02-11 16:18:43 +0000

    Apply 'make format'

--- include/verilated_types.h
+++ include/verilated_types.h
@@ -955,7 +955,8 @@ public:
     bool operator==(const VlAssocArray& rhs) const { return m_map == rhs.m_map; }
     bool operator!=(const VlAssocArray& rhs) const { return m_map != rhs.m_map; }
     bool operator<(const VlAssocArray& rhs) const {
-        for (auto it = m_map.cbegin(), its = rhs.m_map.cbegin(); (it != m_map.end() || it != rhs.m_map.end()) ; ++it, ++its) {
+        for (auto it = m_map.cbegin(), its = rhs.m_map.cbegin();
+             (it != m_map.end() || it != rhs.m_map.end()); ++it, ++its) {
             if (it->second < its->second) return true;
         }
         return false;
