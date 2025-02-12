31099be40e2d6b89b34c8c4b11ce6787be0a0601
Yilou Wang <wangyilou123@gmail.com>
2025-02-12 09:38:34 +0100

    using c style char*

--- include/verilated.cpp
+++ include/verilated.cpp
@@ -918,9 +918,9 @@ void _vl_vsformat(std::string& output, const std::string& format, va_list ap) VL
             }
             case 'p': {  // 'x' but parameter is string
                 const int lbits = va_arg(ap, int);
-                const std::string cstr = va_arg(ap, std::string);
+                const char* const cstr = va_arg(ap, const char*);
                 std::ostringstream oss;
-                for (unsigned char c : cstr) { oss << std::hex << static_cast<int>(c); }
+                for (unsigned char c : std::string(cstr)) { oss << std::hex << static_cast<int>(c); }
                 std::string hex_str = oss.str();
                 if (width > 0 && widthSet) {
                     hex_str = hex_str.size() > width
