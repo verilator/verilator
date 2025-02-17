1a07af57a3b264e11c815b2f9a1aa81604f19ea2
Yutetsu TAKATSUKASA <y.takatsukasa@gmail.com>
2025-02-18 03:34:57 +0900

    Fix unpacked split_var (#5782) (#5785)

---
+++ test_regress/t/t_split_var_types.v
@@ -0,0 +1,31 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain, for
+// any use, without warranty, 2025
+// SPDX-License-Identifier: CC0-1.0
+
+module t(/*AUTOARG*/
+    // Inputs
+    clk
+    );
+    input clk;
+
+    // Test loop
+    always @ (posedge clk) begin
+        $write("*-* All Finished *-*\n");
+        $finish;
+    end
+
+    bug5782 u_bug5782(.data_out());
+
+endmodule
+
+// #5782 internal error with --trace. Bit range is not properly handled.
+module bug5782 (
+    output logic [31:0][15:0] data_out
+);
+    logic [31:0][15:0] data [8] /*verilator split_var*/;
+    always begin
+        data_out = data[7];
+    end
+endmodule
