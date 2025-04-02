8336917b5f142af1f366bbc89c97d53629a247ee
Ryszard Rozak <rrozak@antmicro.com>
2025-04-02 10:38:37 +0200

    Restore and change t_interface_param_another_bad
    Signed-off-by: Ryszard Rozak <rrozak@antmicro.com>

---
+++ test_regress/t/t_interface_param_another_bad.v
@@ -0,0 +1,18 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed into the Public Domain, for any use,
+// without warranty, 2017 by Johan Bjork.
+// SPDX-License-Identifier: CC0-1.0
+
+module t ();
+   simple_bus sb_intf();
+   simple_bus #(.PARAMETER(sb_intf.dummy)) simple();
+   initial begin
+      $write("*-* All Finished *-*\n");
+      $finish;
+   end
+endmodule
+
+interface simple_bus #(PARAMETER = 0);
+   logic dummy;
+endinterface
