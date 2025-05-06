2189af1ed6dcbbd396c84c42c7fde84eb1bd53e2
Todd Strader <tstrader@hudson-trading.com>
2025-05-06 10:31:12 -0400

    fallthrough

--- src/V3String.cpp
+++ src/V3String.cpp
@@ -345,13 +345,13 @@ uint64_t VString::hash(const string& str) VL_PURE {
     const unsigned char* data2 = (const unsigned char*)data;
 
     switch (len & 7) {
-    case 7: h ^= uint64_t(data2[6]) << 48;
-    case 6: h ^= uint64_t(data2[5]) << 40;
-    case 5: h ^= uint64_t(data2[4]) << 32;
-    case 4: h ^= uint64_t(data2[3]) << 24;
-    case 3: h ^= uint64_t(data2[2]) << 16;
-    case 2: h ^= uint64_t(data2[1]) << 8;
-    case 1: h ^= uint64_t(data2[0]); h *= m;
+    case 7: h ^= uint64_t(data2[6]) << 48; /* fallthrough */
+    case 6: h ^= uint64_t(data2[5]) << 40; /* fallthrough */
+    case 5: h ^= uint64_t(data2[4]) << 32; /* fallthrough */
+    case 4: h ^= uint64_t(data2[3]) << 24; /* fallthrough */
+    case 3: h ^= uint64_t(data2[2]) << 16; /* fallthrough */
+    case 2: h ^= uint64_t(data2[1]) << 8; /* fallthrough */
+    case 1: h ^= uint64_t(data2[0]); h *= m; /* fallthrough */
     };
 
     h ^= h >> r;
