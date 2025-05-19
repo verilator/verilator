1eb3a8564206f047e9f078cf337c1d458f49c9aa
Todd Strader <tstrader@hudson-trading.com>
2025-05-19 09:54:10 -0400

    additional scenario

--- test_regress/t/t_concatenate_casts.v
+++ test_regress/t/t_concatenate_casts.v
@@ -4,8 +4,15 @@
 // without warranty, 2025 by Wilson Snyder.
 // SPDX-License-Identifier: CC0-1.0
 
+package my_pkg;
+    typedef enum logic [1:0] {
+        SIG_0, SIG_1, SIG_2
+    } sig_t;
+endpackage : my_pkg
+
 
 module t;
+    import my_pkg::*;
 
     typedef logic [7:0] foo_t;
     typedef logic [31:0] bar_t;
@@ -17,19 +24,17 @@ module t;
     always_comb begin
         the_bars = {32'd7, 32'd8};
         the_foos[0] = {foo_t'(the_bars[1]), foo_t'(the_bars[0])};
-        // NOCOMMIT
-        // the_foos[0][1] = foo_t'(the_bars[1]);
-        // the_foos[0][0] = foo_t'(the_bars[0]);
     end
 
+    logic [6:0] data;
+    logic [2:0] opt;
+
+    assign data = 7'b110_0101;
+    assign opt = {data[5], sig_t'(data[1:0])};
+
     initial begin
-        // NOCOMMIT -- displays
-        $display($bits(the_foos));
-        $display($bits(the_foos[0]));
-        $display($bits(the_foos[0][0]));
-        $display(the_foos[0][1]);
-        $display("===> %x", the_foos);
         if (the_foos != 'h0708) $stop();
+        if (opt != 'b101) $stop();
         $write("*-* All Finished *-*\n");
         $finish;
     end
