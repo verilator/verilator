0d2550dffb9b388f542e3a9fa89bdde361b7029e
Joel Bodenmann <jbo@insane.engineer>
2025-05-20 14:43:38 +0200

    Add missing include

--- include/verilated_threads.cpp
+++ include/verilated_threads.cpp
@@ -29,6 +29,7 @@
 #include <fstream>
 #include <iostream>
 #include <memory>
+#include <pthread_np.h>
 #include <string>
 
 //=============================================================================
