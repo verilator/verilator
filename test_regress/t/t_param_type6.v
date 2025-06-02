efc86cc8c5aea0b5a741097a93d088fc55193e4b
Todd Strader <tstrader@hudson-trading.com>
2025-06-02 08:11:07 -0400

    t_param_typ6 no param case

--- test_regress/t/t_param_type6.v
+++ test_regress/t/t_param_type6.v
@@ -10,6 +10,10 @@ interface intf #(
     the_type foo;
 endinterface
 
+interface no_param_intf;
+    logic [13:0] bar;
+endinterface
+
 module t (/*AUTOARG*/
     // Inputs
     clk
@@ -18,7 +22,11 @@ module t (/*AUTOARG*/
     input clk;
 
     intf #(.the_type (logic [7:0])) intf_eight();
-    sub #(.type_bits (8)) sub_eight (.intf_pin (intf_eight));
+    no_param_intf the_no_param_intf();
+    sub #(.type_bits (8)) sub_eight (
+        .intf_pin (intf_eight),
+        .no_param_intf_pin (the_no_param_intf)
+    );
 
     // finish report
     always @ (posedge clk) begin
@@ -30,12 +38,15 @@ endmodule
 module sub #(
     parameter int type_bits
 )(
-    intf intf_pin
+    intf intf_pin,
+    no_param_intf no_param_intf_pin
 );
 
     localparam type intf_type = type(intf_pin.foo);
+    localparam type no_param_intf_type = type(no_param_intf_pin.bar);
     initial begin
         if ($bits(intf_type) != type_bits) $stop();
+        if ($bits(no_param_intf_type) != 14) $stop();
     end
 
 endmodule
