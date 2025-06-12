4203aa88609e7e7abaca88685ec597c421eebb8b
Todd Strader <tstrader@hudson-trading.com>
2025-06-12 09:49:21 -0400

    actual runtime cast

--- test_regress/t/t_real_cast.v
+++ test_regress/t/t_real_cast.v
@@ -15,16 +15,25 @@ module t (/*AUTOARG*/
     localparam big_t foo = big_t'(8.531630271583128e+16);
 
     big_t bar;
+    int cyc;
+    real some_real;
 
     initial begin
-        bar = big_t'(8.531630271583128e+16);
-        if (bar != 86'd85316302715831280) $stop();
+        cyc = 0;
+        some_real = 5.123;
     end
 
+    always_comb bar = big_t'(some_real);
+
     always @(posedge clk) begin
-        if (foo != 86'd85316302715831280) $stop();
-        $write("*-* All Finished *-*\n");
-        $finish;
+        cyc <= cyc + 1;
+        some_real <= some_real * 1.234e4;
+        if (cyc == 6) begin
+            if (foo != 86'd85316302715831280) $stop();
+            if (bar != 86'd18089031459271914704338944) $stop();
+            $write("*-* All Finished *-*\n");
+            $finish;
+        end
     end
 
 endmodule
