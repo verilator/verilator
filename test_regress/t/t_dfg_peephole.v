cc6e2e10bbe9d9300004f1c0b805377bc0316687
Bartłomiej Chmiel <bchmiel@antmicro.com>
2025-06-24 11:42:42 +0200

    Test license update
    Signed-off-by: Bartłomiej Chmiel <bchmiel@antmicro.com>

--- test_regress/t/t_dfg_peephole.v
+++ test_regress/t/t_dfg_peephole.v
@@ -1,7 +1,7 @@
 // DESCRIPTION: Verilator: Verilog Test module
 //
 // This file ONLY is placed under the Creative Commons Public Domain, for
-// any use, without warranty, 2022 by Geza Lore.
+// any use, without warranty, 2022-2025 by Geza Lore and Antmicro.
 // SPDX-License-Identifier: CC0-1.0
 
 `define signal(name, expr) wire [$bits(expr)-1:0] ``name = expr
