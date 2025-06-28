29ba877e5a46bab30dfaede72a19c4f014e4231f
Geza Lore <gezalore@gmail.com>
2025-06-28 20:09:41 +0100

    Remove unnecessary lint_off

--- test_regress/t/t_nba_mixed_update_clocked.v
+++ test_regress/t/t_nba_mixed_update_clocked.v
@@ -22,9 +22,7 @@ module t (/*AUTOARG*/
   reg y0 = 1'b0;
   reg y1 = 1'b0;
   // 'z[0]' should equal '{8{x[0]}', 'z[1]' should equal '{8{x[1]}}'
-  // verilator lint_off BLKANDNBLK
   reg [1:0][7:0] z;
-  // verilator lint_on BLKANDNBLK
   // 'pair.a' should equal 'x[0]', 'pair.b' should equal 'x[1]'
   struct {
     logic a;
