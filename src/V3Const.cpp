778cb83cbd3c7b5813abea0322e6d4153507d83f
Todd Strader <tstrader@hudson-trading.com>
2025-07-02 17:41:25 -0400

    fix

--- src/V3Const.cpp
+++ src/V3Const.cpp
@@ -2117,7 +2117,7 @@ class ConstVisitor final : public VNVisitor {
                 AstVar* const tempPurep = new AstVar{rhsp->fileline(), VVarType::BLOCKTEMP,
                                                      m_concswapNames.get(rhsp), rhsp->dtypep()};
                 m_modp->addStmtsp(tempPurep);
-                AstNodeAssign* const asnp = nodep->cloneType(
+                AstAssign* const asnp = new AstAssign(nodep->fileline(),
                     new AstVarRef{rhsp->fileline(), tempPurep, VAccess::WRITE}, rhsp);
                 nodep->addHereThisAsNext(asnp);
                 nodep->rhsp(new AstVarRef{rhsp->fileline(), tempPurep, VAccess::READ});
