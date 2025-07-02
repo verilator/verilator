e2e56b8367d0a9de6a55ec58c474eedc20f91078
Ryszard Rozak <rrozak@antmicro.com>
2025-07-02 09:35:11 +0200

    Add test with undefined identifier
    Signed-off-by: Ryszard Rozak <rrozak@antmicro.com>

---
+++ test_regress/t/t_disable_bad.v
@@ -0,0 +1,11 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain, for
+// any use, without warranty, 2025 by Antmicro.
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+   initial begin
+      disable abcd;
+   end
+endmodule: t
