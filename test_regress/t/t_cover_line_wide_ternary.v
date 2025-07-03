314bd6f97a443c3a2f7f4a6b7847673c17b770d3
Todd Strader <tstrader@hudson-trading.com>
2025-07-03 09:19:44 -0400

    Fix more wide ternary + coverage cases

--- test_regress/t/t_cover_line_wide_ternary.v
+++ test_regress/t/t_cover_line_wide_ternary.v
@@ -21,25 +21,41 @@ module t (/*AUTOARG*/
     initial cyc=1;
 
     intf intfs [2] ();
-    intf intf_sel();
+    intf intf_sel_ff();
+    intf intf_sel_comb();
+    intf intf_sel_assign();
 
     always_comb begin
         intfs[0].bar = 123;
         intfs[1].bar = 456;
     end
 
-    always @ (posedge clk) begin
-        {intf_sel.foo, intf_sel.bar, intf_sel.baz} <=
+    always_ff @ (posedge clk) begin
+        {intf_sel_ff.foo, intf_sel_ff.bar, intf_sel_ff.baz} <=
             cyc[0] ?
             {intfs[1].foo, intfs[1].bar, intfs[1].baz} :
             {intfs[0].foo, intfs[0].bar, intfs[0].baz};
     end
 
+    always_comb begin
+        {intf_sel_comb.foo, intf_sel_comb.bar, intf_sel_comb.baz} =
+            cyc[0] ?
+            {intfs[1].foo, intfs[1].bar, intfs[1].baz} :
+            {intfs[0].foo, intfs[0].bar, intfs[0].baz};
+    end
+
+    assign
+        {intf_sel_assign.foo, intf_sel_assign.bar, intf_sel_assign.baz} =
+            cyc[0] ?
+            {intfs[1].foo, intfs[1].bar, intfs[1].baz} :
+            {intfs[0].foo, intfs[0].bar, intfs[0].baz};
+
     always @ (posedge clk) begin
         cyc <= cyc + 1;
         if (cyc==9) begin
-            $display("bar = %0d", intf_sel.bar);
-            if (intf_sel.bar != 123) $stop();
+            if (intf_sel_ff.bar != 123) $stop();
+            if (intf_sel_comb.bar != 456) $stop();
+            if (intf_sel_assign.bar != 456) $stop();
             $write("*-* All Finished *-*\n");
             $finish;
         end
