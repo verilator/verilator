cc62e671318278bd79f1eb02006936605b12e336
Artur Bieniek <abieniek@internships.antmicro.com>
2025-07-08 18:17:43 +0200

    Add covergroup function override test
    Signed-off-by: Artur Bieniek <abieniek@internships.antmicro.com>

---
+++ test_regress/t/t_covergroup_func_override_bad.v
@@ -0,0 +1,17 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain, for
+// any use, without warranty, 2025 by Antmicro.
+// SPDX-License-Identifier: CC0-1.0
+
+/* verilator lint_off COVERIGN */
+module t();
+    covergroup cg;
+    function sample();
+
+    endfunction
+    function get_coverage();
+
+    endfunction
+    endgroup
+endmodule
