7acb3f18df9f2126493a07769c193560ac9c0390
Igor Zaworski <izaworski@internships.antmicro.com>
2025-07-10 09:36:37 +0200

    Indentation fix
    Signed-off-by: Igor Zaworski <izaworski@internships.antmicro.com>

--- test_regress/t/t_typedef_param_class.v
+++ test_regress/t/t_typedef_param_class.v
@@ -15,16 +15,11 @@ endclass
 
 
 module t;
-   initial begin
-      Class1#(Class2)::Some_type1 value1 = 7;
-      // TODO: uncomment those lines when `type` will be fixed
-      // type(value1) in this case causes an error:
-      //  %Error: Internal Error: t/t_typedef_param_class.v:19:16:
-      // ../V3Ast.h:2659: AstNode is not of expected type, but instead has type 'VARREF'
-      //     19 |       if (type(value1) != type(value2)) $stop;
-      // int value2 = 13;
-      // if (type(value1) != type(value2)) $stop;
-      $write("*-* All Finished *-*\n");
-      $finish;
-   end
+  initial begin
+    int value0 = 7;
+    Class1#(Class2)::Some_type1 value1 = value0;
+    int value2 = value1;
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
 endmodule
