0e1d34bfbccc9fb59e9e8e23cf7418ee713ab3c6
Igor Zaworski <izaworski@internships.antmicro.com>
2025-07-18 11:55:40 +0200

    Introduction of first batch of tests
    Signed-off-by: Igor Zaworski <izaworski@internships.antmicro.com>

---
+++ test_regress/t/t_interface_generic_modport2.v
@@ -0,0 +1,28 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain, for
+// any use, without warranty, 2025 by Antmicro.
+// SPDX-License-Identifier: CC0-1.0
+
+interface inf;
+  int v;
+  modport mp (
+    output v
+  );
+endinterface
+
+module GenericModule (interface.mp a);
+  initial begin
+    a.v = 7;
+  end
+endmodule      
+
+module t;
+  inf inf_inst();
+  GenericModule genericModule (inf_inst);
+  initial begin
+    if (inf_inst.v != 7) $stop;
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
---
+++ test_regress/t/t_interface_generic_normal.v
@@ -0,0 +1,29 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain, for
+// any use, without warranty, 2025 by Antmicro.
+// SPDX-License-Identifier: CC0-1.0
+
+interface inf;
+  int v;
+endinterface
+
+module GenericModule (interface a, inf b, interface c);
+  initial begin
+    if (a.v != 7) $stop;
+    if (b.v != 8) $stop;
+    if (c.v != 9) $stop;
+  end
+endmodule      
+
+module t;
+  inf inf_inst[3]();
+  GenericModule genericModule (inf_inst[0], inf_inst[1], inf_inst[2]);
+  initial begin
+    inf_inst[0].v = 7;
+    inf_inst[1].v = 8;
+    inf_inst[2].v = 9;
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
