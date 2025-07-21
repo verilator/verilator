4b1fda5fc1b058b1768e09a043cf58c70ca0e135
Igor Zaworski <izaworski@internships.antmicro.com>
2025-07-21 08:07:12 +0200

    Fix of unexpected warning when trying to access variable not listed in interface modport
    Signed-off-by: Igor Zaworski <izaworski@internships.antmicro.com>

---
+++ test_regress/t/t_interface_generic_modport_bad3.v
@@ -0,0 +1,29 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain, for
+// any use, without warranty, 2025 by Antmicro.
+// SPDX-License-Identifier: CC0-1.0
+
+interface inf;
+  int v;
+  int o;
+  modport mp (
+    input o
+  );
+endinterface
+
+module GenericModule (interface.mp a);
+  initial begin
+    if (a.v != 7) $stop;
+  end
+endmodule
+
+module t;
+  inf inf_inst();
+  GenericModule genericModule (inf_inst);
+  initial begin
+    inf_inst.v = 7;
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
