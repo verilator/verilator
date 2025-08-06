3129d76cdb11548b8f1279af9da41ba20836bafc
taylor-bsg <taylor-bsg@users.noreply.github.com>
2025-08-06 10:18:07 -0700

    Update t_param_slice.py

--- test_regress/t/t_param_slice.py
+++ test_regress/t/t_param_slice.py
@@ -8,15 +8,10 @@
 # SPDX-License-Identifier: LGPL-3.0-only OR Artistic-2.0
 import vltest_bootstrap
 
-# Use the Verilator scenario.  Other scenarios such as 'sc' (SystemC) are not needed here.
 test.scenarios('vlt')
 
-# Compile the Verilog source (t_param_slice.v is detected automatically)
 test.compile(verilator_flags2=['--exe','--main','--timing'])
 
-# Run the compiled simulation
 test.execute()
 
-# Mark the test as passed if no errors occurred and the "*-* All Finished *-*"
-# marker is printed by the Verilog code.
 test.passes()
