c9bd3d8923cc6d24a5f8063651b30c39531f39ee
taylor-bsg <taylor-bsg@users.noreply.github.com>
2025-08-06 08:11:18 -0700

    Update t_param_slice.py

--- test_regress/t/t_param_slice.py
+++ test_regress/t/t_param_slice.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 # DESCRIPTION: Verilator: Test constant parameter slicing of unpacked arrays (issue #6257)
-
+#
+# Copyright 2025 by Michael Taylor. This program is free software; you
+# can redistribute it and/or modify it under the terms of either the GNU
+# Lesser General Public License Version 3 or the Perl Artistic License
+# Version 2.0.
+# SPDX-License-Identifier: LGPL-3.0-only OR Artistic-2.0
 import vltest_bootstrap
 
 # Use the Verilator scenario.  Other scenarios such as 'sc' (SystemC) are not needed here.
