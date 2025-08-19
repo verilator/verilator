50df2bd7916e4e4ccb838be87ee198cb46242e32
Igor Zaworski <izaworski@internships.antmicro.com>
2025-08-19 14:46:59 +0200

    Test update
    Signed-off-by: Igor Zaworski <izaworski@internships.antmicro.com>

--- test_regress/t/t_randomize_member_select.v
+++ test_regress/t/t_randomize_member_select.v
@@ -4,10 +4,21 @@
 // any use, without warranty, 2025 by Antmicro.
 // SPDX-License-Identifier: CC0-1.0
 
+class B;
+  rand int insideB;
+  constraint i {
+    insideB inside {[0:10]};
+  };
+endclass
+
 class A;
   rand logic[31:0] rdata;
   rand int delay;
   int i = 97;
+  rand B b;
+  function new();
+    b = new;
+  endfunction
   constraint delay_bounds {
     delay inside {[0:2]};
   }
@@ -21,7 +32,9 @@ module t;
     i = 7;
     repeat (120) begin
       assert (a.randomize() with {if (a.delay == 1) a.rdata == i;} != 0);
+      assert (a.b.randomize() with {a.b.insideB < 3;} != 0);
       if (a.delay == 1 && a.rdata != 97) $stop;
+      if (a.b.insideB >= 3) $stop;
     end
     $write("*-* All Finished *-*\n");
     $finish;
