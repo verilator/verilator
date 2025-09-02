c72f8f93c96780b8106f595808b251e29e92097b
Ryszard Rozak <rrozak@antmicro.com>
2025-09-02 13:06:37 +0200

    Set dtype
    Signed-off-by: Ryszard Rozak <rrozak@antmicro.com>

--- src/V3Width.cpp
+++ src/V3Width.cpp
@@ -1217,6 +1217,7 @@ class WidthVisitor final : public VNVisitor {
         if (!nodep->lhsp()->dtypep()->similarDType(nodep->rhsp()->dtypep())) {
             nodep->v3error("Incompatible types of nets used for net alias");
         }
+        nodep->dtypeFrom(nodep->lhsp());
     }
 
     void visit(AstWildcardSel* nodep) override {
