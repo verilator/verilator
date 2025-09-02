dbb62ba493d1d6934aea64979f29b64d5733c3b7
Ryszard Rozak <rrozak@antmicro.com>
2025-09-02 12:00:46 +0200

    Fix t_alias test and restore comment
    Signed-off-by: Ryszard Rozak <rrozak@antmicro.com>

--- test_regress/t/t_alias_unsup.v
+++ test_regress/t/t_alias_unsup.v
@@ -33,7 +33,7 @@ module t (  /*AUTOARG*/
     $write("x_bwd = %x, y_bwd = %x\n", x_bwd, y_bwd);
 `endif
     if (y_fwd != 32'hefbeadde) $stop;
-    if (x_bwd == 32'hcefaedfe) $stop;
+    if (x_bwd != 32'hcefaedfe) $stop;
     $write("*-* All Finished *-*\n");
     $finish;
   end
@@ -48,4 +48,14 @@ module swap (
 );
 
   alias {a[7:0], a[15:8], a[23:16], a[31:24]} = b;
+
+  // Equivalent to
+
+  // wire [31:0] a_prime;
+  // wire [31:0] b_prime;
+
+  // assign b_prime = {a[7:0],a[15:8],a[23:16],a[31:24]};
+  // assign {a_prime[7:0],a_prime[15:8],a_prime[23:16],a_prime[31:24]} = b;
+  // assign b = b_prime;
+  // assign a = a_prime;
 endmodule
