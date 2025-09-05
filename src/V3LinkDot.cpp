6acb62bf1a4d5858a34817a669b5a463eaede85f
Ryszard Rozak <rrozak@antmicro.com>
2025-09-05 09:47:42 +0200

    Fix segfault when modport variable is unresolved
    Signed-off-by: Ryszard Rozak <rrozak@antmicro.com>

--- src/V3LinkDot.cpp
+++ src/V3LinkDot.cpp
@@ -3916,6 +3916,11 @@ class LinkDotResolveVisitor final : public VNVisitor {
             }
             dotSymp = m_statep->findDotted(nodep->fileline(), dotSymp, nodep->dotted(), baddot,
                                            okSymp, true);  // Maybe nullptr
+            if (!dotSymp) {
+                nodep->v3error("Couldn't resolve symbol: " << AstNode::prettyNameQ(baddot));
+                return;
+            }
+
             bool modport = false;
             if (const AstVar* varp = VN_CAST(dotSymp->nodep(), Var)) {
                 if (const AstIfaceRefDType* const ifaceRefp
