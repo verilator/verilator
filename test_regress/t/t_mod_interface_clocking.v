0563f5912032b1ea579bfd7314bf689f41224c7b
Ryszard Rozak <rrozak@antmicro.com>
2025-09-16 11:40:51 +0200

    Extend test to have 2 modports
    Signed-off-by: Ryszard Rozak <rrozak@antmicro.com>

--- test_regress/t/t_mod_interface_clocking.v
+++ test_regress/t/t_mod_interface_clocking.v
@@ -11,39 +11,41 @@ interface axi_if;
   clocking cb @(posedge clk);
     inout rlast, rvalid;
   endclocking
-  modport mcb(clocking cb, inout clk, rlast, rvalid);
+  modport md1(clocking cb, inout clk, rlast, rvalid);
+  modport md2(clocking cb);
 endinterface
 
 module sub (
-    axi_if.mcb axi_vi
+    axi_if.md1 axi1,
+    axi_if.md2 axi2
 );
   initial begin
-    axi_vi.clk = 1'b0;
-    #1 axi_vi.clk = 1'b1;
-    #1 axi_vi.clk = 1'b0;
-    #1 axi_vi.clk = 1'b1;
+    axi1.clk = 1'b0;
+    #1 axi1.clk = 1'b1;
+    #1 axi1.clk = 1'b0;
+    #1 axi1.clk = 1'b1;
   end
   initial begin
-    @(negedge axi_vi.rvalid);
-    $display("[%0t] rvalid==%b", $time, axi_vi.rvalid);
-    $display("[%0t] rlast is 1: ", $time, axi_vi.rlast === 1);
-    if (axi_vi.rlast === 1) $stop;
+    @(negedge axi1.rvalid);
+    $display("[%0t] rvalid==%b", $time, axi1.rvalid);
+    $display("[%0t] rlast is 1: ", $time, axi1.rlast === 1);
+    if (axi1.rlast === 1) $stop;
     $write("*-* All Finished *-*\n");
     $finish;
   end
   initial begin
     $display("[%0t] rvalid <= 1", $time);
-    axi_vi.cb.rvalid <= 1'b1;
-    @(posedge axi_vi.rvalid);
+    axi1.cb.rvalid <= 1'b1;
+    @(posedge axi1.rvalid);
     $display("[%0t] rvalid <= 0", $time);
-    axi_vi.cb.rvalid <= 1'b0;
-    @(negedge axi_vi.clk);
+    axi1.cb.rvalid <= 1'b0;
+    @(negedge axi1.clk);
     $display("[%0t] rlast <= 1", $time);
-    axi_vi.cb.rlast <= 1'b1;
+    axi2.cb.rlast <= 1'b1;
   end
 endmodule
 
 module t;
   axi_if axi_vi ();
-  sub i_sub (axi_vi);
+  sub i_sub (.axi1(axi_vi), .axi2(axi_vi));
 endmodule
