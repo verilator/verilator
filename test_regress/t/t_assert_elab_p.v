e5e7e844c46ba636ffaab19d78aadb496ee523f8
Wilson Snyder <wsnyder@wsnyder.org>
2025-09-19 22:59:13 -0400

    Tests: Fix assignment type mismatch (#6461)

--- test_regress/t/t_assert_elab_p.v
+++ test_regress/t/t_assert_elab_p.v
@@ -18,15 +18,8 @@ endmodule
 module t #(
     parameter type ID_T = logic [4:0]
 );
-  function automatic ID_T [3:0][1:0] gen_stage_ids();
-    for (int unsigned st = 0; st < 4; st++) begin
-      for (int unsigned id = 0; id < 2; id++) begin
-        gen_stage_ids[st][id] = 3 + ((st + 1) * 2) + id - (3 + ((st + 1) * 2) + id < 18 ? 0 : 18);
-      end
-    end
-  endfunction
 
-  localparam ID_T STAGE_IDS[3:0][1:0] = gen_stage_ids();
+  localparam ID_T STAGE_IDS[3:0][1:0] = '{default: 5'b1};
 
   pipe_id_match #(
       .ID_T(ID_T),
