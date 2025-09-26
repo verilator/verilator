13d03edf4dced22ba74cfad60f849f6911729549
Igor Zaworski <izaworski@internships.antmicro.com>
2025-09-26 10:27:11 +0200

    Fix - optimization
    Signed-off-by: Igor Zaworski <izaworski@internships.antmicro.com>

--- src/V3Unknown.cpp
+++ src/V3Unknown.cpp
@@ -505,13 +505,18 @@ class UnknownVisitor final : public VNVisitor {
                 = new AstConst{nodep->fileline(), AstConst::WidthedValue{}, nodep->bitp()->width(),
                                static_cast<uint32_t>(declElements - 1)};
             AstNodeExpr* bitp = V3Const::constifyEdit(nodep->bitp()->unlinkFrBack());
+            bool static_ok
+                = declElementsp->width() >= bitp->width()
+                  && static_cast<uint32_t>(declElements - 1) >= (1 << bitp->width()) - 1;
             if (AstConst* const constp = VN_CAST(bitp, Const)) {
-                if (V3Number{nodep}.opGte(declElementsp->num(), constp->num()).isNeqZero()) {
-                    // We don't need to add a conditional; we know the existing expression is ok
-                    VL_DO_DANGLING(declElementsp->deleteTree(), declElementsp);
-                    nodep->bitp(bitp);
-                    return;
-                }
+                static_ok
+                    |= V3Number{nodep}.opGte(declElementsp->num(), constp->num()).isNeqZero();
+            }
+            if (static_ok) {
+                // We don't need to add a conditional; we know the existing expression is ok
+                VL_DO_DANGLING(declElementsp->deleteTree(), declElementsp);
+                nodep->bitp(bitp);
+                return;
             }
             if (!bitp->isPure()) {
                 AstVar* const varp = createAddTemp(bitp);
