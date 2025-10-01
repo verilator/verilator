d86a11903bb4519f1171f2bd35b362f41add41d4
Jonathan Schroeter <jonathan.schroeter@hm.edu>
2025-10-01 13:00:10 +0200

    feat: Add V3Instrumentation-Pass

---
+++ src/V3Instrumentation.h
@@ -0,0 +1,33 @@
+// -*- mode: C++; c-file-style: "cc-mode" -*-
+//*************************************************************************
+// DESCRIPTION: Verilator:
+//
+// Code available from: https://verilator.org
+//
+//*************************************************************************
+//
+// Copyright 2003-2025 by Wilson Snyder. This program is free software; you
+// can redistribute it and/or modify it under the terms of either the GNU
+// Lesser General Public License Version 3 or the Perl Artistic License
+// Version 2.0.
+// SPDX-License-Identifier: LGPL-3.0-only OR Artistic-2.0
+//
+//*************************************************************************
+
+#ifndef VERILATOR_V3INSTRUMENTATION_H_
+#define VERILATOR_V3INSTRUMENTATION_H_
+
+#include "config_build.h"
+#include "verilatedos.h"
+
+class AstNetlist;
+
+//=========================================================================
+
+class V3Instrumentation final {
+public:
+    static void findTargets(AstNetlist* nodep) VL_MT_DISABLED;
+    static void instrument(AstNetlist* nodep) VL_MT_DISABLED;
+};
+
+#endif  // Guard
