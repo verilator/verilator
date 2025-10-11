5977760443d88b42946f69e4cc0ea02182429f6b
Yilou Wang <wangyilou123@gmail.com>
2025-10-11 23:04:56 +0200

    update tests

--- test_regress/t/t_std_randomize_with.py
+++ test_regress/t/t_std_randomize_with.py
@@ -9,8 +9,13 @@
 
 import vltest_bootstrap
 
-test.scenarios('linter')
+test.scenarios('simulator')
 
-test.lint(fails=True, expect_filename=test.golden_filename)
+if not test.have_solver:
+    test.skip("No constraint solver installed")
+
+test.compile()
+
+test.execute()
 
 test.passes()
