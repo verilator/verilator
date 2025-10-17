31af590f7f7db6421c8fef23eb1f4d121729b1ef
Ryszard Rozak <rrozak@antmicro.com>
2025-10-17 15:40:05 +0200

    Change test and add output
    Signed-off-by: Ryszard Rozak <rrozak@antmicro.com>

--- test_regress/t/t_force_unpacked_unsup.v
+++ test_regress/t/t_force_unpacked_unsup.v
@@ -8,11 +8,6 @@
 `define checkh(gotv,expv) do if ((gotv) !== (expv)) begin $write("%%Error: %s:%0d:  got='h%x exp='h%x\n", `__FILE__,`__LINE__, (gotv), (expv)); `stop; end while(0)
 `define checkr(gotv,expv) do if ((gotv) != (expv)) begin $write("%%Error: %s:%0d:  got=%f exp=%f\n", `__FILE__,`__LINE__, (gotv), (expv)); `stop; end while(0);
 
-typedef struct  {
-   logic        l;
-   real         r;
-} struct_t;
-
 module t (  /*AUTOARG*/
     // Inputs
     clk
@@ -21,29 +16,29 @@ module t (  /*AUTOARG*/
 
   integer cyc = 0;
 
-  struct_t in[2];
+  real in[2];
 
   // Test loop
   always @(posedge clk) begin
     cyc <= cyc + 1;
     if (cyc == 0) begin
-      in[0].r <= 1;
+      in[0] <= 1;
     end else if (cyc == 1) begin
-      `checkr(in[0].r, 1);
+      `checkr(in[0], 1);
     end else if (cyc == 2) begin
-      force in[0].r = 0;
+      force in[0] = 0;
     end else if (cyc == 3) begin
-      `checkr(in[0].r, 0);
-      in[0].r <= 1;
+      `checkr(in[0], 0);
+      in[0] <= 1;
     end else if (cyc == 4) begin
-      `checkr(in[0].r, 0);
+      `checkr(in[0], 0);
     end else if (cyc == 5) begin
-      release in[0].r;
+      release in[0];
     end else if (cyc == 6) begin
-      `checkr(in[0].r, 0);
-      in[0].r <= 1;
+      `checkr(in[0], 0);
+      in[0] <= 1;
     end else if (cyc == 7) begin
-      `checkr(in[0].r, 1);
+      `checkr(in[0], 1);
     end
     else if (cyc == 8) begin
       $write("*-* All Finished *-*\n");
