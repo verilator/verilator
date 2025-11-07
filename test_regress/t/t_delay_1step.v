5355ffc2f8378a405237aec4684985331096939c
Pawel Kojma <pkojma@internships.antmicro.com>
2025-11-07 14:20:48 +0100

    Change unit test for 1step
    Signed-off-by: Pawel Kojma <pkojma@internships.antmicro.com>

--- test_regress/t/t_delay_1step.v
+++ test_regress/t/t_delay_1step.v
@@ -1,10 +1,12 @@
 module t;
-   timeunit 1s;
+   timeunit 10s;
    timeprecision 1s;
 
    initial begin
-      #1step;
+      #1;
       if ($time != 1) $stop;
+      repeat(10) #1step;
+      if ($time != 2) $stop;
       #10;
       $write("*-* All Finished *-*\n");
       $finish;
