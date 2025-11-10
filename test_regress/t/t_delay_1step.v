2a8b5766718f71910109d316762b08591fea9ea5
Pawel Kojma <pkojma@internships.antmicro.com>
2025-11-10 09:49:31 +0100

    Appy suggestions
    Signed-off-by: Pawel Kojma <pkojma@internships.antmicro.com>

--- test_regress/t/t_delay_1step.v
+++ test_regress/t/t_delay_1step.v
@@ -1,14 +1,20 @@
-module t;
-   timeunit 10s;
-   timeprecision 1s;
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain, for
+// any use, without warranty, 2025 by Antmicro.
+// SPDX-License-Identifier: CC0-1.0
 
-   initial begin
-      #1;
-      if ($time != 1) $stop;
-      repeat(10) #1step;
-      if ($time != 2) $stop;
-      #10;
-      $write("*-* All Finished *-*\n");
-      $finish;
-   end
+module t;
+  timeunit 10s;
+  timeprecision 1s;
+
+  initial begin
+    #1;
+    if ($time != 1) $stop;
+    repeat(10) #1step;
+    if ($time != 2) $stop;
+    #10;
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
 endmodule
