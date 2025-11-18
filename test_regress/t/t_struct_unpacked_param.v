4f6a6a1a5db4e0d8b587ac46aa4397c35e957d97
Jonathan Drolet <jonathan.drolet@gmail.com>
2025-11-18 18:08:53 -0500

    Added logic to constify unpacked struct
    - Only simple unpacked struct are supported (struct of struct is
      unsupported)

--- test_regress/t/t_struct_unpacked_param.v
+++ test_regress/t/t_struct_unpacked_param.v
@@ -9,38 +9,28 @@
 `define checkd(gotv, expv) do if ((gotv) !== (expv)) begin $write("%%Error: %s:%0d:  got=%0d exp=%0d\n", `__FILE__,`__LINE__, (gotv), (expv)); `stop; end while(0);
 // verilog_format: on
 
-package config_pkg;
-  typedef struct {
-    int UPPER0;
-    struct {
-      int USE_QUAD0;
-      int USE_QUAD1;
-      int USE_QUAD2;
-    } mac;
-    int UPPER2;
-  } config_struct_t;
-
-endpackage
-
 module t;
-  import config_pkg::*;
+
+  typedef struct {
+    int a;
+    int b;
+    int c;
+  } struct_t;
 
   // Constant value unpacked struct support is limited at the moment.
   // Only localparams are supported, returning constant unpacked structure
   // from function or passing unpacked structure as parameters is not
   // (yet) supported
-  localparam config_struct_t MY_CONFIG = '{
-      UPPER0: 10,
-      mac: '{USE_QUAD0: 4, USE_QUAD1: 5, USE_QUAD2: 6},
-      UPPER2: 20
-  };
+  localparam struct_t MY_STRUCT = '{a: 10, b: 5, c: 20};
+
+  // Make standalone localparam to ensure MY_STRUCT is const
+  localparam int C = MY_STRUCT.c;
 
   initial begin
-    `checkd(MY_CONFIG.UPPER0, 10);
-    `checkd(MY_CONFIG.mac.USE_QUAD0, 4);
-    `checkd(MY_CONFIG.mac.USE_QUAD1, 5);
-    `checkd(MY_CONFIG.mac.USE_QUAD2, 6);
-    `checkd(MY_CONFIG.UPPER2, 20);
+    `checkd(MY_STRUCT.a, 10);
+    `checkd(MY_STRUCT.b, 5);
+    `checkd(MY_STRUCT.c, 20);
+    `checkd(C, 20);
     $write("*-* All Finished *-*\n");
     $finish;
   end
