b46b08cfb2b4d81cf86cceaba7d9f24fe22b2aca
Dan Ruelas-Petrisko <petrisko@cs.washington.edu>
2025-11-19 15:05:23 -0800

    nit

--- test_regress/t/t_config_multitop.v
+++ test_regress/t/t_config_multitop.v
@@ -4,7 +4,7 @@
 // any use, without warranty, 2025 by Wilson Snyder.
 // SPDX-License-Identifier: CC0-1.0
 
-// verilator lint_off "MULTITOP"
+// verilator lint_off MULTITOP
 module m1;
   initial $display("In '%m'");
 endmodule
@@ -16,5 +16,5 @@ endmodule
 config cfg1;
   design m1 m2;
 endconfig : cfg1  // Test end label
-// verilator lint_on "MULTITOP"
+// verilator lint_on MULTITOP
 
