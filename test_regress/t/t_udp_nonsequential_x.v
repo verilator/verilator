197dc25e39b1b359e211d19217749bca96c29a09
Michael Bikovitsky <biko@bikodbg.com>
2025-11-22 23:24:45 +0200

    Fix X handling in UDPs

---
+++ test_regress/t/t_udp_nonsequential_x.v
@@ -0,0 +1,49 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain, for
+// any use, without warranty, 2025 by Michael Bikovitsky.
+// SPDX-License-Identifier: CC0-1.0
+
+module t ();
+
+  wire true1;
+  not1 a(true1, '0);
+
+  wire false1;
+  not1 b(false1, '1);
+
+  wire true2;
+  not1 c(true2, '0);
+
+  wire false2;
+  not1 d(false2, '1);
+
+  initial begin
+    if (true1 != '1) $stop;
+    if (false1 != '0) $stop;
+    if (true2 != '1) $stop;
+    if (false2 != '0) $stop;
+    $finish;
+  end
+
+endmodule
+
+primitive not1 (q, d);
+  output q;
+  input  d;
+  table
+    0 : 1;
+    1 : 0;
+    x : x;
+  endtable
+endprimitive
+
+primitive not2 (q, d);
+  output q;
+  input  d;
+  table
+    0 : 1;
+    1 : 0;
+    X : X;
+  endtable
+endprimitive
