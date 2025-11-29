c432766ed2a9246a0e5e1f7f10302917bd1ae6b2
Matthew Ballance <matt.ballance@gmail.com>
2025-11-29 07:56:01 -0800

    Add test for #6735
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

---
+++ test_regress/t/t_dpi_inline_new.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# DESCRIPTION: Verilator: Verilog Test driver/expect definition
+#
+# Copyright 2025 by Wilson Snyder. This program is free software; you
+# can redistribute it and/or modify it under the terms of either the GNU
+# Lesser General Public License Version 3 or the Perl Artistic License
+# Version 2.0.
+# SPDX-License-Identifier: LGPL-3.0-only OR Artistic-2.0
+
+import vltest_bootstrap
+
+
+test.scenarios('simulator')
+
+# Compile the SystemVerilog test with the DPI C stub.
+test.compile(v_flags2=["t/t_dpi_inline_new.c"])
+
+test.execute()
+
+test.passes()
