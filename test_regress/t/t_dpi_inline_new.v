0a5fa13d2458815aa97ab723c5bc71399af39121
Matthew Ballance <matt.ballance@gmail.com>
2025-11-29 09:31:38 -0800

    Update indent: 4->2 spaces
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- test_regress/t/t_dpi_inline_new.v
+++ test_regress/t/t_dpi_inline_new.v
@@ -8,44 +8,44 @@
 
 package pyhdl_if;
 
-    typedef chandle PyObject;
-    import "DPI-C" context function PyObject _pyhdl_if_PyTuple_GetItem(input PyObject p0,
-                                                                        input longint unsigned p1);
+  typedef chandle PyObject;
+  import "DPI-C" context function PyObject _pyhdl_if_PyTuple_GetItem(input PyObject p0,
+                                    input longint unsigned p1);
 
-    function PyObject PyTuple_GetItem(input PyObject p0, input longint unsigned p1);
-        return _pyhdl_if_PyTuple_GetItem(p0, p1);
+  function PyObject PyTuple_GetItem(input PyObject p0, input longint unsigned p1);
+    return _pyhdl_if_PyTuple_GetItem(p0, p1);
+  endfunction
+
+  class py_object;
+    function new(PyObject obj);
+    endfunction
+  endclass
+
+  class py_tuple;
+
+    function py_object get_item(int idx);
+      py_object ret = new(PyTuple_GetItem(null, longint'(idx)));
+      return ret;
     endfunction
 
-    class py_object;
-        function new(PyObject obj);
-        endfunction
-    endclass
-
-    class py_tuple;
-
-        function py_object get_item(int idx);
-            py_object ret = new(PyTuple_GetItem(null, longint'(idx)));
-            return ret;
-        endfunction
-
-    endclass
+  endclass
 
 endpackage
 
 module t (/*AUTOARG*/
-    // Inputs
-    clk
-    );
-    input clk;
+  // Inputs
+  clk
+  );
+  input clk;
 
-    import pyhdl_if::*;
+  import pyhdl_if::*;
 
-    initial begin
-        py_tuple t0 = new;
-        py_object o;
-        o = t0.get_item(1);
-        $write("*-* All Finished *-*\n");
-        $finish;
-    end
+  initial begin
+    py_tuple t0 = new;
+    py_object o;
+    o = t0.get_item(1);
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
 
 endmodule
