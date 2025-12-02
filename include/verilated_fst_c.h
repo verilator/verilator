60fe2c873cc1ad270197b42d751868192f136ff4
Geza Lore <gezalore@gmail.com>
2025-12-02 12:19:45 +0000

    Optimize trace initialization code size (#6749)

--- include/verilated_fst_c.h
+++ include/verilated_fst_c.h
@@ -110,7 +110,7 @@ public:
     //=========================================================================
     // Internal interface to Verilator generated code
 
-    void pushPrefix(const std::string&, VerilatedTracePrefixType);
+    void pushPrefix(const char*, VerilatedTracePrefixType);
     void popPrefix();
 
     void declEvent(uint32_t code, uint32_t fidx, const char* name, int dtypenum,
