494a6217233ffa5d7fbb518fb3ab89f85832642b
em2machine <eric.mejdrich@imec.be>
2025-12-03 15:17:01 +0100

    tests for latest excursion

---
+++ test_regress/t/t_lparam_assign_iface_typedef_nested_modules_pkg4.v
@@ -0,0 +1,51 @@
+package aerial;
+  typedef struct packed {
+    int unsigned NumCca;
+  } cfg_t;
+endpackage
+
+package cca;
+  typedef struct packed {
+    int unsigned NumCc;
+  } cfg_t;
+endpackage
+
+package amb;
+  typedef struct packed {
+    logic start;
+  } rule_t;
+endpackage
+
+interface tb_if #(parameter cca::cfg_t cfg=0)();
+  typedef logic [$clog2(cfg.NumCc)-1:0] cc_index_t;
+endinterface
+
+module modA#(
+  parameter aerial::cfg_t cfg=0
+  //
+  ,localparam type rule_t = amb::rule_t
+)();
+
+  localparam cca::cfg_t cca_cfg = '{
+    NumCc : 4
+  };
+
+  tb_if #(cca_cfg) tb_io();
+  localparam type cc_index_t = tb_io.cc_index_t;
+endmodule
+
+module tb();
+
+  localparam aerial::cfg_t aer_cfg= '{
+    NumCca : 2
+  };
+
+  modA #(aer_cfg) modA();
+
+  initial begin
+    #1;
+    $finish;
+  end
+
+endmodule
+
