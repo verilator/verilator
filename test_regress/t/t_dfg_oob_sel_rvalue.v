5278f420255b34170113d8d7a100af3b068c86b7
Geza Lore <gezalore@gmail.com>
2025-12-06 15:31:27 +0000

    Fix Dfg assertion on out of bounds selects

---
+++ test_regress/t/t_dfg_oob_sel_rvalue.v
@@ -0,0 +1,21 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain, for
+// any use, without warranty, 2025 by Wilson Snyder.
+// SPDX-License-Identifier: CC0-1.0
+
+
+module t(
+   input  logic [0:0][2:0] i,
+   output logic            o
+);
+
+  always_comb begin
+    o = 1'b0;
+    // verilator unroll_full
+    for (int n = 0 ; n < 3; ++n) begin
+      o = o | i[n] == 3'd0; // Intentionally out of bounds
+    end
+  end
+
+endmodule
