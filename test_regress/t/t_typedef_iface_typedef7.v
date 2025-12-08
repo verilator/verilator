a6376cd93ba7ae617e5f405fe509b06a7d724c29
em2machine <>
2025-12-08 16:01:14 +0100

    test cleanup and new test for chained typedefs

--- test_regress/t/t_typedef_iface_typedef7.v
+++ test_regress/t/t_typedef_iface_typedef7.v
@@ -4,17 +4,9 @@
 // any use, without warranty, 2025 by Wilson Snyder.
 // SPDX-License-Identifier: CC0-1.0
 //
-//     assign localparam from interface typedef, single level nesting
-//
+//     Chained typedef aliases from an interface typedef
 
 `define stop $stop
-`define checkd(gotv,expv) \
-  do if ((gotv) !== (expv)) begin \
-    $write("%%Error: %s:%0d:  got=%0d exp=%0d\n", \
-            `__FILE__,`__LINE__, (gotv), (expv)); \
-    `stop; \
-  end while(0);
-
 `define checkh(gotv,expv) \
   do if ((gotv) !== (expv)) begin \
     $write("%%Error: %s:%0d:  got=%0h exp=%0h\n", \
@@ -30,28 +22,15 @@ interface x_if #(
   } rq_t;
 endinterface
 
-interface y_if #(
-  parameter int p_dwidth = 7
-)();
-  typedef struct packed {
-    logic [p_dwidth-1:0] data;
-  } rs_t;
-endinterface
-
-interface z_if #(
-  parameter int p_awidth = 3
-  ,parameter int p_dwidth = 9
-);
-  x_if #(p_awidth) x_if0();
-  y_if #(p_dwidth) y_if0();
-endinterface
-
 module top();
-  z_if #(.p_awidth(16) ,.p_dwidth(8)) if0 [2] ();
+  x_if #(.p_awidth(16)) if0();
 
-  typedef if0[0].x_if0.rq_t rq_t;
+  // First alias of interface typedef
+  typedef if0.rq_t my_rq_t;
+  // Second alias of the alias
+  typedef my_rq_t my_rq2_t;
 
-  rq_t rq;
+  my_rq2_t rq;
 
   always_comb begin
     rq.addr = 'h1234;
@@ -64,3 +43,4 @@ module top();
     $finish;
   end
 endmodule
+
