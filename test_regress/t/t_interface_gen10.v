2e0c793aaa461f68f765802f3e0f9e84d7767573
em2machine <>
2025-12-10 22:25:04 +0100

    additional test

--- test_regress/t/t_interface_gen10.v
+++ test_regress/t/t_interface_gen10.v
@@ -1,34 +1,43 @@
-// DESCRIPTION: Verilator: Verilog Test module
-//
-// This file ONLY is placed into the Public Domain, for any use,
-// without warranty.
-// SPDX-License-Identifier: CC0-1.0
 
-// bug998
+`define stop $stop
+`define checkd(gotv,expv) \
+  do if ((gotv) !== (expv)) begin \
+    $write("%%Error: %s:%0d:  got=%0d exp=%0d\n", \
+            `__FILE__,`__LINE__, (gotv), (expv)); \
+    `stop; \
+  end while(0);
 
-interface intf
-  #(parameter PARAM = 0)
-   ();
-   logic val;
-   function integer func (); return 5; endfunction
+interface a_if #(parameter int a_param=0)();
+  logic[a_param-1:0] x;
+
+  function void to_if(input logic[a_param-1:0] x_in);
+    x = x_in;
+  endfunction
+  function logic[a_param-1:0] from_if();
+    return x;
+  endfunction
 endinterface
 
-module t1(intf mod_intf);
-   initial begin
-      $display("%m %d", mod_intf.val);
-   end
-endmodule
+module tb();
+  genvar a;
 
-module t();
-   generate
-      begin : TestIf
-         intf #(.PARAM(1)) my_intf [0:0] ();
-         t1 t (.mod_intf(my_intf[0]));
+  generate
+    for (a=1; a<3; a++) begin : gen_a
+      a_if #(.a_param(a)) a_if_a();
+      initial begin
+        #1;
+        a_if_a.to_if(a);
       end
-   endgenerate
+    end
+  endgenerate
 
-   initial begin
-      $write("*-* All Finished *-*\n");
-      $finish;
-   end
+  initial begin
+    #1;
+    #1;
+    `checkd(gen_a[1].a_if_a.from_if(), 'h1);
+    `checkd(gen_a[2].a_if_a.from_if(), 'h2);
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+
 endmodule
