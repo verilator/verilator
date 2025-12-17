e6a8dbde476dfd6e79c0d7ae6f813de2087c70d1
Igor Zaworski <izaworski@internships.antmicro.com>
2025-12-17 12:51:01 +0100

    Removal of reuse mechanizm
    Signed-off-by: Igor Zaworski <izaworski@internships.antmicro.com>

--- src/V3SchedVirtIface.cpp
+++ src/V3SchedVirtIface.cpp
@@ -42,10 +42,7 @@ class VirtIfaceVisitor final : public VNVisitor {
 private:
     // NODE STATE
     // AstIface::user1() -> AstVarScope*. Trigger var for this interface
-    // AstCFunc::user1() -> bool. Is visited
-    // AstCFunc::user2() -> bool. Has timing control
     const VNUser1InUse m_user1InUse;
-    const VNUser2InUse m_user2InUse;
 
     // TYPES
     using OnWriteToVirtIface = std::function<void(AstVarRef*, AstIface*)>;
@@ -54,15 +51,10 @@ private:
 
     // STATE
     AstNetlist* const m_netlistp;  // Root node
-    AstAssign* m_trigAssignp = nullptr;  // Previous/current trigger assignment
-    AstIface* m_trigAssignIfacep = nullptr;  // Interface type whose trigger is assigned
-                                             // by m_trigAssignp
-    AstVar* m_trigAssignMemberVarp = nullptr;  // Member pointer whose trigger is assigned
     V3UniqueNames m_vifTriggerNames{"__VvifTrigger"};  // Unique names for virt iface
                                                        // triggers
     VirtIfaceTriggers m_triggers;  // Interfaces and corresponding trigger vars
     AstNodeStmt* m_curStmt = nullptr;  // Current statement
-    bool m_hasTimingControl = false;  // Whether current CFunc has timing control
 
     // METHODS
     // For each write across a virtual interface boundary
@@ -118,12 +110,6 @@ private:
 
     // VISITORS
     void visit(AstNodeProcedure* nodep) override {
-        VL_RESTORER(m_trigAssignp);
-        m_trigAssignp = nullptr;
-        VL_RESTORER(m_trigAssignIfacep);
-        m_trigAssignIfacep = nullptr;
-        VL_RESTORER(m_trigAssignMemberVarp);
-        m_trigAssignMemberVarp = nullptr;
         // Not sure if needed, but be paranoid to match previous behavior as didn't optimize
         // before ..
         if (VN_IS(nodep, AlwaysPost) && writesToVirtIface(nodep)) {
@@ -131,110 +117,18 @@ private:
         }
         iterateChildren(nodep);
     }
-    void visit(AstCFunc* nodep) override {
-        if (nodep->user1SetOnce()) return;
-        // By default set hasTiming to true - it may generate some false positives but it is better
-        // than generating false negatives.
-        // False positive may occur when there are are two funcs and they both call each other
-        nodep->user2(v3Global.usesTiming());
-        VL_RESTORER(m_trigAssignp);
-        m_trigAssignp = nullptr;
-        VL_RESTORER(m_trigAssignIfacep);
-        m_trigAssignIfacep = nullptr;
-        VL_RESTORER(m_trigAssignMemberVarp);
-        m_trigAssignMemberVarp = nullptr;
-        VL_RESTORER(m_hasTimingControl);
-        m_hasTimingControl = false;
-        iterateChildren(nodep);
-        nodep->user2(m_hasTimingControl);
-    }
-    void visit(AstNodeCCall* nodep) override {
-        iterate(nodep->funcp());
-        if (nodep->funcp()->user2()) {
-            m_trigAssignp = nullptr;
-            m_trigAssignIfacep = nullptr;
-            m_trigAssignMemberVarp = nullptr;
-            m_hasTimingControl = true;
-        }
+    void visit(AstNodeIf* const nodep) override {
+        unsupportedWriteToVirtIface(nodep->condp(), "if condition");
         iterateChildren(nodep);
     }
-    void visit(AstNodeIf* nodep) override {
-        unsupportedWriteToVirtIfaceMember(nodep->condp(), "if condition");
-        iterateAndNextNull(nodep->condp());
-        bool hasTimingControl;
-        {
-            VL_RESTORER(m_trigAssignp);
-            VL_RESTORER(m_trigAssignIfacep);
-            VL_RESTORER(m_trigAssignMemberVarp);
-            VL_RESTORER(m_hasTimingControl);
-            m_hasTimingControl = false;
-            iterateAndNextNull(nodep->thensp());
-            hasTimingControl = m_hasTimingControl;
-        }
-        {
-            VL_RESTORER(m_trigAssignp);
-            VL_RESTORER(m_trigAssignIfacep);
-            VL_RESTORER(m_trigAssignMemberVarp);
-            VL_RESTORER(m_hasTimingControl);
-            m_hasTimingControl = false;
-            iterateAndNextNull(nodep->elsesp());
-            hasTimingControl |= m_hasTimingControl;
-        }
-        if (hasTimingControl) {
-            // Clear the trigger assignment, as there could have been timing controls in either
-            // branch
-            m_trigAssignp = nullptr;
-            m_trigAssignIfacep = nullptr;
-            m_trigAssignMemberVarp = nullptr;
-            m_hasTimingControl = true;
-        }
-    }
-    void visit(AstLoop* nodep) override {
+    void visit(AstLoop* const nodep) override {
         UASSERT_OBJ(!nodep->contsp(), nodep, "'contsp' only used before LinkJump");
-        iterateAndNextNull(nodep->contsp());
-        bool hasTimingControl;
-        {
-            VL_RESTORER(m_trigAssignp);
-            VL_RESTORER(m_trigAssignIfacep);
-            VL_RESTORER(m_trigAssignMemberVarp);
-            VL_RESTORER(m_hasTimingControl);
-            m_hasTimingControl = false;
-            iterateAndNextNull(nodep->stmtsp());
-            hasTimingControl = m_hasTimingControl;
-        }
-        if (hasTimingControl) {
-            // Clear the trigger assignment, as there could have been timing controls in the loop
-            m_trigAssignp = nullptr;
-            m_trigAssignIfacep = nullptr;
-            m_trigAssignMemberVarp = nullptr;
-            m_hasTimingControl = true;
-        }
+        iterateChildren(nodep);
     }
     void visit(AstLoopTest* nodep) override {
         unsupportedWriteToVirtIfaceMember(nodep->condp(), "loop condition");
     }
-    void visit(AstJumpBlock* nodep) override {
-        {
-            VL_RESTORER(m_trigAssignp);
-            VL_RESTORER(m_trigAssignIfacep);
-            VL_RESTORER(m_trigAssignMemberVarp);
-            iterateChildren(nodep);
-        }
-        if (v3Global.usesTiming()) {
-            // Clear the trigger assignment, as there could have been timing controls in the jump
-            // block
-            m_trigAssignp = nullptr;
-            m_trigAssignIfacep = nullptr;
-            m_trigAssignMemberVarp = nullptr;
-        }
-    }
     void visit(AstNodeStmt* nodep) override {
-        if (v3Global.usesTiming() && nodep->isTimingControl()) {
-            m_trigAssignp = nullptr;
-            m_trigAssignIfacep = nullptr;
-            m_trigAssignMemberVarp = nullptr;
-            m_hasTimingControl = true;
-        }
         VL_RESTORER(m_curStmt);
         m_curStmt = nodep;
         iterateChildren(nodep);
@@ -262,30 +156,12 @@ private:
             }
         }
 
-        FileLine* const flp = nodep->fileline();
         if (ifacep && memberVarp) {
-            if (ifacep != m_trigAssignIfacep || memberVarp != m_trigAssignMemberVarp) {
-                // Write to different interface member than before - need new trigger assignment
-                m_trigAssignIfacep = ifacep;
-                m_trigAssignMemberVarp = memberVarp;
-                m_trigAssignp = nullptr;
-            }
-            if (!m_trigAssignp) {
-                m_trigAssignp
-                    = new AstAssign{flp, createVirtIfaceMemberTriggerRefp(flp, ifacep, memberVarp),
-                                    new AstConst{flp, AstConst::BitTrue{}}};
-                m_curStmt->addNextHere(m_trigAssignp);
-            }
+            FileLine* const flp = nodep->fileline();
+            m_curStmt->addNextHere(
+                new AstAssign{flp, createVirtIfaceMemberTriggerRefp(flp, ifacep, memberVarp),
+                              new AstConst{flp, AstConst::BitTrue{}}});
         }
-    }
-    void visit(AstNodeExpr* const nodep) override {
-        if (v3Global.usesTiming() && nodep->isTimingControl()) {
-            m_trigAssignp = nullptr;
-            m_trigAssignIfacep = nullptr;
-            m_trigAssignMemberVarp = nullptr;
-            m_hasTimingControl = true;
-        }
-        iterateChildren(nodep);
     }
     void visit(AstNode* nodep) override { iterateChildren(nodep); }
 
