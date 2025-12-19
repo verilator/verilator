7fba62d2be55c04fe26871ee0e4bff5efb123442
Igor Zaworski <izaworski@internships.antmicro.com>
2025-12-19 10:40:32 +0100

    Test improvements
    Signed-off-by: Igor Zaworski <izaworski@internships.antmicro.com>

--- test_regress/t/t_timing_eval_act.v
+++ test_regress/t/t_timing_eval_act.v
@@ -10,15 +10,17 @@ module t;
   event a;
   event e;
 
-  // This function is required to prevent inlining clk_inv as ~clk
-  function calc(logic x);
-    return ~x;
-  endfunction
+  // This $c is required to prevent inlining clk_inv as ~clk
+  assign clk_inv = $c(1) & ~clk;
 
-  assign clk_inv = calc(clk);
+  // This is needed to provide right order of resumption in scheduler
+  initial begin
+    @a;
+    @(posedge clk_inv);
+    @e;
+  end
 
   initial begin
-    if ($c("0")) @a;  // This is needed to provide right order of resumption in scheduler
     forever begin
       @(posedge clk_inv) begin
         clk = 1;
@@ -38,8 +40,10 @@ module t;
     ->a;
     clk = 0;
     #2 $stop;   // If tests fails due to this $stop it is because
-                // order of resumption has changed in scheduler.
+                // order of resumption is not correct in the scheduler.
                 // Order of resumption is crucial in this test so,
-                // it has to be provided somehow
+                // it has to be provided somehow.
+                // This shall never happen since regex is checking
+                // whether order is right.
   end
 endmodule
