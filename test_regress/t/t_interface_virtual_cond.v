747befb3a03f7f5a486a097dd1ef6168fa52fa46
Krzysztof Bieganski <kbieganski@antmicro.com>
2025-12-23 15:35:58 +0100

    Fix formatting
    Signed-off-by: Krzysztof Bieganski <kbieganski@antmicro.com>

--- test_regress/t/t_interface_virtual_cond.v
+++ test_regress/t/t_interface_virtual_cond.v
@@ -5,23 +5,23 @@
 // SPDX-License-Identifier: CC0-1.0
 
 interface Bus;
-   logic [15:0] data;
+  logic [15:0] data;
 endinterface
 
 module t;
-   Bus intf();
-   virtual Bus vif;
+  Bus intf();
+  virtual Bus vif;
 
-   function logic get_vif(inout virtual Bus vif);
-      vif = intf;
-      return 0;
-   endfunction
+  function logic get_vif(inout virtual Bus vif);
+    vif = intf;
+    return 0;
+  endfunction
 
-   initial begin
-      if (get_vif(vif));
-      while (get_vif(vif));
-      do ; while (get_vif(vif));
-      for (int i = 0; get_vif(vif); i++);
-   end
+  initial begin
+    if (get_vif(vif));
+    while (get_vif(vif));
+    do ; while (get_vif(vif));
+    for (int i = 0; get_vif(vif); i++);
+  end
 
 endmodule
