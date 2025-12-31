8d4586f2f2bb48302792515fb31be20d9843bbb0
Ryszard Rozak <rrozak@antmicro.com>
2025-12-31 16:32:11 +0100

    Add case to test and reformat it
    Signed-off-by: Ryszard Rozak <rrozak@antmicro.com>

--- test_regress/t/t_assert_disable_iff.v
+++ test_regress/t/t_assert_disable_iff.v
@@ -4,55 +4,61 @@
 // without warranty, 2019 by Peter Monsson.
 // SPDX-License-Identifier: CC0-1.0
 
-module t (/*AUTOARG*/
-   // Inputs
-   clk
-   );
+module t (  /*AUTOARG*/
+    // Inputs
+    clk
+);
 
-   input clk;
-   int cyc;
+  input clk;
+  int cyc;
 
-   Test test (/*AUTOINST*/
-              // Inputs
-              .clk                      (clk));
+  Test test (  /*AUTOINST*/
+      // Inputs
+      .clk(clk),
+      .cyc(cyc)
+  );
 
-   always @(posedge clk) begin
-      cyc <= cyc + 1;
-      if (cyc == 10) begin
-         $write("*-* All Finished *-*\n");
-         $finish;
-      end
-   end
+  always @(posedge clk) begin
+    cyc <= cyc + 1;
+    if (cyc == 10) begin
+      $write("*-* All Finished *-*\n");
+      $finish;
+    end
+  end
 
 endmodule
 
 module Test (
-    input clk
+    input clk,
+    input int cyc
 );
 
 `ifdef FAIL_ASSERT_1
-   assert property (@(posedge clk) disable iff (0) 0)
-   else $display("wrong disable");
+  assert property (@(posedge clk) disable iff (0) 0)
+  else $display("wrong disable");
 `endif
 
-   assert property (@(posedge clk) disable iff (1) 0) $stop;
-   else $stop;
+  assert property (@(posedge clk) disable iff (1) 0) $stop;
+  else $stop;
 
-   assert property (@(posedge clk) disable iff (1) 1) $stop;
-   else $stop;
+  assert property (@(posedge clk) disable iff (1) 1) $stop;
+  else $stop;
 
-   assert property (@(posedge clk) disable iff (0) 1);
+  assert property (@(posedge clk) disable iff (0) 1);
 
-   //
-   // Cover properties behave differently
-   //
+  // Pass 1st cycle
+  assert property (@(cyc) disable iff (cyc != $sampled(cyc)) cyc == 0);
 
-   cover property (@(posedge clk) disable iff (1) 1) $stop;
+  //
+  // Cover properties behave differently
+  //
 
-   cover property (@(posedge clk) disable iff (1) 0) $stop;
+  cover property (@(posedge clk) disable iff (1) 1) $stop;
 
-   cover property (@(posedge clk) disable iff (0) 1) $display("*COVER: ok");
+  cover property (@(posedge clk) disable iff (1) 0) $stop;
 
-   cover property (@(posedge clk) disable iff (0) 0) $stop;
+  cover property (@(posedge clk) disable iff (0) 1) $display("*COVER: ok");
+
+  cover property (@(posedge clk) disable iff (0) 0) $stop;
 
 endmodule
