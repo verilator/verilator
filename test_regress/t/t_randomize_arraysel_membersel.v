4b690dcf130eedd4f187d6956782701416777f7e
Ryszard Rozak <rrozak@antmicro.com>
2026-01-05 12:55:29 +0100

    Test inline randomization
    Signed-off-by: Ryszard Rozak <rrozak@antmicro.com>

--- test_regress/t/t_randomize_arraysel_membersel.v
+++ test_regress/t/t_randomize_arraysel_membersel.v
@@ -80,6 +80,12 @@ module t;
       if (example.b.d[1] < 2 || example.b.d[1] > 6) $stop;
     end
 
+    // Inline randomization of object with array:
+    repeat (5) begin
+      if (example.randomize(b) with {b.d[1] inside {[2 : 6]};} == 0) $stop;
+      if (example.b.d[1] < 2 || example.b.d[1] > 6) $stop;
+    end
+
     $write("*-* All Finished *-*\n");
     $finish;
   end
