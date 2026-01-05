d0cfdccc15b80af10a3d708841dc5cbdfcaede68
Ryszard Rozak <rrozak@antmicro.com>
2026-01-05 11:37:41 +0100

    Format test
    Signed-off-by: Ryszard Rozak <rrozak@antmicro.com>

--- test_regress/t/t_randomize_arraysel_membersel.v
+++ test_regress/t/t_randomize_arraysel_membersel.v
@@ -6,7 +6,7 @@
 
 class inner_class;
   logic [3:0] c;
-  logic [3:0] d[2];
+  logic [3:0] d [2];
 endclass
 
 class test_class;
@@ -18,87 +18,66 @@ endclass
 module t;
   initial begin
     test_class example;
-    example = new;
+    example   = new;
     example.b = new;
 
     // Simple array element access
-    repeat(5) begin
-      if (std::randomize(example.member[1]) with {example.member[1] inside {[0 : 3]};} == 0) begin
-        $stop;
-      end
-      if (example.member[1] > 3) begin
-        $stop;
-      end
+    repeat (5) begin
+      if (std::randomize(example.member[1]) with {example.member[1] inside {[0 : 3]};} == 0) $stop;
+      if (example.member[1] > 3) $stop;
     end
 
     // Different array indices
-    repeat(5) begin
-      if (std::randomize(example.member[0]) with {example.member[0] inside {[5 : 7]};} == 0) begin
-        $stop;
-      end
-      if (example.member[0] < 5 || example.member[0] > 7) begin
-        $stop;
-      end
+    repeat (5) begin
+      if (std::randomize(example.member[0]) with {example.member[0] inside {[5 : 7]};} == 0) $stop;
+      if (example.member[0] < 5 || example.member[0] > 7) $stop;
     end
 
     // Last element
-    repeat(5) begin
-      if (std::randomize(example.member[4]) with {example.member[4] inside {[10 : 15]};} == 0) begin
+    repeat (5) begin
+      if (std::randomize(example.member[4]) with {example.member[4] inside {[10 : 15]};} == 0)
         $stop;
-      end
-      if (example.member[4] < 10 || example.member[4] > 15) begin
-        $stop;
-      end
+      if (example.member[4] < 10 || example.member[4] > 15) $stop;
     end
 
     // 2D array access
-    repeat(5) begin
-      if (std::randomize(example.member_2d[1][2]) with {example.member_2d[1][2] inside {[8 : 12]};} == 0) begin
+    repeat (5) begin
+      if (std::randomize(
+              example.member_2d[1][2]
+          ) with {
+            example.member_2d[1][2] inside {[8 : 12]};
+          } == 0)
         $stop;
-      end
-      if (example.member_2d[1][2] < 8 || example.member_2d[1][2] > 12) begin
-        $stop;
-      end
+      if (example.member_2d[1][2] < 8 || example.member_2d[1][2] > 12) $stop;
     end
 
     // 2D array different indices
-    repeat(5) begin
-      if (std::randomize(example.member_2d[0][0]) with {example.member_2d[0][0] inside {[1 : 4]};} == 0) begin
+    repeat (5) begin
+      if (std::randomize(
+              example.member_2d[0][0]
+          ) with {
+            example.member_2d[0][0] inside {[1 : 4]};
+          } == 0)
         $stop;
-      end
-      if (example.member_2d[0][0] < 1 || example.member_2d[0][0] > 4) begin
-        $stop;
-      end
+      if (example.member_2d[0][0] < 1 || example.member_2d[0][0] > 4) $stop;
     end
 
     // Nested object: obj.b.c
-    repeat(5) begin
-      if (std::randomize(example.b.c) with {example.b.c inside {[5 : 9]};} == 0) begin
-        $stop;
-      end
-      if (example.b.c < 5 || example.b.c > 9) begin
-        $stop;
-      end
+    repeat (5) begin
+      if (std::randomize(example.b.c) with {example.b.c inside {[5 : 9]};} == 0) $stop;
+      if (example.b.c < 5 || example.b.c > 9) $stop;
     end
 
     // Nested object with array: obj.b.d[0]
-    repeat(5) begin
-      if (std::randomize(example.b.d[0]) with {example.b.d[0] inside {[11 : 14]};} == 0) begin
-        $stop;
-      end
-      if (example.b.d[0] < 11 || example.b.d[0] > 14) begin
-        $stop;
-      end
+    repeat (5) begin
+      if (std::randomize(example.b.d[0]) with {example.b.d[0] inside {[11 : 14]};} == 0) $stop;
+      if (example.b.d[0] < 11 || example.b.d[0] > 14) $stop;
     end
 
     // Nested object with array: obj.b.d[1] (different index)
-    repeat(5) begin
-      if (std::randomize(example.b.d[1]) with {example.b.d[1] inside {[2 : 6]};} == 0) begin
-        $stop;
-      end
-      if (example.b.d[1] < 2 || example.b.d[1] > 6) begin
-        $stop;
-      end
+    repeat (5) begin
+      if (std::randomize(example.b.d[1]) with {example.b.d[1] inside {[2 : 6]};} == 0) $stop;
+      if (example.b.d[1] < 2 || example.b.d[1] > 6) $stop;
     end
 
     $write("*-* All Finished *-*\n");
