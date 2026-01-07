3d8dd31f279ce73f987bbac2575fc9418161c7ab
Oron <oron.port@cornell.edu>
2026-01-07 23:31:32 +0200

    Fix copyright notice format in test file

--- test_regress/t/t_lint_unused_func_param.py
+++ test_regress/t/t_lint_unused_func_param.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # DESCRIPTION: Verilator: Test for false positive UNUSEDSIGNAL in unused functions
 #
-# Copyright 2026.
+# Copyright 2026 by Wilson Snyder.
 # SPDX-License-Identifier: LGPL-3.0-only OR Artistic-2.0
 
 import vltest_bootstrap
