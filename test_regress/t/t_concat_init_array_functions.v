745e0d21b6fb89deb3f028c35db161c39714f073
Pawel Kojma <pkojma@internships.antmicro.com>
2026-01-08 14:05:20 +0100

    delete whitespaces
    Signed-off-by: Pawel Kojma <pkojma@internships.antmicro.com>

--- test_regress/t/t_concat_init_array_functions.v
+++ test_regress/t/t_concat_init_array_functions.v
@@ -14,18 +14,18 @@ endclass
 
 class B;
   virtual function int B_virt_func(int r[3], p[4]);
-    return r[0] + r[1] + r[2] + p[0] + p[1] + p[2] + p[3]; 
+    return r[0] + r[1] + r[2] + p[0] + p[1] + p[2] + p[3];
   endfunction
 endclass
 
 function int func_1_concat(int r[4]);
 // verilator no_inline_task
-  return r[0] + r[1] + r[2] + r[3]; 
+  return r[0] + r[1] + r[2] + r[3];
 endfunction
 
 function int func_2_concat(int r[3], int p[4]);
 // verilator no_inline_task
-  return r[0] + r[1] + r[2] + p[0] + p[1] + p[2] + p[3]; 
+  return r[0] + r[1] + r[2] + p[0] + p[1] + p[2] + p[3];
 endfunction
 
 module t;
@@ -34,7 +34,7 @@ module t;
   B b = new;
   initial begin
     a.r1 = {1,2};
-    a.r2 = 5; 
+    a.r2 = 5;
     s = func_1_concat({a.r1, a.r1});
     if (s != 6) $stop;
 
