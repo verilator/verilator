d6f32b10a88d0dae3b686c931d368a74611baa93
Pawel Kojma <pkojma@internships.antmicro.com>
2026-01-08 15:01:34 +0100

    fix copyright
    Signed-off-by: Pawel Kojma <pkojma@internships.antmicro.com>

--- test_regress/t/t_init_array_bad.v
+++ test_regress/t/t_init_array_bad.v
@@ -1,11 +1,8 @@
 // DESCRIPTION: Verilator: Verilog Test module for SystemVerilog 'alias'
 //
-// Simple bi-directional transitive alias test.
-//
 // This file ONLY is placed under the Creative Commons Public Domain, for
-// any use, without warranty, 2025 by Antmicro.
+// any use, without warranty, 2026 by Antmicro.
 // SPDX-License-Identifier: CC0-1.0
-//
 
 module t;
     bit [7:0] r[3] = {{8'h1, 8'h2}, 8'h5};
