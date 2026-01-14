337482d488126dd9adebf43d0f56899ed4f39764
Pawel Kojma <pkojma@internships.antmicro.com>
2026-01-14 14:11:39 +0100

    Fix memory leak
    Signed-off-by: Pawel Kojma <pkojma@internships.antmicro.com>

--- src/V3Randomize.cpp
+++ src/V3Randomize.cpp
@@ -1309,7 +1309,6 @@ class ConstraintExprVisitor final : public VNVisitor {
         // Convert to plain foreach
         FileLine* const fl = nodep->fileline();
 
-        AstNode* const arrayp = nodep->arrayp()->unlinkFrBack();
         if (!nodep->stmtsp()) {
             nodep->unlinkFrBack();
         } else if (m_wantSingle) {
@@ -1322,13 +1321,13 @@ class ConstraintExprVisitor final : public VNVisitor {
             AstCExpr* const cexprp = new AstCExpr{fl};
             cexprp->dtypeSetString();
             cexprp->add("([&]{\nstd::string ret;\n");
-            cexprp->add(new AstBegin{fl, "", new AstForeach{fl, arrayp, cstmtp}, true});
+            cexprp->add(new AstBegin{fl, "", new AstForeach{fl, nodep->arrayp()->unlinkFrBack(), cstmtp}, true});
             cexprp->add("return ret.empty() ? \"#b1\" : \"(bvand\" + ret + \")\";\n})()");
             nodep->replaceWith(new AstSFormatF{fl, "%@", false, cexprp});
         } else {
             iterateAndNextNull(nodep->stmtsp());
             nodep->replaceWith(new AstBegin{
-                fl, "", new AstForeach{fl, arrayp, nodep->stmtsp()->unlinkFrBackWithNext()},
+                fl, "", new AstForeach{fl, nodep->arrayp()->unlinkFrBack(), nodep->stmtsp()->unlinkFrBackWithNext()},
                 true});
         }
         VL_DO_DANGLING(nodep->deleteTree(), nodep);
