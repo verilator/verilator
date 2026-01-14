987a42a32d29ef0eddcf26a9a523fe2173796592
github action <action@example.com>
2026-01-14 07:50:42 +0000

    Apply 'make format'

--- test_regress/t/t_0.py
+++ test_regress/t/t_0.py
@@ -16,11 +16,14 @@ test.compile(verilator_flags2=[
     '-Wno-SIDEEFFECT', '--Wno-CONSTRAINTIGN', '--Wno-COVERIGN', '--Wno-SYMRSVDWORD',
     '--Wno-CASEINCOMPLETE', '--Wno-IGNOREDRETURN', '--Wno-DECLFILENAME', '--Wno-RANDC',
     '--Wno-UNUSED', '--Wno-UNDRIVEN', '--Wno-VARHIDDEN', '--Wno-EOFNEWLINE', '--Wno-IMPORTSTAR',
-    '--Wno-MISINDENT', '--Wno-MULTITOP', '--Wno-NONSTD', '--Wno-SHORTREAL', '--timescale','1ns/1ns',
-    '--vpi', 't/t_0_uvm_dpi/uvm_dpi.cc'
+    '--Wno-MISINDENT', '--Wno-MULTITOP', '--Wno-NONSTD', '--Wno-SHORTREAL', '--timescale',
+    '1ns/1ns', '--vpi', 't/t_0_uvm_dpi/uvm_dpi.cc'
 ])
 
-test.execute(all_run_flags=['+UVM_TESTNAME=test', '+UVM_NO_RELNOTES', '+UVM_CONFIG_DB_TRACE', '+UVM_OBJECTION_TRACE', '+UVM_PHASE_TRACE'])
+test.execute(all_run_flags=[
+    '+UVM_TESTNAME=test', '+UVM_NO_RELNOTES', '+UVM_CONFIG_DB_TRACE', '+UVM_OBJECTION_TRACE',
+    '+UVM_PHASE_TRACE'
+])
 
 test.file_grep_not(test.run_log_filename, r'UVM_FATAL\s*:\s*[^0\s]')
 
