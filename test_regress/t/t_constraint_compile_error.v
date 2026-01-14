f757d7c65868bedc15283f53d156e21dab33f7fe
Pawel Kojma <pkojma@internships.antmicro.com>
2026-01-14 15:29:55 +0100

    minimize test
    Signed-off-by: Pawel Kojma <pkojma@internships.antmicro.com>

--- test_regress/t/t_constraint_compile_error.v
+++ test_regress/t/t_constraint_compile_error.v
@@ -1,33 +1,19 @@
-package riscv_instr_pkg;
-    typedef struct {
-      int unsigned   size_in_bytes;  
-    } mem_region_t;
+typedef struct {
+  int unsigned   size_in_bytes;
+} mem_region_t;
 
-    class riscv_mem_access_stream;
-      int             max_data_page_id;
-      mem_region_t    data_page[];
-    endclass
-
-    class riscv_load_store_base_instr_stream extends riscv_mem_access_stream;
-      rand int unsigned  data_page_id;
-      rand int   rs1_reg;
-      rand int           max_load_store_offset;
-      constraint addr_c {
-        foreach (data_page[i]) {
-          if (i == data_page_id) {
-            max_load_store_offset == data_page[i].size_in_bytes;
-          }
-        }
-      }
-    endclass
-
-    class riscv_multi_page_load_store_instr_stream;
-      riscv_load_store_base_instr_stream load_store_instr_stream;
-      function void post_randomize();
-        void'(load_store_instr_stream.randomize());
-      endfunction
-    endclass
-endpackage
+class riscv_load_store_base_instr_stream;
+  mem_region_t    data_page;
+  rand int           max_load_store_offset;
+  constraint addr_c {
+      max_load_store_offset == data_page.size_in_bytes;
+  }
+endclass
 
 module t;
+    riscv_load_store_base_instr_stream x;
+    initial begin
+      x = new;
+      void'(x.randomize());
+    end
 endmodule
