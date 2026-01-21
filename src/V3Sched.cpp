2c2ec6597ae8a5fb1c4c0a898d3a509074e408e1
Igor Zaworski <izaworski@internships.antmicro.com>
2026-01-21 08:43:21 +0100

    V3Sched cleaup
    Signed-off-by: Igor Zaworski <izaworski@internships.antmicro.com>

--- src/V3Sched.cpp
+++ src/V3Sched.cpp
@@ -93,20 +93,20 @@ findTriggeredIface(const AstVarScope* vscp, const VirtIfaceTriggers::IfaceSensMa
         // If `vscp->varp()->isVirtIface()` is true then the interface type that viface is pointing
         // to is under `VN_AS(vscp->varp()->dtypep(), IfaceRefDType)->ifacep()`
 
+        ifacep = VN_AS(vscp->varp()->dtypep(), IfaceRefDType)->ifacep();
+
         // Virtual interface is sensitive to a different interface type than it is a virtual type
         // of - this may be a valid behaviour but this function does not expects that
-        UASSERT_OBJ(
-            vscp->varp()->sensIfacep() == nullptr, vscp,
-            "Virtual interface has an ambiguous type - "
-                << vscp->varp()->sensIfacep()->prettyTypeName() << " != "
-                << VN_AS(vscp->varp()->dtypep(), IfaceRefDType)->ifacep()->prettyTypeName());
-        ifacep = VN_AS(vscp->varp()->dtypep(), IfaceRefDType)->ifacep();
+        UASSERT_OBJ(vscp->varp()->sensIfacep() == nullptr, vscp,
+                    "Virtual interface has an ambiguous type - "
+                        << vscp->varp()->sensIfacep()->prettyTypeName()
+                        << " != " << ifacep->prettyTypeName());
     } else {
         // If `vscp->varp()` is of a non-virtual interface type it has `sensIfacep()` set to
-        // interface it is sensible to.
-        UASSERT_OBJ(vscp->varp()->sensIfacep(), vscp, "Not an interface trigger");
+        // interface it is sensitive to
         ifacep = vscp->varp()->sensIfacep();
     }
+    UASSERT_OBJ(ifacep, vscp, "Variable is not sensitive for any interface");
     const auto ifaceIt = vifTrigged.find(ifacep);
     std::vector<AstSenTree*> result;
     if (ifaceIt != vifTrigged.end()) result.push_back(ifaceIt->second);
