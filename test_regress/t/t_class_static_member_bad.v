8d702ec11194df1b7c8f84b0d88e6869953a087f
Bartłomiej Chmiel <bchmiel@antmicro.com>
2026-01-23 13:56:40 +0100

    [#90182] Minimize test
    Signed-off-by: Bartłomiej Chmiel <bchmiel@antmicro.com>

--- test_regress/t/t_class_static_member_bad.v
+++ test_regress/t/t_class_static_member_bad.v
@@ -4,13 +4,9 @@
 // any use, without warranty, 2026 by Antmicro.
 // SPDX-License-Identifier: CC0-1.0
 
-class PhaseNode;
-   int m_successors[int];
-   static mailbox #(PhaseNode) m_phase_hopper = new();
-   static task run_phases();
-      int succ;
-      void'(m_phase_hopper.try_put(this));
-      foreach (m_successors[succ])
-         void'(m_phase_hopper.try_put(null));
+class Cls;
+   mailbox #(Cls) mb = new();
+   static task foo();
+      void'(mb.try_put(this));
    endtask
 endclass
