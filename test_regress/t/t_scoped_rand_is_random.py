3a51dec53546475241035fec77a0386cd614280c
Rodrigo Batista de Moraes <rodrigobatsmoraes@hotmail.com>
2026-01-25 13:23:43 -0300

    Tests: Add test for checking random initialization

---
+++ test_regress/t/t_scoped_rand_is_random.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# DESCRIPTION: Verilator: Verilog Test driver/expect definition
+#
+# Copyright 2025 by Wilson Snyder. This program is free software; you
+# can redistribute it and/or modify it under the terms of either the GNU
+# Lesser General Public License Version 3 or the Perl Artistic License
+# Version 2.0.
+# SPDX-License-Identifier: LGPL-3.0-only OR Artistic-2.0
+
+import vltest_bootstrap
+
+test.scenarios("simulator_st")
+
+test.compile(timing_loop=True, verilator_flags2=["--timing"])
+
+filename = test.obj_dir + "/bits.log"
+
+test.unlink_ok(filename)
+
+for seed in range(1, 6):
+    test.execute(all_run_flags=["+verilator+rand+reset+2", f"+verilator+seed+{seed}"])
+
+unique_strings = set()
+
+with open(filename, "r", encoding="utf-8") as file:
+    for line in file:
+        unique_strings.update(line.split())
+
+count_unique_strings = len(unique_strings)
+
+# A ideal random generator has a fail chance of 1 in 66 million
+expected_unique_strings = 4
+if count_unique_strings < expected_unique_strings:
+    test.error(
+        f"Expected at least {expected_unique_strings} unique strings, got {count_unique_strings}")
+
+test.passes()
