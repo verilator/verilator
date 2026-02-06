f7807e274f8a865b201d100ab7bca7b46fe23736
Pawel Kojma <pkojma@internships.antmicro.com>
2026-02-06 15:31:50 +0100

    Update outputs of tests
    Signed-off-by: Pawel Kojma <pkojma@internships.antmicro.com>

--- test_regress/t/t_90958_unpacked_array_error.v
+++ test_regress/t/t_90958_unpacked_array_error.v
@@ -1,3 +1,9 @@
+// DESCRIPTION: Verilator: Verilog Test module for SystemVerilog
+//
+// This file ONLY is placed under the Creative Commons Public Domain
+// SPDX-FileCopyrightText: 2026 Antmicro
+// SPDX-License-Identifier: CC0-1.0
+
 module test2 (
     input logic b
 );
