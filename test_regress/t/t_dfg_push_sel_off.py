c49198e1275b3983fc49a04dcff8eb2abeba6bbe
github action <action@example.com>
2026-02-07 17:03:52 +0000

    Apply 'make format'

--- test_regress/t/t_dfg_push_sel_off.py
+++ test_regress/t/t_dfg_push_sel_off.py
@@ -14,7 +14,8 @@ test.scenarios('simulator_st')
 test.top_filename = "t/t_dfg_push_sel.v"
 
 test.compile(verilator_flags2=[
-    "--binary", "--stats", "-fno-dfg-pre-inline", "-fno-dfg-post-inline", "-fno-dfg-peephole", "-fno-dfg-push-down-sels"
+    "--binary", "--stats", "-fno-dfg-pre-inline", "-fno-dfg-post-inline", "-fno-dfg-peephole",
+    "-fno-dfg-push-down-sels"
 ])
 
 test.execute()
