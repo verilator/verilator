cc3fdb75f9ce022388a7e367af62b3297e2c53f8
Ryszard Rozak <rrozak@antmicro.com>
2026-02-10 15:03:13 +0100

    Replace real with string
    Signed-off-by: Ryszard Rozak <rrozak@antmicro.com>

--- test_regress/t/t_force_unpacked_unsup.v
+++ test_regress/t/t_force_unpacked_unsup.v
@@ -7,7 +7,7 @@
 // verilog_format: off
 `define stop $stop
 `define checkh(gotv,expv) do if ((gotv) !== (expv)) begin $write("%%Error: %s:%0d:  got='h%x exp='h%x\n", `__FILE__,`__LINE__, (gotv), (expv)); `stop; end while(0)
-`define checkr(gotv,expv) do if ((gotv) != (expv)) begin $write("%%Error: %s:%0d:  got=%f exp=%f\n", `__FILE__,`__LINE__, (gotv), (expv)); `stop; end while(0);
+`define checks(gotv,expv) do if ((gotv) != (expv)) begin $write("%%Error: %s:%0d:  got=%s exp=%s\n", `__FILE__,`__LINE__, (gotv), (expv)); `stop; end while(0);
 // verilog_format: on
 
 module t (
@@ -20,34 +20,34 @@ module t (
 
   struct_t s_array[3000];
   bit big_array[40][40][40];
-  real r_array[2];
+  string string_array[2];
   struct_t my_struct;
 
   // Test loop
   always @(posedge clk) begin
     cyc <= cyc + 1;
     if (cyc == 0) begin
-      r_array[0] <= 1;
+      string_array[0] = "abcd";
       big_array[1][2][3] <= 1;
       s_array[1].x <= 1;
       my_struct.x <= 1;
     end
     else if (cyc == 1) begin
-      `checkr(r_array[0], 1);
-      `checkr(big_array[1][2][3], 1);
+      `checks(string_array[0], "abcd");
+      `checkh(big_array[1][2][3], 1);
       `checkh(s_array[1].x, 1);
       `checkh(my_struct.x, 1);
     end
     else if (cyc == 2) begin
-      force r_array[0] = 0;
+      force string_array[0] = "efgh";
       force big_array[1][2][3] = 0;
       force s_array[1].x = 0;
       force my_struct.x = 0;
     end
     else if (cyc == 3) begin
-      `checkr(r_array[0], 0);
-      `checkr(big_array[1][2][3], 0);
-      r_array[0] <= 1;
+      `checks(string_array[0], "efgh");
+      `checkh(big_array[1][2][3], 0);
+      string_array[0] = "ijkl";
       big_array[1][2][3] <= 1;
       `checkh(s_array[1].x, 0);
       s_array[1].x <= 1;
@@ -55,21 +55,21 @@ module t (
       my_struct.x <= 1;
     end
     else if (cyc == 4) begin
-      `checkr(r_array[0], 0);
-      `checkr(big_array[1][2][3], 0);
+      `checks(string_array[0], "efgh");
+      `checkh(big_array[1][2][3], 0);
       `checkh(s_array[1].x, 0);
       `checkh(my_struct.x, 0);
     end
     else if (cyc == 5) begin
-      release r_array[0];
+      release string_array[0];
       release big_array[1][2][3];
       release s_array[1].x;
       release my_struct.x;
     end
     else if (cyc == 6) begin
-      `checkr(r_array[0], 0);
-      `checkr(big_array[1][2][3], 0);
-      r_array[0] <= 1;
+      `checks(string_array[0], "efgh");
+      `checkh(big_array[1][2][3], 0);
+      string_array[0] = "mnop";
       big_array[1][2][3] <= 1;
       `checkh(s_array[1].x, 0);
       s_array[1].x <= 1;
@@ -77,8 +77,8 @@ module t (
       my_struct.x <= 1;
     end
     else if (cyc == 7) begin
-      `checkr(r_array[0], 1);
-      `checkr(big_array[1][2][3], 1);
+      `checks(string_array[0], "mnop");
+      `checkh(big_array[1][2][3], 1);
       `checkh(s_array[1].x, 1);
       `checkh(my_struct.x, 1);
     end
