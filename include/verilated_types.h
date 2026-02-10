5455d294dc5e73b8c42185b83d81e130b9ab0401
Igor Zaworski <izaworski@internships.antmicro.com>
2026-02-10 10:18:15 +0100

    Review changes
    Signed-off-by: Igor Zaworski <izaworski@internships.antmicro.com>

--- include/verilated_types.h
+++ include/verilated_types.h
@@ -1928,7 +1928,7 @@ public:
     VlClassRef() = default;
     // Init with nullptr
     // cppcheck-suppress noExplicitConstructor
-    VlClassRef(VlNull) {};
+    VlClassRef(VlNull){};
     template <typename... T_Args>
     VlClassRef(VlDeleter& deleter, T_Args&&... args)
         : m_objp{new T_Class} {
@@ -1948,8 +1948,8 @@ public:
     }
     VlClassRef(VlDeleter& deleter, T_Class& args)
         // Copy constructor - this is required since if `T_Class&`
-        // will be provided a compilator will match it to the constructor
-        // with variading template instead of `T_Class&&`
+        // will be provided a compiler will match it to the constructor
+        // with variadic template instead of `T_Class&&`
         : m_objp{new T_Class{args}} {
         m_objp->m_deleterp = &deleter;
     }
