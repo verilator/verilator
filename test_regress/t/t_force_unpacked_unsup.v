16fc6e339248d75220cffdd4338a48d7c75857e9
Ryszard Rozak <rrozak@antmicro.com>
2026-02-13 10:17:20 +0100

    Change copyright format
    Signed-off-by: Ryszard Rozak <rrozak@antmicro.com>

--- test_regress/t/t_force_unpacked_unsup.v
+++ test_regress/t/t_force_unpacked_unsup.v
@@ -1,7 +1,7 @@
 // DESCRIPTION: Verilator: Verilog Test module
 //
 // This file ONLY is placed under the Creative Commons Public Domain
-// SPDX-FileCopyrightText: 2025 Antmicro
+// SPDX-FileCopyrightText: 2026 Antmicro
 // SPDX-License-Identifier: CC0-1.0
 
 // verilog_format: off
