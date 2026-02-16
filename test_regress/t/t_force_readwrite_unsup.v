d67e65e21a47aac204773f6c43784de971dd9c4a
Ryszard Rozak <rrozak@antmicro.com>
2026-02-16 09:23:16 +0100

    Fix license
    Signed-off-by: Ryszard Rozak <rrozak@antmicro.com>

--- test_regress/t/t_force_readwrite_unsup.v
+++ test_regress/t/t_force_readwrite_unsup.v
@@ -4,12 +4,6 @@
 // SPDX-FileCopyrightText: 2025 Antmicro
 // SPDX-License-Identifier: CC0-1.0
 
-// DESCRIPTION: Verilator: Verilog Test module
-//
-// This file ONLY is placed under the Creative Commons Public Domain
-// SPDX-FileCopyrightText: 2025 Antmicro
-// SPDX-License-Identifier: CC0-1.0
-
 class Cls;
   task take_ref(ref logic s);
   endtask
