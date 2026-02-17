9a9fc0d057cf4bda6f89703479fcbb8c0e569baf
Yilou Wang <wangyilou123@gmail.com>
2026-02-17 01:10:07 +0100

    update test

--- test_regress/t/t_randc_enum_constraint.v
+++ test_regress/t/t_randc_enum_constraint.v
@@ -37,30 +37,43 @@ module t;
     ColorClass c;
     AllColorsClass ac;
     int color_seen[5];
+    int first_seq, all_same, this_seq;
 
     // Test 1: randc enum with exclusion constraint
     // Values must be valid enum members (0-4) and not BLACK (4)
     c = new;
     repeat (40) begin
       `checkd(c.randomize(), 1);
-      // Must be a valid enum member (not 5, 6, 7)
       `checkd(c.color <= BLACK, 1);
-      // Must not be BLACK (excluded by constraint)
       `checkd(c.color == BLACK, 0);
     end
 
-    // Test 2: randc enum with range constraint - verify all valid values seen
-    ac = new;
-    repeat (40) begin
-      `checkd(ac.randomize(), 1);
-      `checkd(ac.color <= WHITE, 1);
-      color_seen[ac.color] = 1;
+    // Test 2: randc enum with range constraint - run multiple trials,
+    // verify only valid enum members produced and sequence varies
+    all_same = 1;
+    first_seq = 0;
+    for (int trial = 0; trial < 3; trial++) begin
+      ac = new;
+      this_seq = 0;
+      for (int j = 0; j < 4; j++) color_seen[j] = 0;
+      repeat (40) begin
+        `checkd(ac.randomize(), 1);
+        `checkd(ac.color <= WHITE, 1);
+        color_seen[ac.color] = 1;
+      end
+      // Record first 4 values for sequence comparison
+      repeat (4) begin
+        `checkd(ac.randomize(), 1);
+        this_seq = this_seq * 8 + int'(ac.color);
+      end
+      `checkd(color_seen[0], 1);
+      `checkd(color_seen[1], 1);
+      `checkd(color_seen[2], 1);
+      `checkd(color_seen[3], 1);
+      if (trial == 0) first_seq = this_seq;
+      else if (this_seq != first_seq) all_same = 0;
     end
-    // After 40 iterations (10 full cycles of 4), all values should appear
-    `checkd(color_seen[0], 1);
-    `checkd(color_seen[1], 1);
-    `checkd(color_seen[2], 1);
-    `checkd(color_seen[3], 1);
+    `checkd(all_same, 0);
 
     $write("*-* All Finished *-*\n");
     $finish;
