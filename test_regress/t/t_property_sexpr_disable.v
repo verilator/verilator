33d7bcc01c8b754eac780875b4fd237e9b2060e5
Ryszard Rozak <rrozak@antmicro.com>
2026-02-18 13:27:05 +0100

    Fix tests
    Signed-off-by: Ryszard Rozak <rrozak@antmicro.com>

--- test_regress/t/t_property_sexpr_disable.v
+++ test_regress/t/t_property_sexpr_disable.v
@@ -1,7 +1,7 @@
 // DESCRIPTION: Verilator: Verilog Test module
 //
-// This file ONLY is placed into the Public Domain, for any use,
-// without warranty, 2025 by Antmicro.
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2026 Antmicro
 // SPDX-License-Identifier: CC0-1.0
 
 `define stop $stop
@@ -26,29 +26,24 @@ module t (  /*AUTOARG*/
   localparam MAX = 15;
   integer cyc = 0;
 
-  assert property (@(posedge clk) disable iff (cyc == 4 || cyc > MAX) 1 ##1 cyc < 10)
+  assert property (@(posedge clk) disable iff (cyc == 5 || cyc > MAX) 1 ##1 cyc < 10)
     results[1].passs++;
   else results[1].fails++;
 
-  assert property (@(posedge clk) disable iff (cyc == 5 || cyc > MAX) 1 ##1 cyc < 10)
+  assert property (@(posedge clk) disable iff (1) 1 ##1 0)
     results[2].passs++;
   else results[2].fails++;
 
-  assert property (@(posedge clk) disable iff (1) 1 ##1 0)
+  assert property (@(posedge clk) disable iff (0) 1 ##1 0)
     results[3].passs++;
   else results[3].fails++;
-
-  assert property (@(posedge clk) disable iff (0) 1 ##1 0)
-    results[4].passs++;
-  else results[4].fails++;
 
   always @(clk) begin
     ++cyc;
     if (cyc == MAX) begin
-       expected[1] = '{2, 2};
-       expected[2] = '{2, 3};
-       // expected[3] shouldn't be initialized
-       expected[4] = '{6, 0};
+       expected[1] = '{2, 3};
+       // expected[2] shouldn't be initialized
+       expected[3] = '{6, 0};
       `checkh(results, expected);
       $write("*-* All Finished *-*\n");
       $finish;
