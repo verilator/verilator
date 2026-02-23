596714c9bb223e57ce6cda5259a530ec3a33463b
Nick Brereton <nbrereton@striketechnologies.com>
2026-02-23 13:57:32 -0500

    Add header to t_coroutine_lambda.v

--- test_regress/t/t_coroutine_lambda.v
+++ test_regress/t/t_coroutine_lambda.v
@@ -1,3 +1,10 @@
+// DESCRIPTION: Verilator: Coroutine call inside AstExprStmt lambda test module
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of either the GNU Lesser General Public License Version 3
+// or the Perl Artistic License Version 2.0.
+// SPDX-FileCopyrightText: 2026 Wilson Snyder
+// SPDX-License-Identifier: LGPL-3.0-only OR Artistic-2.0
 interface cpu_if(input logic clk);
 endinterface
 
