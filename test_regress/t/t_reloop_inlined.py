39af55675d5f56aaf0c117a3579f76ab3926fc3c
Geza Lore <glore@tenstorrent.com>
2026-02-23 13:35:10 +0000

    Fix inlining of CFuncs with reloop locals
    The recent V3InlineCFuncs only checks AstCFunc::varsp for locals, but
    V3Reloop used to insert them into AstCFunc::stmtsp resulting in multiple
    locals with the same name being inlined into the caller if the stars
    align. Fix Reloop. Such things will also go away with #6280.

---
+++ test_regress/t/t_reloop_inlined.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# DESCRIPTION: Verilator: Verilog Test driver/expect definition
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of either the GNU Lesser General Public License Version 3
+# or the Perl Artistic License Version 2.0.
+# SPDX-FileCopyrightText: 2026 Wilson Snyder
+# SPDX-License-Identifier: LGPL-3.0-only OR Artistic-2.0
+
+import vltest_bootstrap
+
+test.scenarios('vlt')
+
+test.compile(verilator_flags2=[
+    "--binary", "--unroll-count", "1024", "--inline-cfuncs", "100000000", "--output-split-cfuncs",
+    "50", "--reloop-limit", "2"
+])
+
+test.execute()
+
+test.passes()
