625a33c15589100978d6e3f9dbe1c10e920a5418
Ryszard Rozak <rrozak@antmicro.com>
2026-02-24 13:07:18 +0100

    Add test with unpacked union
    Signed-off-by: Ryszard Rozak <rrozak@antmicro.com>

--- test_regress/t/t_force_unpacked_unsup.v
+++ test_regress/t/t_force_unpacked_unsup.v
@@ -17,9 +17,14 @@ module t (
   integer cyc = 0;
 
   typedef struct {int x;} struct_t;
+  typedef union {
+    int   x;
+    logic y;
+  } union_t;
 
   struct_t s_array[3000];
   bit big_array[40][40][40];
+  union_t my_union;
 
   // Test loop
   always @(posedge clk) begin
@@ -27,40 +32,42 @@ module t (
     if (cyc == 0) begin
       big_array[1][2][3] <= 1;
       s_array[1].x <= 1;
-    end
-    else if (cyc == 1) begin
+      my_union.x <= 1;
+    end else if (cyc == 1) begin
       `checkr(big_array[1][2][3], 1);
       `checkh(s_array[1].x, 1);
-    end
-    else if (cyc == 2) begin
+      `checkh(my_union.x, 1);
+    end else if (cyc == 2) begin
       force big_array[1][2][3] = 0;
       force s_array[1].x = 0;
-    end
-    else if (cyc == 3) begin
+      force my_union.x = 0;
+    end else if (cyc == 3) begin
       `checkr(big_array[1][2][3], 0);
       big_array[1][2][3] <= 1;
       `checkh(s_array[1].x, 0);
       s_array[1].x <= 1;
-    end
-    else if (cyc == 4) begin
+      `checkh(my_union.x, 0);
+      my_union.x <= 1;
+    end else if (cyc == 4) begin
       `checkr(big_array[1][2][3], 0);
       `checkh(s_array[1].x, 0);
-    end
-    else if (cyc == 5) begin
+      `checkh(my_union.x, 0);
+    end else if (cyc == 5) begin
       release big_array[1][2][3];
       release s_array[1].x;
-    end
-    else if (cyc == 6) begin
+      release my_union.x;
+    end else if (cyc == 6) begin
       `checkr(big_array[1][2][3], 0);
       big_array[1][2][3] <= 1;
       `checkh(s_array[1].x, 0);
       s_array[1].x <= 1;
-    end
-    else if (cyc == 7) begin
+      `checkh(my_union.x, 0);
+      my_union.x <= 1;
+    end else if (cyc == 7) begin
       `checkr(big_array[1][2][3], 1);
       `checkh(s_array[1].x, 1);
-    end
-    else if (cyc == 8) begin
+      `checkh(my_union.x, 1);
+    end else if (cyc == 8) begin
       $write("*-* All Finished *-*\n");
       $finish;
     end
