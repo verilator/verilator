89aaabb68e78900f4f8766bb8fb2a3a81340e720
em2machine <92717390+em2machine@users.noreply.github.com>
2026-03-01 16:46:24 +0100

    2 space indents for t_iface_typedef_struct_member

--- test_regress/t/t_iface_typedef_struct_member.v
+++ test_regress/t/t_iface_typedef_struct_member.v
@@ -18,10 +18,10 @@
 // verilog_format: on
 
 package cfg_pkg;
-    typedef struct packed {
-        int unsigned NumUnits;
-        int unsigned LineSize;
-    } cfg_t;
+  typedef struct packed {
+    int unsigned NumUnits;
+    int unsigned LineSize;
+  } cfg_t;
 endpackage
 
 // Parameterized interface with nested struct/union typedefs.
@@ -29,52 +29,52 @@ endpackage
 // The $bits() usage in sub_mod forces widthing during V3Param, moving
 // template RefDTypes into the type table before the template dies.
 interface types_if #(parameter cfg_pkg::cfg_t cfg = 0)();
-    typedef logic [$clog2(cfg.NumUnits)-1:0] idx_t;
+  typedef logic [$clog2(cfg.NumUnits)-1:0] idx_t;
 
-    typedef struct packed {
-        logic [31:$clog2(cfg.LineSize)] tag;
-        logic [$clog2(cfg.LineSize)-1:0] offset;
-    } addr_t;
+  typedef struct packed {
+    logic [31:$clog2(cfg.LineSize)] tag;
+    logic [$clog2(cfg.LineSize)-1:0] offset;
+  } addr_t;
 
-    typedef struct packed {
-        logic [3:0] cmd;
-        union packed {
-            addr_t       addr;   // struct-in-union: MemberDType trigger
-            logic [31:0] raw;
-        } payload;
-    } req_t;
+  typedef struct packed {
+    logic [3:0] cmd;
+    union packed {
+      addr_t       addr;   // struct-in-union: MemberDType trigger
+      logic [31:0] raw;
+    } payload;
+  } req_t;
 endinterface
 
 module sub_mod #(parameter cfg_pkg::cfg_t cfg = 0)();
-    types_if #(cfg) types();
-    typedef types.idx_t  idx_t;
-    typedef types.req_t  req_t;
-    typedef types.addr_t addr_t;
+  types_if #(cfg) types();
+  typedef types.idx_t  idx_t;
+  typedef types.req_t  req_t;
+  typedef types.addr_t addr_t;
 
-    localparam int ReqWidth = $bits(req_t);
+  localparam int ReqWidth = $bits(req_t);
 
-    idx_t  s_idx;
-    req_t  s_req;
-    addr_t s_addr;
+  idx_t  s_idx;
+  req_t  s_req;
+  addr_t s_addr;
 endmodule
 
 module t;
-    localparam cfg_pkg::cfg_t CFG = '{NumUnits: 5, LineSize: 32};
+  localparam cfg_pkg::cfg_t CFG = '{NumUnits: 5, LineSize: 32};
 
-    types_if #(CFG) types();
-    typedef types.req_t req_t;
-    req_t top_req;
+  types_if #(CFG) types();
+  typedef types.req_t req_t;
+  req_t top_req;
 
-    sub_mod #(.cfg(CFG)) sub();
+  sub_mod #(.cfg(CFG)) sub();
 
-    initial begin
-        #1;
-        `checkd($bits(top_req), 36);
-        `checkd($bits(sub.s_req), 36);
-        `checkd($bits(sub.s_idx), 3);
-        `checkd($bits(sub.s_addr), 32);
+  initial begin
+    #1;
+    `checkd($bits(top_req), 36);
+    `checkd($bits(sub.s_req), 36);
+    `checkd($bits(sub.s_idx), 3);
+    `checkd($bits(sub.s_addr), 32);
 
-        $write("*-* All Finished *-*\n");
-        $finish;
-    end
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
 endmodule
