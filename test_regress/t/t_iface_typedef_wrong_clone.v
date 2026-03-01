ad28b3594efc98c393b6bd536b3ef9ab6f4d4d5a
em2machine <92717390+em2machine@users.noreply.github.com>
2026-03-01 16:49:06 +0100

    two space indents for t_iface_typedef_wrong_clone

--- test_regress/t/t_iface_typedef_wrong_clone.v
+++ test_regress/t/t_iface_typedef_wrong_clone.v
@@ -21,65 +21,65 @@
 // verilog_format: on
 
 typedef struct packed {
-    int unsigned AddrBits;
-    int unsigned DataBits;
+  int unsigned AddrBits;
+  int unsigned DataBits;
 } sc_cfg_t;
 
 // Inner types interface - parameterized with struct typedef
 interface sc_types_if #(parameter sc_cfg_t cfg = 0)();
-    typedef logic [cfg.AddrBits-1:0] addr_t;
-    typedef logic [cfg.DataBits-1:0] data_t;
+  typedef logic [cfg.AddrBits-1:0] addr_t;
+  typedef logic [cfg.DataBits-1:0] data_t;
 
-    typedef struct packed {
-        addr_t addr;
-        data_t data;
-    } pkt_t;
+  typedef struct packed {
+    addr_t addr;
+    data_t data;
+  } pkt_t;
 endinterface
 
 // Cache interface - wraps types interface and re-exports typedefs
 interface sc_if #(parameter sc_cfg_t cfg = 0)();
-    sc_types_if #(cfg) sc_io();
+  sc_types_if #(cfg) sc_io();
 
-    typedef sc_io.addr_t addr_t;
-    typedef sc_io.data_t data_t;
-    typedef sc_io.pkt_t  pkt_t;
+  typedef sc_io.addr_t addr_t;
+  typedef sc_io.data_t data_t;
+  typedef sc_io.pkt_t  pkt_t;
 
-    addr_t rq_addr_i;
+  addr_t rq_addr_i;
 endinterface
 
 // Wrapper module that uses the cache interface
 module sc_wrap #(parameter sc_cfg_t cfg = 0)();
-    sc_if #(cfg) cache();
+  sc_if #(cfg) cache();
 
-    typedef cache.addr_t addr_t;
-    typedef cache.pkt_t  pkt_t;
+  typedef cache.addr_t addr_t;
+  typedef cache.pkt_t  pkt_t;
 
-    addr_t local_addr;
-    pkt_t  local_pkt;
+  addr_t local_addr;
+  pkt_t  local_pkt;
 
-    assign cache.rq_addr_i = local_addr;
+  assign cache.rq_addr_i = local_addr;
 endmodule
 
 // Top-level: two wrappers with DIFFERENT configs
 // This creates two clones of sc_if (and sc_types_if) with different params
 module t;
-    localparam sc_cfg_t cfg_narrow = '{AddrBits: 16, DataBits: 32};
-    localparam sc_cfg_t cfg_wide   = '{AddrBits: 32, DataBits: 64};
+  localparam sc_cfg_t cfg_narrow = '{AddrBits: 16, DataBits: 32};
+  localparam sc_cfg_t cfg_wide   = '{AddrBits: 32, DataBits: 64};
 
-    sc_wrap #(.cfg(cfg_narrow)) narrow();
-    sc_wrap #(.cfg(cfg_wide))   wide();
+  sc_wrap #(.cfg(cfg_narrow)) narrow();
+  sc_wrap #(.cfg(cfg_wide))   wide();
 
-    initial begin
-        #1;
-        // narrow: addr=16, data=32, pkt=16+32=48
-        `checkd($bits(narrow.local_addr), 16);
-        `checkd($bits(narrow.local_pkt), 48);
+  initial begin
+    #1;
+    // narrow: addr=16, data=32, pkt=16+32=48
+    `checkd($bits(narrow.local_addr), 16);
+    `checkd($bits(narrow.local_pkt), 48);
 
-        // wide: addr=32, data=64, pkt=32+64=96
-        `checkd($bits(wide.local_addr), 32);
-        `checkd($bits(wide.local_pkt), 96);
+    // wide: addr=32, data=64, pkt=32+64=96
+    `checkd($bits(wide.local_addr), 32);
+    `checkd($bits(wide.local_pkt), 96);
 
-        $write("*-* All Finished *-*\n");
-        $finish;
-    end
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
 endmodule
