d84f6f2d0bd5c7b4d5c43af96510175acb724533
Matthew Ballance <matt.ballance@gmail.com>
2026-03-04 15:25:41 +0000

    Correct indent on coverage tests ; add required test drivers ; back out CMakeLists pch change and adjust test that required it to no longer need it
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- test_regress/t/t_covergroup_dynamic.v
+++ test_regress/t/t_covergroup_dynamic.v
@@ -8,86 +8,86 @@
 
 module t;
 
-   covergroup cg;
-      coverpoint data {
-         bins low  = {[0:1]};
-         bins high = {[2:3]};
-      }
-   endgroup
+  covergroup cg;
+    coverpoint data {
+      bins low  = {[0:1]};
+      bins high = {[2:3]};
+    }
+  endgroup
 
-   int data;
+  int data;
 
-   initial begin
-      cg cg_inst;
-      real cov;
+  initial begin
+    cg cg_inst;
+    real cov;
 
-      // Test 1: Create single dynamic instance
-      $display("Test 1: Single dynamic instance");
-      cg_inst = new;
+    // Test 1: Create single dynamic instance
+    $display("Test 1: Single dynamic instance");
+    cg_inst = new;
 
-      // Initially no coverage
-      cov = cg_inst.get_inst_coverage();
-      $display("  Initial coverage: %f", cov);
-      if (cov != 0.0) $stop;
+    // Initially no coverage
+    cov = cg_inst.get_inst_coverage();
+    $display("  Initial coverage: %f", cov);
+    if (cov != 0.0) $stop;
 
-      // Sample low bin
+    // Sample low bin
+    data = 0;
+    cg_inst.sample();
+    cov = cg_inst.get_inst_coverage();
+    $display("  After sampling low: %f", cov);
+    if (cov < 49.0 || cov > 51.0) $stop;  // ~50%
+
+    // Sample high bin
+    data = 2;
+    cg_inst.sample();
+    cov = cg_inst.get_inst_coverage();
+    $display("  After sampling high: %f", cov);
+    if (cov < 99.0 || cov > 101.0) $stop;  // ~100%
+
+    // Test 2: Multiple dynamic instances
+    $display("Test 2: Multiple dynamic instances");
+    begin
+      cg cg1, cg2, cg3;
+
+      cg1 = new;
+      cg2 = new;
+      cg3 = new;
+
+      // Sample different bins in each
       data = 0;
-      cg_inst.sample();
-      cov = cg_inst.get_inst_coverage();
-      $display("  After sampling low: %f", cov);
-      if (cov < 49.0 || cov > 51.0) $stop;  // ~50%
+      cg1.sample();
 
-      // Sample high bin
       data = 2;
-      cg_inst.sample();
-      cov = cg_inst.get_inst_coverage();
-      $display("  After sampling high: %f", cov);
-      if (cov < 99.0 || cov > 101.0) $stop;  // ~100%
+      cg2.sample();
 
-      // Test 2: Multiple dynamic instances
-      $display("Test 2: Multiple dynamic instances");
-      begin
-         cg cg1, cg2, cg3;
+      data = 1;
+      cg3.sample();
 
-         cg1 = new;
-         cg2 = new;
-         cg3 = new;
+      // Check individual coverage
+      cov = cg1.get_inst_coverage();
+      $display("  cg1 coverage: %f", cov);
+      if (cov < 49.0 || cov > 51.0) $stop;  // 50%
 
-         // Sample different bins in each
-         data = 0;
-         cg1.sample();
+      cov = cg2.get_inst_coverage();
+      $display("  cg2 coverage: %f", cov);
+      if (cov < 49.0 || cov > 51.0) $stop;  // 50%
 
-         data = 2;
-         cg2.sample();
+      cov = cg3.get_inst_coverage();
+      $display("  cg3 coverage: %f", cov);
+      if (cov < 49.0 || cov > 51.0) $stop;  // 50%
+    end
 
-         data = 1;
-         cg3.sample();
+    // Test 3: Reassignment (old instance should be cleaned up)
+    $display("Test 3: Instance reassignment");
+    cg_inst = new;  // Create new, old should be freed
 
-         // Check individual coverage
-         cov = cg1.get_inst_coverage();
-         $display("  cg1 coverage: %f", cov);
-         if (cov < 49.0 || cov > 51.0) $stop;  // 50%
+    // New instance starts with 0% coverage
+    cov = cg_inst.get_inst_coverage();
+    $display("  New instance coverage: %f", cov);
+    if (cov != 0.0) $stop;
 
-         cov = cg2.get_inst_coverage();
-         $display("  cg2 coverage: %f", cov);
-         if (cov < 49.0 || cov > 51.0) $stop;  // 50%
-
-         cov = cg3.get_inst_coverage();
-         $display("  cg3 coverage: %f", cov);
-         if (cov < 49.0 || cov > 51.0) $stop;  // 50%
-      end
-
-      // Test 3: Reassignment (old instance should be cleaned up)
-      $display("Test 3: Instance reassignment");
-      cg_inst = new;  // Create new, old should be freed
-
-      // New instance starts with 0% coverage
-      cov = cg_inst.get_inst_coverage();
-      $display("  New instance coverage: %f", cov);
-      if (cov != 0.0) $stop;
-
-      $write("*-* All Finished *-*\n");
-      $finish;
-   end
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
 
 endmodule
