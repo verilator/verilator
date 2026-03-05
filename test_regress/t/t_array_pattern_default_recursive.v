93c929a85ddc3d702f04a002313e5f106a657e43
mo0nbase <gpggit.difficult148@passmail.net>
2026-03-05 15:20:28 -0500

    fix SPDX copyright in test headers

--- test_regress/t/t_array_pattern_default_recursive.v
+++ test_regress/t/t_array_pattern_default_recursive.v
@@ -1,7 +1,7 @@
 // DESCRIPTION: Verilator: Verilog Test module
 //
 // This file ONLY is placed under the Creative Commons Public Domain.
-// SPDX-FileCopyrightText: 2026 by Verilator Authors
+// SPDX-FileCopyrightText: 2026 Wilson Snyder
 // SPDX-License-Identifier: CC0-1.0
 
 module t;
