5966711007a6a8dc61cb8978355fbf1095dda5c7
mo0nbase <gpggit.difficult148@passmail.net>
2026-03-05 13:14:54 -0500

    add regression for different defaults in different subarrays

--- test_regress/t/t_array_pattern_default_recursive.v
+++ test_regress/t/t_array_pattern_default_recursive.v
@@ -9,6 +9,7 @@ module t;
   int arr_default_scalar[4][4];
   int row[4];
   int arr_default_array[2][4];
+  int arr_mixed_default[2][3];
 
   initial begin
     arr_default_scalar = '{default: 0};
@@ -21,6 +22,14 @@ module t;
     foreach (arr_default_array[i, j]) begin
       if (arr_default_array[i][j] != row[j]) $stop;
     end
+
+    arr_mixed_default = '{0: '{0: 1, default: 3}, default: '{default: 2}};
+    if (arr_mixed_default[0][0] != 1) $stop;
+    if (arr_mixed_default[0][1] != 3) $stop;
+    if (arr_mixed_default[0][2] != 3) $stop;
+    if (arr_mixed_default[1][0] != 2) $stop;
+    if (arr_mixed_default[1][1] != 2) $stop;
+    if (arr_mixed_default[1][2] != 2) $stop;
 
     $write("*-* All Finished *-*\n");
     $finish;
