f52817bc928e740804cd2e4d8690700b0194276f
github action <action@example.com>
2026-03-07 03:00:29 +0000

    Apply 'make format'

--- src/V3EmitV.cpp
+++ src/V3EmitV.cpp
@@ -1020,7 +1020,8 @@ class EmitVBaseVisitorConst VL_NOT_FINAL : public VNVisitorConst {
         }
     }
     void visit(AstClassRefDType* nodep) override {
-        putfs(nodep, nodep->classp() ? EmitCUtil::prefixNameProtect(nodep->classp()) : nodep->prettyDTypeName(false));
+        putfs(nodep, nodep->classp() ? EmitCUtil::prefixNameProtect(nodep->classp())
+                                     : nodep->prettyDTypeName(false));
     }
     void visit(AstRequireDType* nodep) override { iterateConst(nodep->lhsp()); }
     void visit(AstModport* nodep) override {
