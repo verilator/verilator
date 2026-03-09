68b9b1948500c53194e003857bf4ced0858e88c1
em2machine <92717390+em2machine@users.noreply.github.com>
2026-03-09 14:41:42 +0100

    anonymize test a little bit and fix commas

--- test_regress/t/t_iface_nested_width2.v
+++ test_regress/t/t_iface_nested_width2.v
@@ -17,7 +17,7 @@ package utils;
   endfunction
 endpackage
 
-package aerial;
+package soc;
   // Configuration structure
   typedef struct packed {
     // CB Configuration
@@ -25,14 +25,14 @@ package aerial;
     int unsigned CBPids;
     int unsigned CBFnum;
     // Coarse parameters
-    int unsigned NumAitile;
-    int unsigned NumCca;
+    int unsigned Tile;
+    int unsigned NumBb;
     // CCA Parameters
-    int unsigned NumCc;
+    int unsigned NumDd;
     // CC Parameters
-    int unsigned CCNumWaves;
-    int unsigned CCNumWaveThreads;
-    int unsigned CCNumIds;  // check this one
+    int unsigned DDNumW;
+    int unsigned DDNumWT;
+    int unsigned DDIds;  // check this one
   } cfg_t;
 endpackage
 
@@ -45,7 +45,7 @@ package cb;
   } cfg_t;
 endpackage
 
-package cca;
+package bba;
    typedef logic [11:0] csr_index_t;
    typedef logic [1:0] cmd_bnum_t;
    typedef logic [3:0] rq_cmd_t;
@@ -54,25 +54,25 @@ package cca;
       rq_cmd_t rq;
       rs_cmd_t rs;
    } cmd_t;
-endpackage: cca
+endpackage: bba
 
-interface aicc_types_if #(parameter aerial::cfg_t cfg=0)();
+interface bb_types_if #(parameter soc::cfg_t cfg=0)();
   //
-  typedef logic [$clog2(cfg.NumCc)-1:0] cc_index_t;
-  typedef logic [$clog2(cfg.CCNumIds)-1:0] trans_id_t;
+  typedef logic [$clog2(cfg.NumDd)-1:0] bb_index_t;
+  typedef logic [$clog2(cfg.DDIds)-1:0] trans_id_t;
 
   typedef struct packed {
     logic hdr_vld;
     logic [1:0] bnum;
     logic is_rq;
-    cca::cmd_t cmd;
-    cc_index_t cc_index;
+    bba::cmd_t cmd;
+    bb_index_t cc_index;
     trans_id_t trans_id;
   } cmd_meta_t;
 
   typedef union packed {
-    logic [cfg.CCNumWaveThreads*64-1:0] raw;
-    logic [cfg.CCNumWaveThreads-1:0][63:0] raw_a;
+    logic [cfg.DDNumWT*64-1:0] raw;
+    logic [cfg.DDNumWT-1:0][63:0] raw_a;
   } cmd_data_t;
 
   typedef struct packed {
@@ -99,17 +99,17 @@ interface cb_port_vc_if #(parameter cb::cfg_t cfg=0)();
   tx_beat_t tx_beat;
 endinterface
 
-module simple_macro_mem #(
+module simple_mem #(
   parameter int p_num_buffered_rq = 16,
   parameter int p_storage_size = 65536
  )(
-  aicc_types_if aicc_types,
+  bb_types_if bb_types,
   // CB Interface
   cb_port_vc_if cb_vc0_io,
   cb_port_vc_if cb_vc1_io
  );
 
-  typedef aicc_types.cmd_beat_t cmd_beat_t;
+  typedef bb_types.cmd_beat_t cmd_beat_t;
 
   logic flit_ot_vld_d, flit_ot_vld_q;
   cmd_beat_t flit_ot_d, flit_ot_q;
@@ -123,68 +123,65 @@ module simple_macro_mem #(
   end
 endmodule
 
-module tm_top #(
-  parameter aerial::cfg_t aer_cfg=0)(
+module xm_top #(
+  parameter soc::cfg_t soc_cfg=0)(
     // Interface to SMM from CB
     cb_port_vc_if cb_vc0_io,
     cb_port_vc_if cb_vc1_io
   );
 
-  aicc_types_if #(aer_cfg) aicc_types();
+  bb_types_if #(soc_cfg) bb_types();
 
-  simple_macro_mem #() simple_macro_mem(
-    .aicc_types(aicc_types),
+  simple_mem #() simple_mem(
+    .bb_types(bb_types),
     .cb_vc0_io(cb_vc0_io),
     .cb_vc1_io(cb_vc1_io)
   );
 endmodule
 
-module aitile_top #(
-  parameter aerial::cfg_t aer_cfg=0
+module tile_top #(
+  parameter soc::cfg_t soc_cfg=0
 )(
  );
 
-  aicc_types_if #(aer_cfg) aicc_types();
-  typedef aicc_types.cmd_beat_t cmd_beat_t;
+  bb_types_if #(soc_cfg) bb_types();
+  typedef bb_types.cmd_beat_t cmd_beat_t;
 
-  // cluster bus
   localparam cb::cfg_t cb_cfg = '{
-    Rids : aer_cfg.CBRids
-    ,Pids : aer_cfg.CBPids
-    ,Fnum : aer_cfg.CBFnum
-    ,XdatSize:$bits(cmd_beat_t)
+    Rids : soc_cfg.CBRids,
+    Pids : soc_cfg.CBPids,
+    Fnum : soc_cfg.CBFnum,
+    XdatSize:$bits(cmd_beat_t)
   };
 
-  // CB <-> TM (simple macro mem)
   cb_port_vc_if#(cb_cfg) cb_tm_vc0_io();
   cb_port_vc_if#(cb_cfg) cb_tm_vc1_io();
 
-  // TM
-  tm_top #(
-    .aer_cfg(aer_cfg)
-  ) tm_top(
+  xm_top #(
+    .soc_cfg(soc_cfg)
+  ) xm_top(
     .cb_vc0_io(cb_tm_vc0_io),
     .cb_vc1_io(cb_tm_vc1_io)
   );
 
 endmodule
 
-module aerial_top #(parameter aerial::cfg_t aer_cfg=0)(
+module soc_top #(parameter soc::cfg_t soc_cfg=0)(
 
  );
 
   genvar a;
-  localparam int p_num_aitile = aer_cfg.NumAitile;
+  localparam int p_num_tile = soc_cfg.Tile;
 
-  aicc_types_if #(aer_cfg) aicc_types();
-  typedef aicc_types.cmd_beat_t cmd_beat_t;
+  bb_types_if #(soc_cfg) bb_types();
+  typedef bb_types.cmd_beat_t cmd_beat_t;
 
   // CB and RBUS
   localparam cb::cfg_t cb_cfg = '{
-    Rids : aer_cfg.CBRids
-    ,Pids : aer_cfg.CBPids
-    ,Fnum : aer_cfg.CBFnum
-    ,XdatSize:$bits(cmd_beat_t)
+    Rids : soc_cfg.CBRids,
+    Pids : soc_cfg.CBPids,
+    Fnum : soc_cfg.CBFnum,
+    XdatSize:$bits(cmd_beat_t)
   };
 
   // cb interfaces (HTM dead ends)
@@ -192,18 +189,18 @@ module aerial_top #(parameter aerial::cfg_t aer_cfg=0)(
   cb_port_vc_if#(cb_cfg) cb_vc1_io();
 
   // HTM
-  tm_top #(
-    .aer_cfg(aer_cfg)
-  ) tm_top(
+  xm_top #(
+    .soc_cfg(soc_cfg)
+  ) xm_top(
     .cb_vc0_io(cb_vc0_io),
     .cb_vc1_io(cb_vc1_io)
   );
 
   generate
-    for(a=0; a<p_num_aitile; a++) begin : gen_aitiles
-      aitile_top #(
-        .aer_cfg(aer_cfg)
-      ) aitile(
+    for(a=0; a<p_num_tile; a++) begin : gen_tiles
+      tile_top #(
+        .soc_cfg(soc_cfg)
+      ) tile(
       );
     end
   endgenerate
@@ -212,34 +209,34 @@ endmodule
 
 module t();
 
-  parameter aerial::cfg_t aer_cfg = '{
+  parameter soc::cfg_t soc_cfg = '{
     // Cluster Bus
-    CBRids : 16
-    ,CBPids : 32
-    ,CBFnum : 4
+    CBRids : 16,
+    CBPids : 32,
+    CBFnum : 4,
     // Coarse parameters
-    ,NumAitile : 4
-    ,NumCca : 1
+    Tile : 4,
+    NumBb : 1,
     // CCA Parameters
-    ,NumCc : 3
+    NumDd : 3,
     // CC Parameters
-    ,CCNumWaves : 4
-    ,CCNumWaveThreads : 8
-    ,CCNumIds : 5
+    DDNumW : 4,
+    DDNumWT : 8,
+    DDIds : 5
   };
 
-  aerial_top #(
-    .aer_cfg(aer_cfg)
-  ) aerial_top();
+  soc_top #(
+    .soc_cfg(soc_cfg)
+  ) soc_top();
 
   // Check that cb_cfg.XdatSize is correctly computed as $bits(cmd_beat_t) = 525
-  // for both aerial_top and aitile_top paths
+  // for both soc_top and tile_top paths
   initial begin
-    `checkd(aerial_top.cb_cfg.XdatSize, 525);
-    `checkd(aerial_top.gen_aitiles[0].aitile.cb_cfg.XdatSize, 525);
-    `checkd(aerial_top.gen_aitiles[1].aitile.cb_cfg.XdatSize, 525);
-    `checkd(aerial_top.gen_aitiles[2].aitile.cb_cfg.XdatSize, 525);
-    `checkd(aerial_top.gen_aitiles[3].aitile.cb_cfg.XdatSize, 525);
+    `checkd(soc_top.cb_cfg.XdatSize, 525);
+    `checkd(soc_top.gen_tiles[0].tile.cb_cfg.XdatSize, 525);
+    `checkd(soc_top.gen_tiles[1].tile.cb_cfg.XdatSize, 525);
+    `checkd(soc_top.gen_tiles[2].tile.cb_cfg.XdatSize, 525);
+    `checkd(soc_top.gen_tiles[3].tile.cb_cfg.XdatSize, 525);
     $display("PASS: All cb_cfg.XdatSize = 525");
   end
 endmodule
