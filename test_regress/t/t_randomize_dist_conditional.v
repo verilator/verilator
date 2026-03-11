db9d782061165af5236ffd34642f2ded827d3bb0
Wilson Snyder <wsnyder@wsnyder.org>
2026-03-11 07:10:41 -0400

    Commentary: Changes update

--- test_regress/t/t_randomize_dist_conditional.v
+++ test_regress/t/t_randomize_dist_conditional.v
@@ -4,8 +4,11 @@
 // SPDX-FileCopyrightText: 2026
 // SPDX-License-Identifier: CC0-1.0
 
+// verilog_format: off
 `define stop $stop
 `define checkd(gotv,expv) do if ((gotv) !== (expv)) begin $write("%%Error: %s:%0d:  got=%0d exp=%0d\n", `__FILE__,`__LINE__, (gotv), (expv)); `stop; end while(0);
+// verilog_format: on
+
 // Test constraint with conditional dist operator
 
 class DistScalar;
