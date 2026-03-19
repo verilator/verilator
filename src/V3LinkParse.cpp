10b39019664a9c08fdb87a59a409b78f7ecabbc2
github action <action@example.com>
2026-03-19 04:07:12 +0000

    Apply 'make format'

--- src/V3LinkParse.cpp
+++ src/V3LinkParse.cpp
@@ -1362,7 +1362,8 @@ class LinkParseVisitor final : public VNVisitor {
                 } else if (optp->name() == "comment") {
                     optType = VCoverOptionType::COMMENT;
                 } else {
-                    optp->v3warn(COVERIGN, "Ignoring unsupported coverage option: " + optp->name());
+                    optp->v3warn(COVERIGN,
+                                 "Ignoring unsupported coverage option: " + optp->name());
                 }
                 nodep->addOptionsp(new AstCoverOption{optp->fileline(), optType,
                                                        optp->valuep()->cloneTree(false)});
