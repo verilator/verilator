c45902348ba1f3c22296348b2601507fe99c4bdb
Matthew Ballance <matt.ballance@gmail.com>
2026-03-25 16:05:00 +0000

    Clean up unneeded dump()
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- src/V3AstNodes.cpp
+++ src/V3AstNodes.cpp
@@ -3736,10 +3736,6 @@ void AstWith::dumpJson(std::ostream& str) const {
 //######################################################################
 // Functional coverage dump methods
 
-void AstCovergroup::dump(std::ostream& str) const { this->AstNode::dump(str); }
-
-void AstCovergroup::dumpJson(std::ostream& str) const { dumpJsonGen(str); }
-
 void AstCoverpoint::dump(std::ostream& str) const { this->AstNodeFuncCovItem::dump(str); }
 
 void AstCoverpoint::dumpJson(std::ostream& str) const { this->AstNodeFuncCovItem::dumpJson(str); }
