f93673b60baa19037ed94545377479edf7b54445
Igor Zaworski <izaworski@antmicro.com>
2026-03-27 17:55:47 +0100

    Complex expression test
    Signed-off-by: Igor Zaworski <izaworski@antmicro.com>

---
+++ test_regress/t/t_fourstate_complex_expr.v
@@ -0,0 +1,23 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2026 Antmicro
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+  function logic f(logic a);
+    if (a === 1'b1) $write("1");
+    else if (a === 1'b0) $write("0");
+    else if (a === 1'bx) $write("x");
+    else if (a === 1'bz) $write("z");
+    else $stop;
+    $write("\n");
+    return a;
+  endfunction
+
+  initial begin
+    if (((f(1) && f('x)) ? (f(0) && f('x)) : (f('x) && f(0))) !== 0) $stop;
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
