10967401131c41dc385414d712a12d8be2ea3aeb
github action <action@example.com>
2026-03-30 23:12:27 +0000

    Apply 'make format'

--- src/V3EmitCFunc.h
+++ src/V3EmitCFunc.h
@@ -616,8 +616,7 @@ public:
             // Wide functions assign into the array directly, don't need separate assign statement
             m_wideTempRefp = VN_AS(nodep->lhsp(), VarRef);
             paren = false;
-        } else if (nodep->isWide() && !unpackDtp
-                   && !VN_IS(nodep->rhsp(), Const)) {
+        } else if (nodep->isWide() && !unpackDtp && !VN_IS(nodep->rhsp(), Const)) {
             putnbs(nodep, "VL_ASSIGN_W(");
             puts(cvtToStr(nodep->widthMin()) + ", ");
             iterateAndNextConstNull(nodep->lhsp());
