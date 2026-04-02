5925c326c1500ddc0f39f76af0777af1721f779a
Yilou Wang <wangyilou123@gmail.com>
2026-04-02 14:09:05 +0200

    2 space indents fix

--- test_regress/t/t_interface_virtual_func_wait.v
+++ test_regress/t/t_interface_virtual_func_wait.v
@@ -5,37 +5,37 @@
 // SPDX-License-Identifier: CC0-1.0
 
 interface my_if;
-   logic clk = 0;
-   bit   clk_active = 0;
+  logic clk = 0;
+  bit   clk_active = 0;
 
-   initial begin
-      wait (clk_active);
-      forever #5 clk = ~clk;
-   end
+  initial begin
+    wait (clk_active);
+    forever #5 clk = ~clk;
+  end
 
-   function void start_clk();
-      clk_active = 1;
-   endfunction
+  function void start_clk();
+    clk_active = 1;
+  endfunction
 endinterface
 
 class Driver;
-   virtual my_if vif;
-   task run();
-      #10;
-      vif.start_clk();
-   endtask
+  virtual my_if vif;
+  task run();
+    #10;
+    vif.start_clk();
+  endtask
 endclass
 
 module t;
-   my_if intf();
-   my_if intf_unused();  // Second instance triggers the bug
+  my_if intf();
+  my_if intf_unused();  // Second instance triggered the bug
 
-   initial begin
-      automatic Driver d = new;
-      d.vif = intf;
-      d.run();
-      repeat (4) @(posedge intf.clk);
-      $write("*-* All Finished *-*\n");
-      $finish;
-   end
+  initial begin
+    automatic Driver d = new;
+    d.vif = intf;
+    d.run();
+    repeat (4) @(posedge intf.clk);
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
 endmodule
