0a50ddf287a8950a2ae4d7e90fac9fbaeee139b1
Artur Bieniek <abieniek@antmicro.com>
2026-04-07 12:44:52 +0200

    Support rise/fall delays (#7368)
    Signed-off-by: Artur Bieniek <abieniek@antmicro.com>

--- test_regress/t/t_risefall_delay.py
+++ test_regress/t/t_risefall_delay.py
@@ -4,17 +4,15 @@
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of either the GNU Lesser General Public License Version 3
 # or the Perl Artistic License Version 2.0.
-# SPDX-FileCopyrightText: 2024 Wilson Snyder
+# SPDX-FileCopyrightText: 2026 Wilson Snyder
 # SPDX-License-Identifier: LGPL-3.0-only OR Artistic-2.0
 
 import vltest_bootstrap
 
-test.scenarios('vlt')
-test.top_filename = "t/t_gate_basic.v"
+test.scenarios('simulator')
 
-test.compile(
-    verilator_flags2=["--timing", "-Wall", "-Wno-DECLFILENAME -Wno-SPECIFYIGN -Wno-UNUSED"],
-    fails=True,
-    expect_filename=test.golden_filename)
+test.compile(timing_loop=True, verilator_flags2=["--timing"])
+
+test.execute()
 
 test.passes()
