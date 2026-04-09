c483bbb0fa1b2561bea72bd005aed415b74eb925
github action <action@example.com>
2026-04-09 18:58:50 +0000

    Apply 'make format'

--- test_regress/t/t_assert_rep_bad_count.py
+++ test_regress/t/t_assert_rep_bad_count.py
@@ -11,7 +11,6 @@ import vltest_bootstrap
 
 test.scenarios('linter')
 
-test.lint(verilator_flags2=['--error-limit 100'],
-         fails=True, expect_filename=test.golden_filename)
+test.lint(verilator_flags2=['--error-limit 100'], fails=True, expect_filename=test.golden_filename)
 
 test.passes()
