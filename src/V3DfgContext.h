247023657d28fdc26997aafc5e1e876cb903bc16
Geza Lore <glore@tenstorrent.com>
2026-04-09 13:32:16 +0100

    Internals: Improve Dfg pattern dumps

--- src/V3DfgContext.h
+++ src/V3DfgContext.h
@@ -25,11 +25,12 @@
 #include "config_build.h"
 #include "verilatedos.h"
 
-#include "V3DfgPatternStats.h"
+#include "V3Ast.h"
 #include "V3DfgPeepholePatterns.h"
+#include "V3Error.h"
 #include "V3File.h"
+#include "V3Global.h"
 #include "V3Stats.h"
-#include "V3String.h"
 
 class V3DfgContext;
 
