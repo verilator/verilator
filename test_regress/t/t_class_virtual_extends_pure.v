e1381b75a103fc4b4ac1203ffed8c23d70cab6b9
Nikolay Puzanov <punzik@gmail.com>
2026-04-10 17:14:49 +0300

    Use standard 'SPDX-FileCopyrightText: 2026 Wilson Snyder'

--- test_regress/t/t_class_virtual_extends_pure.v
+++ test_regress/t/t_class_virtual_extends_pure.v
@@ -1,7 +1,7 @@
 // DESCRIPTION: Verilator: Verilog Test module
 //
 // This file ONLY is placed under the Creative Commons Public Domain.
-// SPDX-FileCopyrightText: 2026 Nikolay Puzanov
+// SPDX-FileCopyrightText: 2026 Wilson Snyder
 // SPDX-License-Identifier: CC0-1.0
 
 virtual class Base;
