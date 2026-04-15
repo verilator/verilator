4fe121e5aa9a9d4ebe2033cdbe68a6658a2cb052
Wilson Snyder <wsnyder@wsnyder.org>
2026-04-15 17:46:08 -0400

    Commentary: Changes update

--- test_regress/t/t_trace_split_struct.v
+++ test_regress/t/t_trace_split_struct.v
@@ -9,37 +9,39 @@
 
 bit global_bit;
 
-module t (clk, clk2);
-   input clk;
-   input clk2;
-   integer cyc = 0;
+module t (
+    clk,
+    clk2
+);
+  input clk;
+  input clk2;
+  integer cyc = 0;
 
-   typedef struct packed {
-      bit         b1;
-      bit         b0;
-   } strp_t;
+  typedef struct packed {
+    bit b1;
+    bit b0;
+  } strp_t;
 
-   strp_t       v_strp, v_strp2;
-   logic foo;
+  strp_t v_strp, v_strp2;
+  logic foo;
 
-   logic [7:0]       unpacked_array[-7:0];
+  logic [7:0] unpacked_array[-7:0];
 
-   always @ (posedge clk) begin
-      cyc <= cyc + 1;
-      foo <= ~foo;
-      v_strp.b0 <= cyc[0];
-      v_strp2.b0 <= cyc[2];
-      unpacked_array[0] = cyc[8:1];
-      if (cyc == 5) begin
-         $write("*-* All Finished *-*\n");
-         $finish;
-      end
-   end
+  always @(posedge clk) begin
+    cyc <= cyc + 1;
+    foo <= ~foo;
+    v_strp.b0 <= cyc[0];
+    v_strp2.b0 <= cyc[2];
+    unpacked_array[0] = cyc[8:1];
+    if (cyc == 5) begin
+      $write("*-* All Finished *-*\n");
+      $finish;
+    end
+  end
 
-   always @(posedge clk2) begin
-      v_strp.b1 <= cyc[1];
-      v_strp2.b1 <= cyc[3];
-     for (int i = -1; i > -8; i--)
-       unpacked_array[i] = cyc[7:0];
-   end
+  always @(posedge clk2) begin
+    v_strp.b1 <= cyc[1];
+    v_strp2.b1 <= cyc[3];
+    for (int i = -1; i > -8; i--) unpacked_array[i] = cyc[7:0];
+  end
 endmodule
--- test_regress/t/t_trace_type_alias.v
+++ test_regress/t/t_trace_type_alias.v
@@ -6,41 +6,41 @@
 
 module Sub #(
     parameter int data_width
-)(
+) (
     input clk,
-    output logic [data_width-1:0]       read_data
+    output logic [data_width-1:0] read_data
 );
-    logic [0:0][data_width-1:0] read_data_2d;
-    always_ff @(posedge clk) read_data_2d <= $random;
-    always_comb read_data = read_data_2d[0];
+  logic [0:0][data_width-1:0] read_data_2d;
+  always_ff @(posedge clk) read_data_2d <= $random;
+  always_comb read_data = read_data_2d[0];
 endmodule
 
 module t ();
-    typedef struct packed {
-        logic [7:0] field_a;
-        logic [7:0] field_b;
-        logic [7:0] field_c;
-        logic field_d;
-        logic field_e;
-        logic field_f;
-        logic field_g;
-        logic field_h;
-        logic field_i;
-        logic field_j;
-        logic field_k;
-    } struct_t;
-    struct_t the_struct;
-    logic clk;
+  typedef struct packed {
+    logic [7:0] field_a;
+    logic [7:0] field_b;
+    logic [7:0] field_c;
+    logic field_d;
+    logic field_e;
+    logic field_f;
+    logic field_g;
+    logic field_h;
+    logic field_i;
+    logic field_j;
+    logic field_k;
+  } struct_t;
+  struct_t the_struct;
+  logic clk;
 
-    Sub #(
-        .data_width   ($bits(struct_t))
-    ) the_sub(
-        .clk,
-        .read_data (the_struct)
-    );
+  Sub #(
+      .data_width($bits(struct_t))
+  ) the_sub (
+      .clk,
+      .read_data(the_struct)
+  );
 
-    initial begin
-        $write("*-* All Finished *-*\n");
-        $finish;
-    end
+  initial begin
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
 endmodule
