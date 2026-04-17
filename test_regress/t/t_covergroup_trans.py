1d72c050aa80f2866dd4b8688fed6c5380efb75f
Matthew Ballance <matt.ballance@gmail.com>
2026-04-17 02:22:42 +0000

    Migrate to new test format
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- test_regress/t/t_covergroup_trans.py
+++ test_regress/t/t_covergroup_trans.py
@@ -8,14 +8,8 @@
 # SPDX-License-Identifier: LGPL-3.0-only OR Artistic-2.0
 
 import vltest_bootstrap
+import coverage_covergroup_common
 
 test.scenarios('vlt_all')
 
-test.compile(verilator_flags2=['--coverage --Wno-COVERIGN --dumpi-tree 3 --dumpi-tree-json 3'])
-
-test.execute()
-
-test.covergroup_coverage_report()
-test.files_identical(test.obj_dir + '/covergroup_report.txt', test.golden_filename)
-
-test.passes()
+coverage_covergroup_common.run(test, verilator_flags2=['--Wno-COVERIGN --dumpi-tree 3 --dumpi-tree-json 3'])
