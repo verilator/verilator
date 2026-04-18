9f34c1176bd625a2c3767293f74095e1ca168dd9
Yogish Sekhar <yogish.sekhar@zerorisc.com>
2026-04-18 11:25:09 +0100

    New test for gcov gaps

---
+++ test_regress/t/t_cover_fsm_styles.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# DESCRIPTION: Verilator: FSM coverage style coverage test
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of either the GNU Lesser General Public License Version 3
+# or the Perl Artistic License Version 2.0.
+# SPDX-FileCopyrightText: 2026 Wilson Snyder
+# SPDX-License-Identifier: LGPL-3.0-only OR Artistic-2.0
+
+import vltest_bootstrap
+
+test.scenarios('simulator')
+
+test.compile(verilator_flags2=['--cc --coverage-fsm'])
+
+test.execute()
+
+test.file_grep(test.obj_dir + "/coverage.dat", r"fsm_state")
+test.file_grep(test.obj_dir + "/coverage.dat", r"fsm_arc")
+test.file_grep(test.obj_dir + "/coverage.dat", r"ANY->S0\[reset\]")
+test.file_grep(test.obj_dir + "/coverage.dat", r"default->S0")
+test.file_grep(test.obj_dir + "/coverage.dat", r"S0->S1")
+test.file_grep(test.obj_dir + "/coverage.dat", r"S0->S2")
+test.file_grep(test.obj_dir + "/coverage.dat", r"S1->S3")
+
+test.passes()
