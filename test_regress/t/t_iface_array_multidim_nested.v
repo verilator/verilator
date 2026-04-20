5db3c22147f9af43d823c57ea0e5780943d9192f
Eric Mejdrich <ericmejdrich@omniworks.be>
2026-04-20 14:47:55 +0200

    fixing spacing and headers.  updated CONTRIBUTORS

--- test_regress/t/t_iface_array_multidim_nested.v
+++ test_regress/t/t_iface_array_multidim_nested.v
@@ -1,66 +1,68 @@
-// DESCRIPTION: Verilator: multi-dim iface array where outer iface contains an inner iface.
+// DESCRIPTION: Verilator: Verilog Test module
 //
-// Exercises composition: outer_if oarr[A][B]() where outer_if contains a single
-// inner_if instance. Access oarr[i][j].inner.data.
-//
+// This file ONLY is placed under the Creative Commons Public Domain.
 // SPDX-FileCopyrightText: 2026 Wilson Snyder
 // SPDX-License-Identifier: CC0-1.0
 
+// Multi-dim iface array where outer iface contains an inner iface.
+// outer_if oarr[A][B](), outer_if holds a single inner_if instance.
+// Access via oarr[i][j].inner.data.
+
 interface inner_if;
-   logic [7:0] data;
+  logic [7:0] data;
 endinterface
 
 interface outer_if;
-   inner_if inner();
-   logic [7:0] tag;
+  inner_if inner();
+  logic [7:0] tag;
 endinterface
 
 module t;
-   localparam int A = 2;
-   localparam int B = 2;
+  localparam int A = 2;
+  localparam int B = 2;
 
-   outer_if oarr [A-1:0][B-1:0] ();
+  outer_if oarr [A-1:0][B-1:0] ();
 
-   genvar gi, gj;
-   generate
-      for (gi = 0; gi < A; gi++) begin : g_a
-         for (gj = 0; gj < B; gj++) begin : g_b
-            initial begin
-               oarr[gi][gj].tag = 8'(gi * 16 + gj);
-               oarr[gi][gj].inner.data = 8'(gi * B + gj + 100);
-            end
-         end
+  genvar gi, gj;
+  generate
+    for (gi = 0; gi < A; gi++) begin : g_a
+      for (gj = 0; gj < B; gj++) begin : g_b
+        initial begin
+          oarr[gi][gj].tag = 8'(gi * 16 + gj);
+          oarr[gi][gj].inner.data = 8'(gi * B + gj + 100);
+        end
       end
-   endgenerate
+    end
+  endgenerate
 
-   logic [7:0] chk_tag [A-1:0][B-1:0];
-   logic [7:0] chk_inner [A-1:0][B-1:0];
-   generate
-      for (gi = 0; gi < A; gi++) begin : g_a_chk
-         for (gj = 0; gj < B; gj++) begin : g_b_chk
-            always_comb chk_tag[gi][gj] = oarr[gi][gj].tag;
-            always_comb chk_inner[gi][gj] = oarr[gi][gj].inner.data;
-         end
+  logic [7:0] chk_tag [A-1:0][B-1:0];
+  logic [7:0] chk_inner [A-1:0][B-1:0];
+  generate
+    for (gi = 0; gi < A; gi++) begin : g_a_chk
+      for (gj = 0; gj < B; gj++) begin : g_b_chk
+        always_comb chk_tag[gi][gj] = oarr[gi][gj].tag;
+        always_comb chk_inner[gi][gj] = oarr[gi][gj].inner.data;
       end
-   endgenerate
+    end
+  endgenerate
 
-   initial begin
-      #1;
-      for (int i = 0; i < A; i++) begin
-         for (int j = 0; j < B; j++) begin
-            if (chk_tag[i][j] !== 8'(i * 16 + j)) begin
-               $write("%%Error: oarr[%0d][%0d].tag=%0d expected %0d\n",
-                      i, j, chk_tag[i][j], i * 16 + j);
-               $stop;
-            end
-            if (chk_inner[i][j] !== 8'(i * B + j + 100)) begin
-               $write("%%Error: oarr[%0d][%0d].inner.data=%0d expected %0d\n",
-                      i, j, chk_inner[i][j], i * B + j + 100);
-               $stop;
-            end
-         end
+  initial begin
+    #1;
+    for (int i = 0; i < A; i++) begin
+      for (int j = 0; j < B; j++) begin
+        if (chk_tag[i][j] !== 8'(i * 16 + j)) begin
+          $write("%%Error: oarr[%0d][%0d].tag=%0d expected %0d\n",
+                 i, j, chk_tag[i][j], i * 16 + j);
+          $stop;
+        end
+        if (chk_inner[i][j] !== 8'(i * B + j + 100)) begin
+          $write("%%Error: oarr[%0d][%0d].inner.data=%0d expected %0d\n",
+                 i, j, chk_inner[i][j], i * B + j + 100);
+          $stop;
+        end
       end
-      $write("*-* All Finished *-*\n");
-      $finish;
-   end
+    end
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
 endmodule
--- test_regress/t/t_iface_array_multidim_xref.v
+++ test_regress/t/t_iface_array_multidim_xref.v
@@ -1,63 +1,64 @@
-// DESCRIPTION: Verilator: cross-hierarchy dotted refs through a multi-dim iface
-// array of a *parameterized* interface.  Exercises IfaceCapture + the multi-dim
-// dotted-access resolver together.
+// DESCRIPTION: Verilator: Verilog Test module
 //
+// This file ONLY is placed under the Creative Commons Public Domain.
 // SPDX-FileCopyrightText: 2026 Wilson Snyder
 // SPDX-License-Identifier: CC0-1.0
 
+// Cross-hierarchy dotted refs through a multi-dim iface array of a
+// parameterized interface.  Exercises IfaceCapture plus the multi-dim
+// dotted-access resolver together.
+
 interface bus_if #(parameter int W = 8);
-   logic [W-1:0] data;
+  logic [W-1:0] data;
 endinterface
 
-// Wrapper that holds the multi-dim iface array; a nested sub-module reaches
-// up through hierarchy to read/write cells.
+// Wrapper holding the multi-dim iface array; parent reads its cells via dots.
 module holder;
-   localparam int A = 2;
-   localparam int B = 3;
+  localparam int A = 2;
+  localparam int B = 3;
 
-   bus_if #(.W(8)) bus [A-1:0][B-1:0] ();
+  bus_if #(.W(8)) bus [A-1:0][B-1:0] ();
 
-   genvar gi, gj;
-   generate
-      for (gi = 0; gi < A; gi++) begin : g_a
-         for (gj = 0; gj < B; gj++) begin : g_b
-            initial bus[gi][gj].data = 8'(gi * B + gj + 5);
-         end
+  genvar gi, gj;
+  generate
+    for (gi = 0; gi < A; gi++) begin : g_a
+      for (gj = 0; gj < B; gj++) begin : g_b
+        initial bus[gi][gj].data = 8'(gi * B + gj + 5);
       end
-   endgenerate
+    end
+  endgenerate
 endmodule
 
 module t;
-   holder h();
+  holder h();
 
-   // Cross-hierarchy reads via explicit dotted path through the multi-dim array.
-   initial begin
-      #1;
-      if (h.bus[0][0].data !== 8'd5) begin
-         $write("%%Error: h.bus[0][0].data=%0d expected 5\n", h.bus[0][0].data);
-         $stop;
-      end
-      if (h.bus[0][1].data !== 8'd6) begin
-         $write("%%Error: h.bus[0][1].data=%0d expected 6\n", h.bus[0][1].data);
-         $stop;
-      end
-      if (h.bus[0][2].data !== 8'd7) begin
-         $write("%%Error: h.bus[0][2].data=%0d expected 7\n", h.bus[0][2].data);
-         $stop;
-      end
-      if (h.bus[1][0].data !== 8'd8) begin
-         $write("%%Error: h.bus[1][0].data=%0d expected 8\n", h.bus[1][0].data);
-         $stop;
-      end
-      if (h.bus[1][1].data !== 8'd9) begin
-         $write("%%Error: h.bus[1][1].data=%0d expected 9\n", h.bus[1][1].data);
-         $stop;
-      end
-      if (h.bus[1][2].data !== 8'd10) begin
-         $write("%%Error: h.bus[1][2].data=%0d expected 10\n", h.bus[1][2].data);
-         $stop;
-      end
-      $write("*-* All Finished *-*\n");
-      $finish;
-   end
+  initial begin
+    #1;
+    if (h.bus[0][0].data !== 8'd5) begin
+      $write("%%Error: h.bus[0][0].data=%0d expected 5\n", h.bus[0][0].data);
+      $stop;
+    end
+    if (h.bus[0][1].data !== 8'd6) begin
+      $write("%%Error: h.bus[0][1].data=%0d expected 6\n", h.bus[0][1].data);
+      $stop;
+    end
+    if (h.bus[0][2].data !== 8'd7) begin
+      $write("%%Error: h.bus[0][2].data=%0d expected 7\n", h.bus[0][2].data);
+      $stop;
+    end
+    if (h.bus[1][0].data !== 8'd8) begin
+      $write("%%Error: h.bus[1][0].data=%0d expected 8\n", h.bus[1][0].data);
+      $stop;
+    end
+    if (h.bus[1][1].data !== 8'd9) begin
+      $write("%%Error: h.bus[1][1].data=%0d expected 9\n", h.bus[1][1].data);
+      $stop;
+    end
+    if (h.bus[1][2].data !== 8'd10) begin
+      $write("%%Error: h.bus[1][2].data=%0d expected 10\n", h.bus[1][2].data);
+      $stop;
+    end
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
 endmodule
