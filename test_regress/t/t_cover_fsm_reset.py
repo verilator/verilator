d05fc3a5f172021d132a62e938cc3118bbfcd086
Yogish Sekhar <yogish.sekhar@zerorisc.com>
2026-04-22 23:09:32 +0100

    Review feedback changes and golden files

--- test_regress/t/t_cover_fsm_reset.py
+++ test_regress/t/t_cover_fsm_reset.py
@@ -26,13 +26,7 @@ test.run(cmd=[
          tee=False,
          verilator_run=True)
 
-test.file_grep(test.obj_dir + "/summary.log", r"Coverage Summary:")
-test.file_grep(test.obj_dir + "/summary.log", r"line")
-test.file_grep(test.obj_dir + "/summary.log", r"toggle")
-test.file_grep(test.obj_dir + "/summary.log", r"branch")
-test.file_grep(test.obj_dir + "/summary.log", r"expr")
-test.file_grep(test.obj_dir + "/summary.log", r"fsm_state")
-test.file_grep(test.obj_dir + "/summary.log", r"fsm_arc")
+test.files_identical(test.obj_dir + "/summary.log", "t/" + test.name + "_summary.out")
 
 test.run(cmd=[
     os.environ["VERILATOR_ROOT"] + "/bin/verilator_coverage",
