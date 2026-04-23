4e36788a0727934972953f32fa6960c2e39eb0c0
Geza Lore <glore@tenstorrent.com>
2026-04-23 12:06:33 +0100

    Optimize mix of Concat/Extend assignments
    Generalize concatenation splitting/balancing in V3FuncOpt to also handle
    AstExtend (which is a Concat with a 0 LHS).

--- test_regress/t/t_opt_balance_cats.v
+++ test_regress/t/t_opt_balance_cats.v
@@ -4,30 +4,51 @@
 // SPDX-FileCopyrightText: 2024 Wilson Snyder
 // SPDX-License-Identifier: CC0-1.0
 
-module t(i, o);
+module t(
+  clk,
+  i0, o0,
+  i1, o1
+);
   localparam N = 2000; // Deliberately not multiple of 32
 
-  input i;
-  wire [N-1:0] i;
+  input clk;
+  wire clk;
 
-  output o;
-  wire [N-1:0] o;
+  // Case 1: concatenations only
+
+  input i0;
+  wire [N-1:0] i0;
+
+  output o0;
+  wire [N-1:0] o0;
 
   for (genvar n = 0 ; n + 31 < N ; n += 32) begin
-    assign o[n+ 0 +: 1] = i[(N-1-n)- 0 -: 1];
-    assign o[n+ 1 +: 1] = i[(N-1-n)- 1 -: 1];
-    assign o[n+ 2 +: 2] = i[(N-1-n)- 2 -: 2];
-    assign o[n+ 4 +: 4] = i[(N-1-n)- 4 -: 4];
-    assign o[n+ 8 +: 8] = i[(N-1-n)- 8 -: 8];
-    assign o[n+16 +: 8] = i[(N-1-n)-16 -: 8];
-    assign o[n+24 +: 4] = i[(N-1-n)-24 -: 4];
-    assign o[n+28 +: 2] = i[(N-1-n)-28 -: 2];
-    assign o[n+30 +: 1] = i[(N-1-n)-30 -: 1];
-    assign o[n+31 +: 1] = i[(N-1-n)-31 -: 1];
+    assign o0[n+ 0 +: 1] = i0[(N-1-n)- 0 -: 1];
+    assign o0[n+ 1 +: 1] = i0[(N-1-n)- 1 -: 1];
+    assign o0[n+ 2 +: 2] = i0[(N-1-n)- 2 -: 2];
+    assign o0[n+ 4 +: 4] = i0[(N-1-n)- 4 -: 4];
+    assign o0[n+ 8 +: 8] = i0[(N-1-n)- 8 -: 8];
+    assign o0[n+16 +: 8] = i0[(N-1-n)-16 -: 8];
+    assign o0[n+24 +: 4] = i0[(N-1-n)-24 -: 4];
+    assign o0[n+28 +: 2] = i0[(N-1-n)-28 -: 2];
+    assign o0[n+30 +: 1] = i0[(N-1-n)-30 -: 1];
+    assign o0[n+31 +: 1] = i0[(N-1-n)-31 -: 1];
   end
 
   for (genvar n = N / 32 * 32; n < N ; ++n) begin
-    assign o[n] = i[N-1-n];
+    assign o0[n] = i0[N-1-n];
+  end
+
+  // Case 2: mixed concatenations and zero extension
+
+  input i1;
+  logic [N-1:0] i1;
+
+  output o1;
+  logic [N-1:0] o1;
+
+  always @(posedge clk) begin
+    o1 = N'({i1[0 +: N/4], (N/2 - 128)'(i1[N/2 +: N/8]), 128'({i1[0 +: 10], 22'(i1[10 +: 10])})});
   end
 
 endmodule
