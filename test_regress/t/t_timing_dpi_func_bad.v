de0df2ebeb611bbd418f0ceb606fab4e4ed47dee
Pawel Kojma <pkojma@internships.antmicro.com>
2026-04-24 09:06:46 +0200

    Fix copyrights and outs
    Signed-off-by: Pawel Kojma <pkojma@internships.antmicro.com>

--- test_regress/t/t_timing_dpi_func_bad.v
+++ test_regress/t/t_timing_dpi_func_bad.v
@@ -2,6 +2,7 @@
 //
 // This file ONLY is placed under the Creative Commons Public Domain, for
 // any use, without warranty, 2026 by Antmicro.
+// SPDX-FileCopyrightText: 2026 Antmicro
 // SPDX-License-Identifier: CC0-1.0
 
 module dpi_test ();
