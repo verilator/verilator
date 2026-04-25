0d07de590cb99b00e20763962e71804465a0c0f6
Matthew Ballance <matt.ballance@gmail.com>
2026-04-25 18:08:41 +0000

    Test cleanup
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- test_regress/t/t_covergroup_unsup_ign.py
+++ test_regress/t/t_covergroup_unsup_ign.py
@@ -12,6 +12,7 @@ import vltest_bootstrap
 test.scenarios('vlt')
 test.top_filename = "t/t_covergroup_unsup.v"
 
-test.lint(verilator_flags2=['--assert --coverage --Wno-COVERIGN +define+T_COVERGROUP_UNSUP_IGN'])
+test.lint(verilator_flags2=['--assert --coverage --Wno-COVERIGN +define+T_COVERGROUP_UNSUP_IGN'],
+          expect_filename=test.golden_filename)
 
 test.passes()
