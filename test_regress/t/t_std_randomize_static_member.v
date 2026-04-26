0c125d054f0cd6dbfbab07ec4983907a4d0ae42d
hjsanjana <sanjanajagadeesha23@gmail.com>
2026-04-26 02:47:32 -0700

    Use 2-space indentation in test file

--- test_regress/t/t_std_randomize_static_member.v
+++ test_regress/t/t_std_randomize_static_member.v
@@ -11,70 +11,70 @@
 
 package tlogy_m_pkg;
 class v_cfg;
-   static int num_of_ds = 0;
+  static int num_of_ds = 0;
 endclass
 
 class tlogy_m;
-   v_cfg v[$];
+  v_cfg v[$];
 endclass
 endpackage
 
 package s_pkg;
 import tlogy_m_pkg::*;
 class s_cfg;
-   tlogy_m t_m;
-   bit     t_mode;
-   int     a_d_idx;
+  tlogy_m t_m;
+  bit t_mode;
+  int a_d_idx;
 
-   function void setup_h_iw_cfg();
-      if (t_mode) begin
-         foreach (t_m.v[i]) begin
-            if (std::randomize(a_d_idx) with {
-                   if (t_m.v[i].num_of_ds > 1) {
-                      a_d_idx inside {[0:(t_m.v[i].num_of_ds - 1)]};
-                      a_d_idx != 0;
-                   }
-                } == 0)
-              $stop;
-         end
+  function void setup_h_iw_cfg();
+    if (t_mode) begin
+      foreach (t_m.v[i]) begin
+        if (std::randomize(a_d_idx) with {
+               if (t_m.v[i].num_of_ds > 1) {
+                 a_d_idx inside {[0:(t_m.v[i].num_of_ds - 1)]};
+                 a_d_idx != 0;
+               }
+             } == 0)
+          $stop;
       end
-   endfunction
+    end
+  endfunction
 endclass
 endpackage
 
 module t;
-   import tlogy_m_pkg::*;
-   import s_pkg::*;
+  import tlogy_m_pkg::*;
+  import s_pkg::*;
 
-   initial begin
-      automatic s_cfg cfg = new;
-      automatic tlogy_m tm = new;
-      automatic v_cfg vc0 = new;
-      automatic v_cfg vc1 = new;
+  initial begin
+    automatic s_cfg cfg = new;
+    automatic tlogy_m tm = new;
+    automatic v_cfg vc0 = new;
+    automatic v_cfg vc1 = new;
 
-      // Set up: push two entries in the queue and set num_of_ds = 3
-      // so the constraint branch (num_of_ds > 1) is exercised.
-      tm.v.push_back(vc0);
-      tm.v.push_back(vc1);
-      v_cfg::num_of_ds = 3;
+    // Set up: push two entries in the queue and set num_of_ds = 3
+    // so the constraint branch (num_of_ds > 1) is exercised.
+    tm.v.push_back(vc0);
+    tm.v.push_back(vc1);
+    v_cfg::num_of_ds = 3;
 
-      cfg.t_m = tm;
-      cfg.t_mode = 1'b1;
+    cfg.t_m = tm;
+    cfg.t_mode = 1'b1;
 
-      repeat (20) begin
-         cfg.setup_h_iw_cfg();
-         // When num_of_ds = 3 the constraint is:
-         //   a_d_idx inside {[0:2]} && a_d_idx != 0
-         // so a_d_idx must be 1 or 2.
-         if (cfg.a_d_idx < 1 || cfg.a_d_idx > 2) $stop;
-      end
-
-      // Also verify that the "no constraint" branch (num_of_ds <= 1)
-      // compiles and runs without crashing.
-      v_cfg::num_of_ds = 0;
+    repeat (20) begin
       cfg.setup_h_iw_cfg();
+      // When num_of_ds = 3 the constraint is:
+      //   a_d_idx inside {[0:2]} && a_d_idx != 0
+      // so a_d_idx must be 1 or 2.
+      if (cfg.a_d_idx < 1 || cfg.a_d_idx > 2) $stop;
+    end
 
-      $write("*-* All Finished *-*\n");
-      $finish;
-   end
+    // Also verify that the "no constraint" branch (num_of_ds <= 1)
+    // compiles and runs without crashing.
+    v_cfg::num_of_ds = 0;
+    cfg.setup_h_iw_cfg();
+
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
 endmodule
