ade33903909624ea325b36a634bf4037725bb0e9
Miguel Perez Andrade <miguel.perezandrade@gmail.com>
2026-05-03 21:20:01 -0300

    fixed indentation

--- test_regress/t/t_runflag_seed_zero.v
+++ test_regress/t/t_runflag_seed_zero.v
@@ -5,12 +5,12 @@
 // SPDX-License-Identifier: CC0-1.0
 
 module t;
-   int seed;
-   initial begin
-      seed = $get_initial_random_seed();
-      $display("seed=%0d", seed);
-      if (seed == 0) $stop;  // +verilator+seed+0 must be replaced by a non-zero picked seed
-      $write("*-* All Finished *-*\n");
-      $finish(2);
-   end
+  int seed;
+  initial begin
+    seed = $get_initial_random_seed();
+    $display("seed=%0d", seed);
+    if (seed == 0) $stop;  // +verilator+seed+0 must be replaced by a non-zero picked seed
+    $write("*-* All Finished *-*\n");
+    $finish(2);
+  end
 endmodule
