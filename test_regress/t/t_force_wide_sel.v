d9913ebba66b625314bada837cc56a4410d34d25
Krzysztof Bieganski <kbieganski@antmicro.com>
2026-05-13 10:47:40 +0200

    Increase coverage
    Signed-off-by: Krzysztof Bieganski <kbieganski@antmicro.com>

--- test_regress/t/t_force_wide_sel.v
+++ test_regress/t/t_force_wide_sel.v
@@ -16,29 +16,25 @@ module t (
   logic [127:0] sig;
 
   always @(posedge clk) begin
-    cyc  <= cyc + 1;
-    sig  <= '0;
+    cyc <= cyc + 1;
+    sig <= '0;
+
+    sig[127:64] <= '0;  // write path
 
     if (cyc == 1) begin
       force sig[31] = 1'b1;
       force sig[32] = 1'b1;
     end
     else if (cyc == 3) begin
-      // width <= 8
-      `checkh(sig[33:26], 8'h60);
-
-      // 8 < width <= 16
-      `checkh(sig[39:24], 16'h180);
-
-      // width <= 32
-      `checkh(sig[40:20], 21'h1800);
-
-      // 32 < width <= 64
-      `checkh(sig[50:10], 41'h600000);
-
-      // width > 64
-      `checkh(sig[100:5], (96'h1 << 26) | (96'h1 << 27));
-
+      `checkh(sig[33:26], 8'h60);      // width <= 8
+      `checkh(sig[39:24], 16'h180);    // 8 < width <= 16
+      `checkh(sig[40:20], 21'h1800);   // 16 < width <= 32
+      `checkh(sig[51:20], 32'h1800);
+      `checkh(sig[29:0], 30'h0);
+      `checkh(sig[50:10], 41'h600000); // 32 < width <= 64
+      `checkh(sig[73:10], 64'h600000);
+      `checkh(sig[100:5], (96'h1 << 26) | (96'h1 << 27)); // width > 64
+      `checkh(sig[70:6], (65'h1 << 25) | (65'h1 << 26));
       $write("*-* All Finished *-*\n");
       $finish;
     end
