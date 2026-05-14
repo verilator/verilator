331a4b5f1fb7e813882c9fd866bb60a629580815
Artur Bieniek <abieniek@antmicro.com>
2026-05-14 15:39:27 +0200

    Test public non-overlapping signals
    Makes sure that public signals are not replaced with a regular read.
    I wanted to add this check to `t_vpi_force`, but apparently for something to be VPI-forced, it must be marked as forceable, which in current implementation makes a whole-width RHS0 that always overlaps.
    
    Signed-off-by: Artur Bieniek <abieniek@antmicro.com>

--- test_regress/t/t_force_wide_sel.v
+++ test_regress/t/t_force_wide_sel.v
@@ -14,16 +14,20 @@ module t (
 );
   integer cyc = 0;
   logic [127:0] sig;
+  logic [127:0] publicSig /*verilator public_flat_rw*/;
 
   always @(posedge clk) begin
     cyc <= cyc + 1;
     sig <= '0;
+    publicSig <= '0;
 
     sig[127:64] <= '0;  // write path
 
     if (cyc == 1) begin
       force sig[31] = 1'b1;
       force sig[32] = 1'b1;
+      force publicSig[31] = 1'b1;
+      force publicSig[32] = 1'b1;
     end
     else if (cyc == 3) begin
       `checkh(sig[33:26], 8'h60);      // width <= 8
@@ -35,6 +39,16 @@ module t (
       `checkh(sig[73:10], 64'h600000);
       `checkh(sig[100:5], (96'h1 << 26) | (96'h1 << 27)); // width > 64
       `checkh(sig[70:6], (65'h1 << 25) | (65'h1 << 26));
+
+      `checkh(publicSig[33:26], 8'h60);      // width <= 8
+      `checkh(publicSig[39:24], 16'h180);    // 8 < width <= 16
+      `checkh(publicSig[40:20], 21'h1800);   // 16 < width <= 32
+      `checkh(publicSig[51:20], 32'h1800);
+      `checkh(publicSig[29:0], 30'h0);
+      `checkh(publicSig[50:10], 41'h600000); // 32 < width <= 64
+      `checkh(publicSig[73:10], 64'h600000);
+      `checkh(publicSig[100:5], (96'h1 << 26) | (96'h1 << 27)); // width > 64
+      `checkh(publicSig[70:6], (65'h1 << 25) | (65'h1 << 26));
       $write("*-* All Finished *-*\n");
       $finish;
     end
