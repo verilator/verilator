d2baecf146078f1fb7eda6ddf348bfe2aece4599
Muzaffer Kal <muzaffer@kal.cc>
2026-05-14 22:24:10 -0700

    Fix timed initial NBA scheduling for SVA sampling

---
+++ test_regress/t/t_assert_preponed_nba.v
@@ -0,0 +1,35 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2026 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+  bit clk = 0;
+  int cnt = 0;
+  bit fire = 0;
+
+  always #1 clk = ~clk;
+
+  always_ff @(posedge clk) begin
+    if (fire) cnt <= cnt + 1;
+  end
+
+  assert property (@(posedge clk) fire |-> (cnt == 0))
+  else begin
+    $write("%%Error: sampled fire=1 cnt=%0d, expected preponed cnt 0\n", cnt);
+    $stop;
+  end
+
+  initial begin
+    @(posedge clk);
+    // verilator lint_off INITIALDLY
+    fire <= 1;
+    @(posedge clk);
+    fire <= 0;
+    // verilator lint_on INITIALDLY
+    repeat (2) @(posedge clk);
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
