adddec654769f9acf1918021bc5da41bf215ecac
Geza Lore <glore@tenstorrent.com>
2026-05-24 00:36:25 +0100

    Tests: Reduce peak memory use of UVM tests (#7648)

--- test_regress/t/t_uvm_dpi_v2020_3_1.py
+++ test_regress/t/t_uvm_dpi_v2020_3_1.py
@@ -17,7 +17,11 @@ if re.search(r'clang', test.cxx_version):
     test.skip("uvm_regex.cc from upstream has clang warnings")
 
 test.compile(verilator_flags2=[
-    "--binary", test.build_jobs, "--vpi", "+define+T_V2020_3_1", "+incdir+t/uvm/v2020_3_1",
+    "--binary",
+    test.build_jobs_groups,
+    "--vpi",
+    "+define+T_V2020_3_1",
+    "+incdir+t/uvm/v2020_3_1",  #
     test.pli_filename
 ])
 
