bd5af041b8e974319b37e3b9e0b8e2ac5da8798d
Artur Bieniek <abieniek@antmicro.com>
2026-05-28 15:29:35 +0200

    Fix UB in bitshift causing events to be lost
    Signed-off-by: Artur Bieniek <abieniek@antmicro.com>

---
+++ test_regress/t/t_fork_join_none_wait_ev.v
@@ -0,0 +1,40 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain, for
+// SPDX-FileCopyrightText: 2026 Antmicro
+// SPDX-License-Identifier: CC0-1.0
+
+// verilog_format: off
+`define stop $stop
+`define checkd(gotv,expv) do if ((gotv) !== (expv)) begin $write("%%Error: %s:%0d:  got=%0d exp=%0d\n", `__FILE__,`__LINE__, (gotv), (expv)); `stop; end while(0);
+// verilog_format: on
+
+module t(
+  input clk
+);
+  int cyc = 0;
+  int cnt = 0;
+  event ev[128];
+
+  always @(posedge clk) begin
+    ++cyc;
+    if (cyc >= 3) begin
+      `checkd(cnt, 128);
+      $finish;
+    end
+  end
+
+  for (genvar i = 0; i < 128; ++i) begin : gen_ev_wait
+    always @(posedge clk) begin
+      if (cyc < 3) begin
+        fork
+          begin
+            ->ev[i];
+            @(ev[i]);
+            cnt++;
+          end
+        join_none
+      end
+    end
+  end
+endmodule
