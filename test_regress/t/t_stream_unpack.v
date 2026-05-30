eda9992c834c79faecee649433f26a08d33a5fb3
Paul Swirhun <paulswirhun@gmail.com>
2026-05-30 16:11:15 -0700

    Issue 7627: add support for streaming into 2-D unpacked arrays

--- test_regress/t/t_stream_unpack.v
+++ test_regress/t/t_stream_unpack.v
@@ -285,6 +285,103 @@ module t;
     `checkh(p[0], 128'habcd0123456789abfadecafedeadbeef);
     `checkh(p[1], 128'hfadecafedeadbeefabcd0123456789ab);
 
+    begin
+      automatic logic arr2d_1 [2][2];
+      automatic logic [3:0] packed_4;
+
+      // Right-side test (unpack)
+      packed_4 = 4'b1100;
+      arr2d_1 = { >> {packed_4}};
+      `checkh(arr2d_1[0][0], 1'b1);
+      `checkh(arr2d_1[0][1], 1'b1);
+      `checkh(arr2d_1[1][0], 1'b0);
+      `checkh(arr2d_1[1][1], 1'b0);
+
+      // Left-side test (pack)
+      arr2d_1[0][0] = 1'b0;
+      arr2d_1[0][1] = 1'b1;
+      arr2d_1[1][0] = 1'b0;
+      arr2d_1[1][1] = 1'b1;
+      { >> {packed_4}} = arr2d_1;
+      `checkh(packed_4, 4'b0101);
+
+      // Constant source test
+      arr2d_1 = { >> {4'b1010}};
+      `checkh(arr2d_1[0][0], 1'b1);
+      `checkh(arr2d_1[0][1], 1'b0);
+      `checkh(arr2d_1[1][0], 1'b1);
+      `checkh(arr2d_1[1][1], 1'b0);
+
+      // 3D Test
+      begin
+        automatic logic arr3d_1 [2][2][2];
+        automatic logic [7:0] packed_8;
+
+        packed_8 = 8'b1100_1010;
+        arr3d_1 = { >> {packed_8}};
+        `checkh(arr3d_1[0][0][0], 1'b1);
+        `checkh(arr3d_1[0][0][1], 1'b1);
+        `checkh(arr3d_1[0][1][0], 1'b0);
+        `checkh(arr3d_1[0][1][1], 1'b0);
+        `checkh(arr3d_1[1][0][0], 1'b1);
+        `checkh(arr3d_1[1][0][1], 1'b0);
+        `checkh(arr3d_1[1][1][0], 1'b1);
+        `checkh(arr3d_1[1][1][1], 1'b0);
+
+        packed_8 = 8'h0;
+        { >> {packed_8}} = arr3d_1;
+        `checkh(packed_8, 8'b1100_1010);
+      end
+
+      // 48-bit Test (tests VL_UNPACK_UI_Q)
+      begin
+        automatic logic [47:0] packed_48;
+        automatic logic [11:0] arr2d_12 [2][2];
+
+        packed_48 = 48'habcdef_012345;
+        arr2d_12 = { >> {packed_48}};
+        `checkh(arr2d_12[0][0], 12'habc);
+        `checkh(arr2d_12[0][1], 12'hdef);
+        `checkh(arr2d_12[1][0], 12'h012);
+        `checkh(arr2d_12[1][1], 12'h345);
+      end
+
+      // 96-bit Test (tests VL_UNPACK_UI_W)
+      begin
+        automatic logic [95:0] packed_96;
+        automatic logic [23:0] arr2d_24 [2][2];
+
+        packed_96 = 96'h123456_789abc_def012_345678;
+        arr2d_24 = { >> {packed_96}};
+        `checkh(arr2d_24[0][0], 24'h123456);
+        `checkh(arr2d_24[0][1], 24'h789abc);
+        `checkh(arr2d_24[1][0], 24'hdef012);
+        `checkh(arr2d_24[1][1], 24'h345678);
+      end
+
+      // 2D Array of QData (64-bit) Elements Test
+      begin
+        automatic logic [127:0] packed_128;
+        automatic logic [63:0] arr2d_q [1][2];
+
+        packed_128 = 128'hfadecafedeadbeef_abcd0123456789ab;
+        arr2d_q = { >> {packed_128}};
+        `checkh(arr2d_q[0][0], 64'hfadecafedeadbeef);
+        `checkh(arr2d_q[0][1], 64'habcd0123456789ab);
+      end
+
+      // 2D Array of Wide (96-bit) Elements Test
+      begin
+        automatic logic [191:0] packed_192;
+        automatic logic [95:0] arr2d_w [1][2];
+
+        packed_192 = 192'h123456789abcdef012345678_9abcdef01234567812345678;
+        arr2d_w = { >> {packed_192}};
+        `checkh(arr2d_w[0][0], 96'h123456789abcdef012345678);
+        `checkh(arr2d_w[0][1], 96'h9abcdef01234567812345678);
+      end
+    end
+
     $write("*-* All Finished *-*\n");
     $finish;
   end
