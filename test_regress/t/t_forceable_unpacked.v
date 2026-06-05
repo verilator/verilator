a162e6849a0647a4eba5d9c8105555ac0b8d2f24
Nikolai Kumar <nikolai.kumar@gmail.com>
2026-06-05 17:24:42 -0500

    Address comment 3

--- test_regress/t/t_forceable_unpacked.v
+++ test_regress/t/t_forceable_unpacked.v
@@ -6,23 +6,68 @@
 
 `define checkh(g,e) do if ((g) !==(e)) begin $write("%%Error: %s:%0d: got=%x exp=%x\n", `__FILE__,`__LINE__, (g),(e)); $stop; end while(0)
 
+`ifdef CMT
+  `define FORCEABLE /*verilator forceable*/
+`else
+  `define FORCEABLE
+`endif
 module t (input wire clk, output reg [31:0] cyc);
-  reg [15:0] tmp;
-  initial begin cyc = 0; tmp = 16'h0260; end
+  initial cyc = 0;
   always @(posedge clk) cyc <= cyc + 1;
 
-  reg [3:0] var_arr [1:0][1:0] `ifdef CMT /*verilator forceable*/ `endif;
-  always @* {var_arr[0][0], var_arr[0][1], var_arr[1][0], var_arr[1][1]} = tmp;
+  reg [4:3] var_arr [7:6][5:4] `FORCEABLE;
+  //verilator lint_off ASCRANGE
+  reg [3:4] var_arr_a [6:7][4:5] `FORCEABLE;
+
+  initial begin
+    var_arr[6][4]   = 2'h1; var_arr[6][5]   = 2'h2;
+    var_arr[7][4]   = 2'h3; var_arr[7][5]   = 2'h2;
+    var_arr_a[6][4] = 2'h2; var_arr_a[6][5] = 2'h1;
+    var_arr_a[7][4] = 2'h1; var_arr_a[7][5] = 2'h3;
+  end
 
   always @(posedge clk) case (cyc)
-    1:       force var_arr[0][0] = 4'h7;
-    3:       `checkh(var_arr[0][0], 4'h7);
-    4:       `checkh(var_arr[0][1], 4'ha);
-    5: begin `checkh(var_arr[0][1], 4'ha); `checkh(var_arr[1][0], 4'hb); end
-    6:       release var_arr[0][0];
-    7: begin `checkh(var_arr[0][1], 4'h2); `checkh(var_arr[1][0], 4'hb); end
-    8:       `checkh(var_arr[0][0], 4'h0);
-    9: begin `checkh(var_arr[0][1], 4'h2); `checkh(var_arr[1][0], 4'h6);
-             $write("*-* All Finished *-*\n"); $finish; end
+    0: begin
+      `checkh(var_arr[6][4],   2'h1);
+      `checkh(var_arr[6][5],   2'h2);
+      `checkh(var_arr[7][4],   2'h3);
+      `checkh(var_arr[7][5],   2'h2);
+      `checkh(var_arr_a[6][4], 2'h2);
+      `checkh(var_arr_a[6][5], 2'h1);
+      `checkh(var_arr_a[7][4], 2'h1);
+      `checkh(var_arr_a[7][5], 2'h3);
+    end
+    1: begin
+      `checkh(var_arr[6][5],   2'h3);
+      `checkh(var_arr_a[6][5], 2'h2);
+      `checkh(var_arr[6][4],   2'h1);
+      `checkh(var_arr[7][4],   2'h3);
+      `checkh(var_arr_a[6][4], 2'h2);
+      `checkh(var_arr_a[7][4], 2'h1);
+    end
+    2: begin
+      `checkh(var_arr[6][5],   2'h3);
+      `checkh(var_arr[7][4],   2'h2);
+      `checkh(var_arr_a[6][5], 2'h2);
+      `checkh(var_arr_a[7][4], 2'h3);
+    end
+    3: begin
+      `checkh(var_arr[6][5],   2'h2);
+      `checkh(var_arr[7][4],   2'h2);
+      `checkh(var_arr_a[6][5], 2'h1);
+      `checkh(var_arr_a[7][4], 2'h3);
+    end
+    4: begin
+      `checkh(var_arr[6][4],   2'h1);
+      `checkh(var_arr[6][5],   2'h2);
+      `checkh(var_arr[7][4],   2'h3);
+      `checkh(var_arr[7][5],   2'h2);
+      `checkh(var_arr_a[6][4], 2'h2);
+      `checkh(var_arr_a[6][5], 2'h1);
+      `checkh(var_arr_a[7][4], 2'h1);
+      `checkh(var_arr_a[7][5], 2'h3);
+      $write("*-* All Finished *-*\n");
+      $finish;
+    end
   endcase
 endmodule
