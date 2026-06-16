38fd99b37b8505f3fe58e3dcfe34a2b4eb779268
Jakub Michalski <143384197+jmichalski-ant@users.noreply.github.com>
2026-06-16 10:21:11 +0200

    Fix out-of-bounds read value for 2-state types (#7785)
    Signed-off-by: Jakub Michalski <jmichalski@antmicro.com>

---
+++ test_regress/t/t_oob_2state_array.v
@@ -0,0 +1,28 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2026 Antmicro
+// SPDX-License-Identifier: CC0-1.0
+
+`define stop $stop
+`define checkh(gotv,expv) do if ((gotv) !== (expv)) begin $write("%%Error: %s:%0d:  got=%0x exp=%0x (%s !== %s)\n", `__FILE__,`__LINE__, (gotv), (expv), `"gotv`", `"expv`"); `stop; end while(0);
+
+
+module t;
+  logic clk = 1'b0;
+  always #1 clk = ~clk;
+
+  int idx = 0;
+  bit a[0:2] = {1, 1, 1};
+
+  always @(posedge clk) begin
+    if (idx == 4) begin
+      $write("*-* All Finished *-*\n");
+      $finish;
+    end
+    else begin
+      idx <= idx + 1;
+      `checkh(a[idx], idx <= 2 ? 1 : 0);
+    end
+  end
+endmodule
