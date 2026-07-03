f82f59a0246f7e62f2f3a237446fdf10788cd09f
Wilson Snyder <wsnyder@wsnyder.org>
2026-07-03 13:01:05 -0400

    Commentary: Changes update

--- test_regress/t/t_unique0_case.v
+++ test_regress/t/t_unique0_case.v
@@ -7,7 +7,6 @@
 // verilog_format: off
 `define stop $stop
 `define checkh(gotv,expv) do if ((gotv) !== (expv)) begin $write("%%Error: %s:%0d:  got='h%x exp='h%x\n", `__FILE__,`__LINE__, (gotv), (expv)); `stop; end while(0);
-`define checkp(gotv,expv_s) do begin string gotv_s; gotv_s = $sformatf("%p", gotv); if ((gotv_s) != (expv_s)) begin $write("%%Error: %s:%0d:  got='%s' exp='%s'\n", `__FILE__,`__LINE__, (gotv_s), (expv_s)); `stop; end end while(0);
 // verilog_format: on
 
 module t;
@@ -18,7 +17,7 @@ module t;
     y = 0;
     x = 0;
     z = 0;
-    unique0 case(increment_x())
+    unique0 case (increment_x())
       1: begin
         y = 1;
       end
