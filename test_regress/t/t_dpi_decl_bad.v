646dcd38384b8017e27d559f4fe093a446891ddc
Jakub Michalski <143384197+jmichalski-ant@users.noreply.github.com>
2026-07-08 14:22:24 +0200

    Support user-provided DPI-C function declarations (#7626) (#7893)
    Fixes #7626.

---
+++ test_regress/t/t_dpi_decl_bad.v
@@ -0,0 +1,14 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2026 Antmicro
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+  import "DPI-C" function string func(input string arg) /*verilator dpi_c_decl*/;
+
+  initial begin
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
