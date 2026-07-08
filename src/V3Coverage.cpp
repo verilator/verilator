c7e8075972a3317e0eadce801a1fc12e696b312b
Patrick Creighton <pcreighton429@gmail.com>
2026-07-08 09:18:33 -0700

    Fix --coverage on labelled inline assert/cover property (#7898) (#7904)
    Fixes #7898

--- src/V3Coverage.cpp
+++ src/V3Coverage.cpp
@@ -385,8 +385,7 @@ class CoverageVisitor final : public VNVisitor {
         VL_RESTORER(m_state);
         VL_RESTORER(m_exprStmtsp);
         VL_RESTORER(m_inToggleOff);
-        // skip properties for expresison coverage
-        if (!VN_IS(nodep, Property)) m_exprStmtsp = nodep;
+        m_exprStmtsp = nodep;
         m_inToggleOff = true;
         createHandle(nodep);
         iterateChildren(nodep);
@@ -744,6 +743,11 @@ class CoverageVisitor final : public VNVisitor {
             nodep->addCoverincsp(
                 newCoverInc(nodep->fileline(), declp, m_beginHier + "_vlCoverageUserTrace"));
         }
+    }
+    void visit(AstPropSpec* nodep) override {
+        VL_RESTORER(m_exprStmtsp);
+        m_exprStmtsp = nullptr;
+        iterateChildren(nodep);
     }
     void visit(AstStop* nodep) override {
         UINFO(4, "  STOP: " << nodep);
