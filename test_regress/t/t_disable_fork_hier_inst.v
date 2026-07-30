b997ac30c9a9a9ebfed7160195d7ab671050740c
username <marco.bartoli.dev@gmail.com>
2026-07-30 13:27:02 +0200

    Different modules test

--- test_regress/t/t_disable_fork_hier_inst.v
+++ test_regress/t/t_disable_fork_hier_inst.v
@@ -29,12 +29,56 @@ module child (
     done = 1'b1;
   end
 endmodule
+
+// This separate case disables a fork declared in a sibling module, rather than
+// another instance of the source fork declaration.
+module fork_target (
+    output bit survived,
+    output bit done
+);
+
+  initial begin : init
+    fork : target_fork
+      begin
+        #5;
+        survived = 1'b1;
+      end
+    join_none
+    #6;
+    done = 1'b1;
+  end
+endmodule
+
+module disabler (
+    output bit survived,
+    output bit done
+);
+
+  initial begin
+    fork : source_fork
+      begin
+        #2;
+        disable t.target_inst.init.target_fork;
+      end
+      begin
+        #4;
+        survived = 1'b1;
+      end
+    join
+    done = 1'b1;
+  end
+endmodule
+
 module t;
 
   bit survived0;
   bit done0;
   bit survived1;
   bit done1;
+  bit target_survived;
+  bit target_done;
+  bit disabler_survived;
+  bit disabler_done;
 
   child child0 (
       .survived(survived0),
@@ -44,6 +88,16 @@ module t;
   child child1 (
       .survived(survived1),
       .done(done1)
+  );
+
+  fork_target target_inst (
+      .survived(target_survived),
+      .done(target_done)
+  );
+
+  disabler disabler_inst (
+      .survived(disabler_survived),
+      .done(disabler_done)
   );
 
   initial begin
@@ -56,5 +110,13 @@ module t;
     `checkd(survived1, 1'b1);
     $write("*-* All Finished *-*\n");
     $finish;
+  end
+
+  initial begin
+    #7;
+    `checkd(target_done, 1'b1);
+    `checkd(target_survived, 1'b0);
+    `checkd(disabler_done, 1'b1);
+    `checkd(disabler_survived, 1'b1);
   end
 endmodule
