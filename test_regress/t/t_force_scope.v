f28cf7c895e9d3d6764cfb5326e7cb5daa163bea
Artur Bieniek <abieniek@antmicro.com>
2026-07-31 16:10:58 +0200

    Fix force helpers in multiply instantiated modules
    Signed-off-by: Artur Bieniek <abieniek@antmicro.com>

--- test_regress/t/t_force_scope.v
+++ test_regress/t/t_force_scope.v
@@ -8,6 +8,8 @@ module child (
     input wire i
 );
   /*verilator no_inline_module*/
+
+  logic [2:0] unpacked[2] = '{3'b001, 3'b010};
 endmodule
 
 module t;
@@ -16,10 +18,13 @@ module t;
 
   initial begin
     force a.i = 1'b1;
+    force a.unpacked[0] = 3'b111;
 
     if (a.i !== 1'b1) $stop;
     if (b.i !== 1'b0) $stop;
     if (a.i === b.i) $stop;
+    if (a.unpacked[0] !== 3'b111) $stop;
+    if (b.unpacked[0] !== 3'b001) $stop;
 
     $write("*-* All Finished *-*\n");
     $finish;
