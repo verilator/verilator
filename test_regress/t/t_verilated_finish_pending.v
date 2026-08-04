925a6640d139b6ab9f4ad6dfe9d4d1442e386eb7
Yilou Wang <wangyilou123@gmail.com>
2026-08-04 07:46:54 +0200

    Fix queued $finish/$stop request thread ordering (#7946 prep) (#7952)

---
+++ test_regress/t/t_verilated_finish_pending.v
@@ -0,0 +1,8 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2026 PlanV GmbH
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+endmodule
