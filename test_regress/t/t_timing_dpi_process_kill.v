fd93b5656cb8b404e1370247f3408b38a9be647f
Pawel Kojma <pkojma@internships.antmicro.com>
2026-08-06 17:00:25 +0200

    Add test for dpi timings with std::process
    Signed-off-by: Pawel Kojma <pkojma@internships.antmicro.com>

---
+++ test_regress/t/t_timing_dpi_process_kill.v
@@ -0,0 +1,72 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2026 Antmicro
+// SPDX-License-Identifier: CC0-1.0
+
+event evt1, evt2, evt3;
+
+class Foo;
+  process p;
+  bit event_received;
+
+  function new();
+    p = process::self();
+  endfunction
+
+  virtual task ewait();
+    @evt1 $display("%t: Foo received event `evt1`", $time);
+    event_received = 1;
+    ->evt2;
+  endtask
+endclass
+
+class Bar extends Foo;
+  function new();
+    super.new();
+    $display("%t: Constructing Bar", $time);
+  endfunction
+
+  virtual task ewait();
+    @evt1 $display("%t: Bar received event `evt1`", $time);
+    event_received = 1;
+  endtask
+endclass
+
+export "DPI-C" v_export = task dpi_export;
+task dpi_export();
+  process p;
+  Foo foo;
+  Bar bar;
+  fork
+    begin
+      foo = new;
+      foo.ewait();
+    end
+    begin
+      bar = new;
+      p   = process::self();
+      bar.ewait();
+    end
+  join_none
+  #10;
+  p.kill();
+
+  if ($time != 10) $stop;
+
+  ->evt1;
+  @evt2 begin
+    if (!foo.event_received) $stop;
+    if (bar.event_received) $stop;
+  end
+endtask
+
+import "DPI-C" context task dpi_import();
+
+module t;
+  initial begin
+    dpi_import();
+    $display("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
