01074bd925253e808016ccea890871c65e71eaa0
sunimali <sunimali.r@siu.edu>
2026-08-17 15:56:02 -0500

    Add licensing information to public variable offset test files

--- test_regress/t/t_public_flat_rw_large_offset.v
+++ test_regress/t/t_public_flat_rw_large_offset.v
@@ -1,4 +1,8 @@
 // DESCRIPTION: Verilator: Test public variable offsets larger than 32 bits
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2026 Sunimali Rathnayake
+// SPDX-License-Identifier: CC0-1.0
 
 module t;
   longint padding1[1<<28];
