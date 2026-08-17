edcf703b5219b8b3c35842088852440fbdf3fce2
sunimali <sunimali.r@siu.edu>
2026-08-17 16:10:37 -0500

    Update copyright information in public variable offset test files

--- test_regress/t/t_public_flat_rw_large_offset.v
+++ test_regress/t/t_public_flat_rw_large_offset.v
@@ -1,7 +1,7 @@
 // DESCRIPTION: Verilator: Test public variable offsets larger than 32 bits
 //
 // This file ONLY is placed under the Creative Commons Public Domain.
-// SPDX-FileCopyrightText: 2026 Sunimali Rathnayake
+// SPDX-FileCopyrightText: 2026 Wilson Snyder
 // SPDX-License-Identifier: CC0-1.0
 
 module t;
