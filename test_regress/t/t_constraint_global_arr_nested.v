8ebfe7abada28fa962f790a1f795642d595523c1
Kamil Danecki <kdanecki@internships.antmicro.com>
2026-08-25 18:46:08 +0200

    expand test
    Signed-off-by: Kamil Danecki <kdanecki@internships.antmicro.com>

--- test_regress/t/t_constraint_global_arr_nested.v
+++ test_regress/t/t_constraint_global_arr_nested.v
@@ -6,6 +6,23 @@
 
 `define stop $stop
 `define checkd(gotv,expv) do if ((gotv) !== (expv)) begin $write("%%Error: %s:%0d:  got=%0d exp=%0d\n", `__FILE__,`__LINE__, (gotv), (expv)); `stop; end while(0);
+`define check_rand(cl, field, cond) \
+begin \
+   automatic longint prev_result; \
+   automatic int ok; \
+   if (!bit'(cl.randomize())) $stop; \
+   prev_result = longint'(field); \
+   if (!(cond)) $stop; \
+   repeat(9) begin \
+      longint result; \
+      if (!bit'(cl.randomize())) $stop; \
+      result = longint'(field); \
+      if (!(cond)) $stop; \
+      if (result != prev_result) ok = 1; \
+      prev_result = result; \
+   end \
+   if (ok != 1) $stop; \
+end
 
 /* verilator lint_off WIDTHTRUNC */
 class Inner;
@@ -41,7 +58,8 @@ endclass
 class Outer;
   int m_idx;
   rand Middle m_mid;
-  rand Middle m_mid_arr[2];
+  rand Middle m_mid2;
+  rand Middle m_mid_arr[3];
   rand Middle m_mid_arr2[3][2];
   rand Middle m_mid_arr3[3][2];
   rand Middle m_mid_arr4[3][2];
@@ -56,6 +74,8 @@ class Outer;
     m_mid = new;
     m_mid.m_obj = new;
     foreach (m_mid.m_arr[i]) m_mid.m_arr[i] = new;
+    m_mid2 = new;
+    foreach (m_mid2.m_arr[i]) m_mid2.m_arr[i] = new;
     foreach (m_mid_arr[i]) begin
       m_mid_arr[i] = new;
       m_mid_arr[i].m_obj = new;
@@ -96,11 +116,17 @@ class Outer;
   constraint c_array_index {
     m_mid.m_arr[0].m_x == 200;
     m_mid.m_arr[0].m_y == 201;
+
+    m_mid2.m_arr[0].m_x < 200;
+    m_mid2.m_arr[0].m_y < 201;
   }
 
   constraint c_array_index_idx {
     m_mid.m_arr[m_idx].m_x == 202;
     m_mid.m_arr[m_idx].m_y == 203;
+
+    m_mid2.m_arr[m_idx].m_x < 202;
+    m_mid2.m_arr[m_idx].m_y < 203;
   }
 
   // Case 3: Nested array indexing
@@ -112,6 +138,8 @@ class Outer;
   // Case 4: Multiple array indices
   constraint c_multi_array {
     m_mid_arr[1].m_arr[2].m_y == 400;
+
+    m_mid_arr[2].m_arr[2].m_y < 400;
   }
 
   // Case 5: Associative array element member access
@@ -122,6 +150,7 @@ class Outer;
 
   constraint c_string_assoc {
     m_holder.m_string_assoc[m_key].m_x == 43;
+    m_holder.m_string_assoc[m_key].m_y < 43;
   }
 
   constraint c_assoc_nested {
@@ -176,36 +205,44 @@ module t_constraint_global_arr_nested;
     o.m_holder.mode[0].x = 42;
     o.m_holder.mode[0].x.rand_mode(0);
 
-    repeat (2) begin
-      if (o.randomize() != 1) $stop;
+    if (o.randomize() != 1) $stop;
 
-      foreach (o.m_mid_arr2[i])
-        foreach (o.m_mid_arr2[i][j])
-          if (o.m_mid_arr2[i][j].m_obj.m_x != i + j) $stop;
-      foreach (o.m_mid_arr3[i])
-        foreach (o.m_mid_arr3[i][j])
-          if (o.m_mid_arr3[i][j].m_obj.m_x != i - j) $stop;
-      foreach (o.m_mid_arr4[i])
-        if (o.m_mid_arr4[i][1].m_obj.m_x != i) $stop;
-      `checkd(o.m_mid.m_obj.m_x, 100);
-      `checkd(o.m_mid.m_obj.m_y, 101);
-      `checkd(o.m_mid.m_arr[0].m_x, 200);
-      `checkd(o.m_mid.m_arr[0].m_y, 201);
-      `checkd(o.m_mid.m_arr[1].m_x, 202);
-      `checkd(o.m_mid.m_arr[1].m_y, 203);
-      `checkd(o.m_mid_arr[0].m_obj.m_x, 300);
-      `checkd(o.m_mid_arr[0].m_obj.m_y, 301);
-      `checkd(o.m_mid_arr[1].m_arr[2].m_y, 400);
-      `checkd(o.m_assoc[0].m_x, 500);
-      `checkd(o.m_assoc[1].m_x, 502);
-      `checkd(o.m_holder.m_string_assoc["abc"].m_x, 43);
-      `checkd(o.m_assoc_nested[123][1].m_x, 501);
-      `checkd(o.m_holder.mode[0].x, 42);
-      foreach (o.m_holder.items[i]) begin
-        `checkd(o.m_holder.items[i].x, i);
-        `checkd(o.m_holder.items[i].y, i + 10);
-      end
+    foreach (o.m_mid_arr2[i])
+      foreach (o.m_mid_arr2[i][j])
+        if (o.m_mid_arr2[i][j].m_obj.m_x != i + j) $stop;
+    foreach (o.m_mid_arr3[i])
+      foreach (o.m_mid_arr3[i][j])
+        if (o.m_mid_arr3[i][j].m_obj.m_x != i - j) $stop;
+    foreach (o.m_mid_arr4[i])
+      if (o.m_mid_arr4[i][1].m_obj.m_x != i) $stop;
+    `checkd(o.m_mid.m_obj.m_x, 100);
+    `checkd(o.m_mid.m_obj.m_y, 101);
+    `checkd(o.m_mid.m_arr[0].m_x, 200);
+    `checkd(o.m_mid.m_arr[0].m_y, 201);
+    `checkd(o.m_mid.m_arr[1].m_x, 202);
+    `checkd(o.m_mid.m_arr[1].m_y, 203);
+    `checkd(o.m_mid_arr[0].m_obj.m_x, 300);
+    `checkd(o.m_mid_arr[0].m_obj.m_y, 301);
+    `checkd(o.m_mid_arr[1].m_arr[2].m_y, 400);
+    `checkd(o.m_assoc[0].m_x, 500);
+    `checkd(o.m_assoc[1].m_x, 502);
+    `checkd(o.m_holder.m_string_assoc["abc"].m_x, 43);
+    `checkd(o.m_assoc_nested[123][1].m_x, 501);
+    `checkd(o.m_holder.mode[0].x, 42);
+    foreach (o.m_holder.items[i]) begin
+      `checkd(o.m_holder.items[i].x, i);
+      `checkd(o.m_holder.items[i].y, i + 10);
     end
+    `check_rand(o, o.m_mid2.m_arr[0].m_x, o.m_mid2.m_arr[0].m_x < 200);
+    `check_rand(o, o.m_mid2.m_arr[0].m_y, o.m_mid2.m_arr[0].m_y < 201);
+
+    `check_rand(o, o.m_mid2.m_arr[1].m_x, o.m_mid2.m_arr[1].m_x < 202);
+    `check_rand(o, o.m_mid2.m_arr[1].m_y, o.m_mid2.m_arr[1].m_y < 203);
+
+    `check_rand(o, o.m_mid_arr[2].m_arr[2].m_y, o.m_mid_arr[2].m_arr[2].m_y < 400);
+
+    `check_rand(o, o.m_holder.m_string_assoc["abc"].m_y, o.m_holder.m_string_assoc["abc"].m_y < 43);
+
     repeat (4) begin
      if (randc_o.randomize() != 1)
        $fatal(1, "randc randomize failed");
