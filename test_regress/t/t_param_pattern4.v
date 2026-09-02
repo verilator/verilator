81330df3fdfce8f3fcd8178ce3a8bb26d367e58b
Todd Strader <tstrader@hudson-trading.com>
2026-09-02 09:24:35 -0400

    more testing

--- test_regress/t/t_param_pattern4.v
+++ test_regress/t/t_param_pattern4.v
@@ -20,20 +20,64 @@ module dut #(
   assign o_sum = sum;
 endmodule
 
+// Same, for an interface
+interface Ifc #(
+    parameter int ARRAY_LEN = 3,
+    parameter int ARRAY_PARAM[ARRAY_LEN] = '{2, 2, 4}
+);
+  int sum;
+  always_comb begin
+    sum = 0;
+    for (int i = 0; i < ARRAY_LEN; ++i) sum += ARRAY_PARAM[i];
+  end
+endinterface
+
+// Same, for a class
+class Cls #(
+    parameter int ARRAY_LEN = 3,
+    parameter int ARRAY_PARAM[ARRAY_LEN] = '{2, 2, 4}
+);
+  static function int sum();
+    sum = 0;
+    for (int i = 0; i < ARRAY_LEN; ++i) sum += ARRAY_PARAM[i];
+  endfunction
+endclass
+
 module t;
   int o_default;
   int o_wide;
+  int o_narrow;
 
   dut u_default (.o_sum(o_default));
 
   dut #(
       .ARRAY_LEN(4),
       .ARRAY_PARAM('{2, 2, 2, 4})
-  ) u_wide (.o_sum(o_wide));
+  ) u_wide (
+      .o_sum(o_wide)
+  );
+
+  dut #(
+      .ARRAY_LEN(2),
+      .ARRAY_PARAM('{2, 4})
+  ) u_narrow (
+      .o_sum(o_narrow)
+  );
+
+  Ifc i_default ();
+  Ifc #(
+      .ARRAY_LEN(4),
+      .ARRAY_PARAM('{2, 2, 2, 4})
+  ) i_wide ();
 
   initial begin
     if (o_default !== 8) $stop;
     if (o_wide !== 10) $stop;
+    if (o_narrow !== 6) $stop;
+    if (i_default.sum !== 8) $stop;
+    if (i_wide.sum !== 10) $stop;
+    if (Cls#()::sum() !== 8) $stop;
+    if (Cls#(4, '{2, 2, 2, 4})::sum() !== 10) $stop;
     $write("*-* All Finished *-*\n");
     $finish;
   end
