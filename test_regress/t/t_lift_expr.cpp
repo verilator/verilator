78369e8138f3a6afd0d45f58123149f5691ad391
Geza Lore <glore@tenstorrent.com>
2026-09-16 09:18:12 +0100

    Internals: Make V3LiftExpr non optional
    This pass has been working well over the last 6 months with no bug
    reports. It is an essential lowering transform that later passes can
    depend, it is not an optimization. Making it non-optional.

---
+++ test_regress/t/t_lift_expr.cpp
@@ -0,0 +1,17 @@
+// -*- mode: C++; c-file-style: "cc-mode" -*-
+//*************************************************************************
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of either the GNU Lesser General Public License Version 3
+// or the Perl Artistic License Version 2.0.
+// SPDX-FileCopyrightText: 2026 Wilson Snyder
+// SPDX-License-Identifier: LGPL-3.0-only OR Artistic-2.0
+//
+//*************************************************************************
+
+#include "Vt_lift_expr__Dpi.h"
+#include "svdpi.h"
+
+svLogic impure_0() { return sv_0; }
+
+svLogic impure_1() { return sv_1; }
