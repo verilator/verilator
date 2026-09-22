31a183ecb30b5c3b3675b0393a7593766e3a2cda
Kristof Marien <kristofmarien@meta.com>
2026-09-22 15:36:44 -0700

    Revert unneeded change

--- src/V3Randomize.cpp
+++ src/V3Randomize.cpp
@@ -2206,9 +2206,8 @@ class ConstraintExprVisitor final : public VNVisitor {
         if (VN_AS(nodep->fromp(), SFormatF)->name() == "%s.%s") {
             newp = new AstSFormatF{fl, "%s.%s." + nodep->name(), false,
                                    VN_AS(nodep->fromp(), SFormatF)->exprsp()->cloneTreePure(true)};
-            if (newp->exprsp()->nextp()->name().rfind("#x", 0) == 0) {
-                newp->exprsp()->nextp()->name("%x");  // for #x%x to %x
-            }
+            if (newp->exprsp()->nextp()->name().rfind("#x", 0) == 0)
+                newp->exprsp()->nextp()->name("%x");  //  for #x%x to %x
         } else {
             newp = new AstSFormatF{fl, nodep->fromp()->name() + "." + nodep->name(), false,
                                    nullptr};
