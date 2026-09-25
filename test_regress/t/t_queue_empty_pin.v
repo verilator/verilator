3d998e78ce8d1672d170a25337eb1563c4dc8171
Francesco Urbani <18237108+urbanij@users.noreply.github.com>
2026-09-25 11:23:16 +0200

    Cover empty queue argument contexts

--- test_regress/t/t_queue_empty_pin.v
+++ test_regress/t/t_queue_empty_pin.v
@@ -13,9 +13,14 @@ module t;
     if (q.size != 0) $stop;
   endtask
 
+  task dtsk(int q[]);
+    if (q.size != 0) $stop;
+  endtask
+
   initial begin
     tsk();
     qtsk({});
+    dtsk({});
 
     $write("*-* All Finished *-*\n");
     $finish;
